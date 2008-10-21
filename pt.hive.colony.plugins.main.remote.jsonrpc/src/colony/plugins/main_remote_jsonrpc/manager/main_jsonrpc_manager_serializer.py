fce9ac183600fa25d2490c373be7669acdd86a39
João Magalhães <joamag@hive.pt>
2008-10-21 21:33:14 +0000

    initial import
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@51 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_remote_jsonrpc/manager/main_jsonrpc_manager_serializer.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2120 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 16:01:47 +0100 (Ter, 21 Out 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+__credits__ = "Jan-Klaas Kollhof <keyjaque@yahoo.com>"
+""" The credits for the module """
+
+import re
+import types
+
+import main_jsonrpc_manager_exceptions
+
+char_replacements = {
+        "\t" : "\\t",
+        "\b" : "\\b",
+        "\f" : "\\f",
+        "\n" : "\\n",
+        "\r" : "\\r",
+        "\\" : "\\\\",
+        "/" : "\\/",
+        "\"" : "\\\""}
+
+escape_char_to_char = {
+        "t": "\t",
+        "b": "\b",
+        "f": "\f",
+        "n": "\n",
+        "r": "\r",
+        "\\": "\\",
+        "/": "/",
+        "\"" : "\""}
+
+string_escape_re = re.compile(r"[\x00-\x19\\\"/\b\f\n\r\t]")
+digits_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def escape_char(match):
+    c = match.group(0)
+    try:
+        replacement = char_replacements[c]
+        return replacement
+    except KeyError:
+        d = ord(c)
+        if d < 32:
+            return "\\u%04x" % d
+        else:
+            return c
+
+def dumps(obj):
+    return unicode("".join([part for part in dump_parts(obj)]))
+
+def dump_parts(obj):
+    obj_type = type(obj)
+    if obj == None:
+        yield u"null"
+    elif obj_type is types.FunctionType:
+        yield u"\"function\""
+    elif obj_type is types.ModuleType:
+        yield u"\"module\""
+    elif obj_type is types.MethodType:
+        yield u"\"method\""
+    elif obj_type is types.InstanceType:
+        yield u"{"
+        is_first = True
+        obj_items = dir(obj)
+        for obj_item in obj_items:
+            if is_first:
+                is_first = False
+            else:
+                yield u","
+            obj_value = getattr(obj, obj_item)
+            yield "\"" + obj_item + "\"" + ":"
+            for part in dump_parts(obj_value):
+                yield part
+        yield u"}"
+    elif obj_type is types.BooleanType:
+        if obj:
+            yield u"true"
+        else:
+            yield u"false"
+    elif obj_type is types.DictionaryType:
+        yield u"{"
+        is_first = True
+        for (key, value) in obj.items():
+            if is_first:
+                is_first = False
+            else:
+                yield u","
+            yield u"\"" + string_escape_re.sub(escape_char, key) + u"\":"
+            for part in dump_parts(value):
+                yield part
+        yield u"}"
+    elif obj_type in types.StringTypes:
+        yield u"\"" + string_escape_re.sub(escape_char, obj) + u"\""
+    elif obj_type in [types.TupleType, types.ListType, types.GeneratorType]:
+        yield u"["
+        is_first = True
+        for item in obj:
+            if is_first:
+                is_first = False
+            else:
+                yield u","
+            for part in dump_parts(item):
+                yield part
+        yield u"]"
+    elif obj_type in [types.IntType, types.LongType, types.FloatType]:
+        yield unicode(obj)
+    else:
+        raise main_jsonrpc_manager_exceptions.JSONEncodeException(obj)
+
+def loads(s):
+    stack = []
+    chars = iter(s)
+    value = None
+    curr_char_is_next = False
+
+    try:
+        while(1):
+            skip = False
+            if not curr_char_is_next:
+                c = chars.next()
+            while(c in [" ", "\t", "\r", "\n"]):
+                c = chars.next()
+            curr_char_is_next = False
+            if c == "\"":
+                value = ""
+                try:
+                    c = chars.next()
+                    while c != "\"":
+                        if c == "\\":
+                            c = chars.next()
+                            try:
+                                value += escape_char_to_char[c]
+                            except KeyError:
+                                if c == "u":
+                                    hex_code = chars.next() + chars.next() + chars.next() + chars.next()
+                                    value += unichr(int(hex_code, 16))
+                                else:
+                                    raise main_jsonrpc_manager_exceptions.JSONDecodeException("Bad Escape Sequence Found")
+                        else:
+                            value += c
+                        c = chars.next()
+                except StopIteration:
+                    raise main_jsonrpc_manager_exceptions.JSONDecodeException("Expected end of String")
+            elif c == "{":
+                stack.append({})
+                skip = True
+            elif c == "}":
+                value = stack.pop()
+            elif c == "[":
+                stack.append([])
+                skip = True
+            elif c == "]":
+                value = stack.pop()
+            elif c in [",", ":"]:
+                skip = True
+            elif c in digits_list or c == "-":
+                digits = [c]
+                c = chars.next()
+                num_conv = int
+                try:
+                    while c in digits_list:
+                        digits.append(c)
+                        c = chars.next()
+                    if c == ".":
+                        num_conv = float
+                        digits.append(c)
+                        c = chars.next()
+                        while c in digits_list:
+                            digits.append(c)
+                            c = chars.next()
+                        if c.upper() == "E":
+                            digits.append(c)
+                            c = chars.next()
+                            if c in ["+", "-"]:
+                                digits.append(c)
+                                c = chars.next()
+                                while c in digits_list:
+                                    digits.append(c)
+                                    c = chars.next()
+                            else:
+                                raise main_jsonrpc_manager_exceptions.JSONDecodeException("Expected + or -")
+                except StopIteration:
+                    pass
+                value = num_conv("".join(digits))
+                curr_char_is_next = True
+
+            elif c in ["t", "f", "n"]:
+                kw = c + chars.next() + chars.next() + chars.next()
+                if kw == "null":
+                    value = None
+                elif kw == "true":
+                    value = True
+                elif kw == "fals" and chars.next() == "e":
+                    value = False
+                else:
+                    raise main_jsonrpc_manager_exceptions.JSONDecodeException("Expected Null, False or True")
+            else:
+                raise main_jsonrpc_manager_exceptions.JSONDecodeException("Expected []{},\" or Number, Null, False or True")
+
+            if not skip:
+                if len(stack):
+                    top = stack[-1]
+                    if type(top) is types.ListType:
+                        top.append(value)
+                    elif type(top) is types.DictionaryType:
+                        stack.append(value)
+                    elif type(top) in types.StringTypes:
+                        key = stack.pop()
+                        stack[-1][key] = value
+                    else:
+                        raise main_jsonrpc_manager_exceptions.JSONDecodeException("Expected dictionary key, or start of a value")
+                else:
+                    return value
+    except StopIteration:
+         raise main_jsonrpc_manager_exceptions.JSONDecodeException("Unexpected end of JSON source")
---
+++ pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_remote_jsonrpc/manager/main_jsonrpc_manager_system.py
@@ -0,0 +1,277 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2132 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 18:22:04 +0100 (Ter, 21 Out 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import main_jsonrpc_manager_serializer
+import main_jsonrpc_manager_exceptions
+
+HANDLER_FILENAME = "jsonrpc.py"
+LIST_METHODS_NAME = "system.listMethods"
+
+class MainJsonrpcManager:
+
+    main_jsonrpc_manager_plugin = None
+
+    service_methods = []
+    service_methods_map = {}
+
+    def __init__(self, main_jsonrpc_manager_plugin):
+        self.main_jsonrpc_manager_plugin = main_jsonrpc_manager_plugin
+
+        self.service_objects = []
+        self.service_methods_map = {}
+
+    def get_handler_filename(self):
+        return HANDLER_FILENAME
+
+    def is_request_handler(self, request):
+        # retrieves the simple filename from the complete path filename
+        simple_filename = request.filename.split("/")[-1]
+
+        if simple_filename == HANDLER_FILENAME:
+            return True
+        else:
+            return False
+
+    def handle_request(self, request):
+        # sets the content type for the request
+        request.content_type = "text/plain;charset=utf-8"
+
+        # retrieves the post data
+        post_data = request.read()
+
+        # the translated request
+        translated_request = self.translate_request(post_data)
+
+        # retrieves the id of the request
+        id_ = translated_request["id"]
+
+        # retrieves the method of the request
+        method_name = translated_request["method"]
+
+        # retrieves the parameters of the request
+        parameters = translated_request["params"]
+
+        # in case there is a list methods request
+        if method_name == LIST_METHODS_NAME and parameters == []:
+            result = self.service_methods
+            error = None
+        # tries to call the requested method
+        elif method_name in self.service_methods_map:
+            # retrieves the rpc method
+            rpc_method = self.service_methods_map[method_name]
+
+            # retrieves the number of arguments for the rpc method
+            rpc_method_number_arguments = rpc_method.func_code.co_argcount - 1
+
+            # retrieves the number of parameters sent
+            number_parameters = len(parameters)
+
+            # in case the number of sent arguments is the expected
+            if not number_parameters == rpc_method_number_arguments:
+                result = None
+                error = main_jsonrpc_manager_exceptions.InvalidNumberArguments("The number of sent arguments is " + str(number_parameters) + ", expected " + str(rpc_method_number_arguments))
+            else:
+                # calls the rpc method with the given arguments
+                result = rpc_method(*parameters)
+                error = None
+        # in case the method name is not valid
+        else:
+            result = None
+            error = main_jsonrpc_manager_exceptions.InvalidMethod("The method name " + method_name + " is not valid")
+
+        # serializes the result into json
+        result_request = self.translate_result(result, error, id_)
+
+        # encodes the result into utf-8
+        result_request_encoded = result_request.encode("utf-8")
+
+        # writes the serialized result into the buffer
+        request.write(result_request_encoded)
+
+        # flushes the request, sending the output to the client
+        request.flush()
+
+    def update_service_methods(self, updated_rpc_service_plugin = None):
+
+        if updated_rpc_service_plugin:
+            updated_rpc_service_plugins = [updated_rpc_service_plugin]
+        else:
+            # clears the service methods list
+            self.service_methods = []
+
+            # clears the service map
+            self.service_methods_map = {}
+
+            # retrieves the updated rpc service plugins
+            updated_rpc_service_plugins = self.main_jsonrpc_manager_plugin.rpc_service_plugins
+
+        for rpc_service_plugin in updated_rpc_service_plugins:
+            # retrieves all the method names for the current rpc service
+            available_rpc_methods = rpc_service_plugin.get_available_rpc_methods()
+
+            # retrieves all the method alias for the current rpc service
+            available_rpc_methods_alias = rpc_service_plugin.get_rpc_methods_alias()
+
+            # in case the plugin contains the rpc method metadata
+            if rpc_service_plugin.contains_metadata_key("rpc_method"):
+                # retrieves the metadata values for the rpc method
+                metadata_values = rpc_service_plugin.get_metadata_key("rpc_method")
+
+                # iterates over all the metadata values
+                for metadata_value in metadata_values:
+                    # retrieves the method name of the rpc method
+                    method_name = metadata_value["method_name"]
+
+                    # retrieves the alias for the rpc method
+                    alias = metadata_value["alias"]
+
+                    # retrieves the method for the rpc method from the plugin instance
+                    method = getattr(rpc_service_plugin, method_name)
+
+                    # adds the method to the list of available rpc methods
+                    available_rpc_methods.append(method)
+
+                    # adds the alias to the list of available rpc methods alias
+                    available_rpc_methods_alias[method] = alias
+
+            # retrieves the list of all the available rpc methods
+            available_rpc_methods_string = [value.__name__ for value in available_rpc_methods]
+
+            # iterates over all the rpc method alias keys
+            for available_rpc_method_alias_key in available_rpc_methods_alias:
+                available_rpc_methods_alias_string = available_rpc_methods_alias[available_rpc_method_alias_key]
+                available_rpc_methods_string.extend(available_rpc_methods_alias_string)
+
+            self.service_methods.extend(available_rpc_methods_string)
+
+            # retrieves the service id
+            service_id = rpc_service_plugin.get_service_id()
+
+            # retrieves the list of service alias
+            service_alias = rpc_service_plugin.get_service_alias()
+
+            # creates a list with all the possible service names
+            service_names = [service_id] + service_alias
+
+            # iterates over all the possible service names
+            for service_name in service_names:
+                for available_rpc_method_string in available_rpc_methods_string:
+                    composite_available_rpc_method_string = service_name + "." + available_rpc_method_string
+                    self.service_methods.append(composite_available_rpc_method_string)
+
+            # iterates over all the available rpc methods to generate the service methods map
+            for available_rpc_method in available_rpc_methods:
+                # creates the service method names list
+                service_method_names = []
+
+                # creates the service method basic names list
+                service_method_basic_names = []
+
+                # adds the available rpc method to the service method names list
+                service_method_names.append(available_rpc_method.__name__)
+
+                # adds the available rpc method to the service basic method names list
+                service_method_basic_names.append(available_rpc_method.__name__)
+
+                # retrieves all the alias to the current service methods
+                alias_service_method_names = [value for value in available_rpc_methods_alias[available_rpc_method]]
+
+                # adds the available rpc method alias to the service method names list
+                service_method_names.extend(alias_service_method_names)
+
+                # adds the available rpc method alias to the service basic method names list
+                service_method_basic_names.extend(alias_service_method_names)
+
+                # iterates over all the service names
+                for service_name in service_names:
+                    for service_method_basic_name in service_method_basic_names:
+                        service_method_complex_name = service_name + "." + service_method_basic_name
+                        service_method_names.append(service_method_complex_name)
+
+                # iterates over all the service method names
+                for service_method_name in service_method_names:
+                    # adds the available rpc method to the map with the service method name as key
+                    self.service_methods_map[service_method_name] = available_rpc_method
+
+    def translate_request(self, data):
+        """
+        Translates the given json data data into a python request
+        
+        @type data: String
+        @param data: The json data to be translated into a python request
+        @rtype: Any
+        @return: The translated python request
+        """
+
+        try:
+            request = main_jsonrpc_manager_serializer.loads(data)
+        except:
+            raise main_jsonrpc_manager_exceptions.ServiceRequestNotTranslatable(data)
+
+        # returns the translated request
+        return request
+
+    def translate_result(self, result, error, id_):
+        """
+        Translates the given python result into json data
+        
+        @type result: Any
+        @param result: The python result to be translated into json data
+        @type error: Error
+        @param error: The error for the current request
+        @type id_: int
+        @param id_: The id of the request and result
+        @rtype: String
+        @return: The translated json data
+        """
+
+        # in case there is an error
+        if not error == None:
+            error = {"name" : error.__class__.__name__, "message" : error.message}
+            data = main_jsonrpc_manager_serializer.dumps({"result" : None, "id" : id_, "error" : error})
+            return data
+
+        try:
+            data = main_jsonrpc_manager_serializer.dumps({"result" : result, "id" : id_, "error" : error})
+        except main_jsonrpc_manager_exceptions.JSONEncodeException, e:
+            error = {"name" : "JSONEncodeException", "message" : "Result Object Not Serializable"}
+            data = main_jsonrpc_manager_serializer.dumps({"result" : None, "id" : id_, "error" : error})
+
+        # returns the json data
+        return data
