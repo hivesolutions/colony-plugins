418cf59bf1348faa45920cf954ceb85002d182cc
João Magalhães <joamag@hive.pt>
2008-10-24 18:02:10 +0000

    new izpack project
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@156 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/izpack/izpack_build_automation_extension_system.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Ter, 21 Out 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+class IzpackBuildAutomationExtension:
+    """
+    The izpack build automation extension class.
+    """
+
+    izpack_build_automation_extension_plugin = None
+    """ The izpack build automation extension plugin """
+
+    def __init__(self, izpack_build_automation_extension_plugin):
+        """
+        Constructor of the class.
+        
+        @type izpack_build_automation_extension_plugin: IzpackBuildAutomationExtensionPlugin
+        @param izpack_build_automation_extension_plugin: The izpack build automation extension plugin.
+        """
+
+        self.izpack_build_automation_extension_plugin = izpack_build_automation_extension_plugin
+
+    def run_automation(self, stage, parameters, plugin):
+        pass
