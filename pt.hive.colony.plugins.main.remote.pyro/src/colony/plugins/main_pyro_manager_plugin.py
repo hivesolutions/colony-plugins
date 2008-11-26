efa0051602977ba9f45b4c71af2d08fc062c007a
João Magalhães <joamag@hive.pt>
2008-11-26 16:12:36 +0000

    small front-end method added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@493 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.pyro/src/colony/plugins/main_pyro_manager_plugin.py
+++ pt.hive.colony.plugins.main.remote.pyro/src/colony/plugins/main_pyro_manager_plugin.py
@@ -92,6 +92,9 @@ class MainPyroManagerPlugin(colony.plugins.plugin_system.Plugin):
     def is_active(self):
         return self.main_pyro_manager.is_active()
 
+    def get_handler_name(self):
+        return self.main_pyro_manager.get_handler_name()
+
     @colony.plugins.decorators.load_allowed_capability("rpc_service")
     def rpc_service_capability_load_allowed(self, plugin, capability):
         self.rpc_service_plugins.append(plugin)
