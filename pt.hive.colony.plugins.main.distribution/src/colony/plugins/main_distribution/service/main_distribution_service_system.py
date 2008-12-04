bee90185cef605309531cd48d68cc96d18de622a
João Magalhães <joamag@hive.pt>
2008-12-04 22:15:37 +0000

    initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@623 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.main.distribution/src/colony/plugins/main_distribution/service/main_distribution_service_system.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Tue, 21 Oct 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+SERVICE_ID = "main_distribution_service"
+""" The service id """
+
+class MainDistributionService:
+    """
+    The main distribution service class.
+    """
+
+    main_distribution_service_plugin = None
+    """ The main distribution service plugin """
+
+    def __init__(self, main_distribution_service_plugin):
+        """
+        Constructor of the class.
+        
+        @type main_distribution_service_plugin: MainDistributionServicePlugin
+        @param main_distribution_service_plugin: The main distribution service plugin.
+        """
+
+        self.main_distribution_service_plugin = main_distribution_service_plugin
+
+    def get_service_id(self):
+        return SERVICE_ID
+
+    def get_service_alias(self):
+        return []
+
+    def get_available_rpc_methods(self):
+        return []
+
+    def get_rpc_methods_alias(self):
+        return {}
+
+    def get_all_registry_entries(self):
+        return []
+
+    def unload_plugin_manager(self):
+        # retrieves the plugin manager
+        manager = self.main_distribution_service_plugin.manager
+
+        # unloads the plugin manager system
+        manager.unload_system()
