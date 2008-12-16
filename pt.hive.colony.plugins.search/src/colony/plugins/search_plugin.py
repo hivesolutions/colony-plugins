454f20a03b4ab05fdf7ff8ca644e8f6e57e6da8f
Luís Martinho <lmartinho@hive.pt>
2008-12-16 15:17:56 +0000

    initial search plugin commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@737 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.search/src/colony/plugins/search_plugin.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt> & Luís Martinho <lmartinho@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 684 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-12-08 15:16:55 +0000 (Seg, 08 Dez 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.plugins.plugin_system
+import colony.plugins.decorators
+
+class SearchPlugin(colony.plugins.plugin_system.Plugin):
+    """
+    The main class for the Search plugin.
+    """
+
+    id = "pt.hive.colony.plugins.search"
+    name = "Search Plugin"
+    short_name = "Search"
+    description = "Search Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
+    capabilities = ["search"]
+    capabilities_allowed = ["search_crawler"]
+    dependencies = [colony.plugins.plugin_system.PluginDependency(
+                    "pt.hive.colony.plugins.search.interpreter", "1.0.0")]
+    events_handled = []
+    events_registrable = []
+
+    search_system = None
+
+    search_crawler_plugins = []
+
+    search_interpreter_plugin = None
+
+    def load_plugin(self):
+        colony.plugins.plugin_system.Plugin.load_plugin(self)
+        global search
+        import search.search_system
+        self.search_system = search.search_system.SearchSystem(self)
+
+    def end_load_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_load_plugin(self)    
+
+    def unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_unload_plugin(self)
+
+    @colony.plugins.decorators.load_allowed("pt.hive.colony.plugins.search", "1.0.0")
+    def load_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    @colony.plugins.decorators.unload_allowed("pt.hive.colony.plugins.search", "1.0.0")
+    def unload_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.plugins.decorators.inject_dependencies("pt.hive.colony.plugins.search", "1.0.0")
+    def dependency_injected(self, plugin):
+        colony.plugins.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def create_index(self, properties):
+        return self.search_system.create_index(properties)
+
+    def load_index(self, properties):
+        return self.search_system.load_index(properties)
+
+    @colony.plugins.decorators.load_allowed_capability("search_crawler")
+    def search_provider_file_system_load_allowed(self, plugin, capability):
+        self.search_crawler_plugins.append(plugin)
+
+    @colony.plugins.decorators.unload_allowed_capability("search_crawler")
+    def search_provider_file_system_unload_allowed(self, plugin, capability):
+        self.search_crawler_plugins.remove(plugin)
+
+    def get_search_interpreter_plugin(self):
+        return self.search_interpreter_plugin
+
+    @colony.plugins.decorators.plugin_inject("pt.hive.colony.plugins.search.interpreter")
+    def set_search_interpreter_plugin(self, search_interpreter_plugin):
+        self.search_interpreter_plugin = search_interpreter_plugin
