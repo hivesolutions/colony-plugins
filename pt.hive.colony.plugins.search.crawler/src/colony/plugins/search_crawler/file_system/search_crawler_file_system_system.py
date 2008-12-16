822ce9ec7e02b9f1a9330a3cae394d8e72f6267c
Luís Martinho <lmartinho@hive.pt>
2008-12-16 13:16:18 +0000

    crawler plugin initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@732 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.search.crawler/src/colony/plugins/search_crawler/file_system/search_crawler_file_system_system.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Jo‹o Magalh‹es <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Tue, 21 Oct 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import os.path
+
+import search_crawler_file_system_exceptions
+
+class SearchCrawlerFileSystem:
+    """
+    The search crawler file system class.
+    """
+
+    search_crawler_file_system_plugin = None
+    """ The search crawler file system plugin """
+
+    def __init__(self, search_crawler_file_system_plugin):
+        """
+        Constructor of the class.
+        
+        @type search_provider_text_plugin: SearchProviderTextPlugin
+        @param search_provider_text_plugin: The search provider text plugin.
+        """
+
+        self.search_crawler_file_system_plugin = search_crawler_file_system_plugin
+
+    def get_tokens(self, properties):
+        if not "start_path" in properties:
+            raise search_crawler_file_system_exceptions.MissingProperty("start_path")
+
+        start_path = properties["start_path"]
+
+        token_list = []
+
+        os.path.walk(start_path, self.walker, token_list)
+
+        return token_list
+
+    def walker(self, args, directory_name, names):
+        # open the directory
+        
+        # iterate through all the search provider text file plugins
+        # have the search provider text file plugin process the file name
+
+        token_list = args
+
+        # creates the file paths list
+        file_paths_list = [directory_name + "/" + value for value in names]
+
+        for file_path in file_paths_list:
+            properties = {"file_path" : file_path}
+
+            search_provider_file_system_plugin = self.get_handler_plugin(properties)
+
+            if search_provider_file_system_plugin:
+                tokens = search_provider_file_system_plugin.get_tokens(properties)
+                token_list.append(tokens)
+    
+    def get_handler_plugin(self, properties):
+        search_provider_file_system_plugins = self.search_crawler_file_system_plugin.search_provider_file_system_plugins
+
+        for search_provider_file_system_plugin in search_provider_file_system_plugins:
+            if search_provider_file_system_plugin.is_file_provider(properties):
+                return search_provider_file_system_plugin
---
+++ pt.hive.colony.plugins.search.crawler/src/colony/plugins/search_crawler_file_system_plugin.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Jo‹o Magalh‹es <joamag@hive.pt> & Lu’s Martinho <lmartinho@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 684 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-12-08 15:16:55 +0000 (Seg, 08 Dez 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.plugins.plugin_system
+import colony.plugins.decorators
+
+class SearchCrawlerFileSystemPlugin(colony.plugins.plugin_system.Plugin):
+    """
+    The main class for the Search Crawler File System plugin.
+    """
+
+    id = "pt.hive.colony.plugins.search.crawler.file_system"
+    name = "Search Crawler File System Plugin"
+    short_name = "Search Crawler File System"
+    description = "Search Crawler File System Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
+    capabilities = ["search_crawler"]
+    capabilities_allowed = ["search_provider.file_system"]
+    dependencies = []
+    events_handled = []
+    events_registrable = []
+
+    search_crawler_file_system = None
+
+    search_provider_file_system_plugins = []
+
+    def load_plugin(self):
+        colony.plugins.plugin_system.Plugin.load_plugin(self)
+        global search_crawler
+        import search_crawler.file_system.search_crawler_file_system_system
+        self.search_crawler_file_system = search_crawler.file_system.search_crawler_file_system_system.SearchCrawlerFileSystem(self)
+
+    def end_load_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_load_plugin(self)    
+
+    def unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_unload_plugin(self)
+
+    @colony.plugins.decorators.load_allowed("pt.hive.colony.plugins.search.crawler.file_system", "1.0.0")
+    def load_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    def dependency_injected(self, plugin):
+        colony.plugins.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_tokens(self, properties):
+        return self.search_crawler_file_system.get_tokens(properties)
+
+    @colony.plugins.decorators.load_allowed_capability("search_provider.file_system")
+    def search_provider_file_system_load_allowed(self, plugin, capability):
+        self.search_provider_file_system_plugins.append(plugin)
+
+    @colony.plugins.decorators.unload_allowed_capability("search_provider.file_system")
+    def search_provider_file_system_unload_allowed(self, plugin, capability):
+        self.search_provider_file_system_plugins.remove(plugin)
