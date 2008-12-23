f9f43daaa1caf5dbd7c72ab3da0ebc97d3e806be
João Magalhães <joamag@hive.pt>
2008-12-23 23:03:19 +0000

    new imports added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@859 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http_plugin.py
+++ pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http_plugin.py
@@ -38,6 +38,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
 import colony.plugins.plugin_system
+import colony.plugins.decorators
 
 class MainServiceHttpPlugin(colony.plugins.plugin_system.Plugin):
     """
