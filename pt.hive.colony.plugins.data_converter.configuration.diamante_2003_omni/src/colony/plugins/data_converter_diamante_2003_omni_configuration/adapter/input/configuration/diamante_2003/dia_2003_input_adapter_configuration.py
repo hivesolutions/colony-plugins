e16e530ad45a3deb32fecaee653fb78877d0954e
Simão Rio <srio@hive.pt>
2009-03-13 14:14:50 +0000

    first set complete
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@1887 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.data_converter.configuration.diamante_2003_omni/src/colony/plugins/data_converter_diamante_2003_omni_configuration/adapter/input/configuration/diamante_2003/dia_2003_input_adapter_configuration.py
+++ pt.hive.colony.plugins.data_converter.configuration.diamante_2003_omni/src/colony/plugins/data_converter_diamante_2003_omni_configuration/adapter/input/configuration/diamante_2003/dia_2003_input_adapter_configuration.py
@@ -52,7 +52,8 @@ class Diamante2003InputAdapterConfiguration:
     """ Reference to the plugin that owns this code """
 
     #@todo: move this to xml file
-    work_unit_tables_map = {"customer" : ["clientes", "paises", "profissa", "observa", "zonas", "password", "forneced"]} 
+    work_unit_tables_map = {"customer" : ["empresas", "lojas", "funciona", "vendedor", "password", "zonas", "bancos", "estilo", "pureza", "qualidad", "tipopeca", "coleccao", "tiposerv", "talhe", "tipoprod", "tipopaga", "forneced", "produtos", "imagens", "profissa", "clientes"]}
+
     """ Dictionary relating work unit name with a list of the database table names that need to be processed for the work unit to be complete """
 
     def __init__(self, parent_plugin):
@@ -164,7 +165,6 @@ class Diamante2003InputAdapterConfiguration:
             value = newstr
         return value
 
-    #@todo: this function should be on the output adapter instead
     def column_handler_diamante_gender_id_to_hive_gender_id(self, arguments):
         """
         Converts a Diamante 2003 gender enumeration to a Hive gender enumeration
@@ -177,14 +177,174 @@ class Diamante2003InputAdapterConfiguration:
 
         DIAMANTE_GENDER_MALE = "1"
         DIAMANTE_GENDER_FEMALE = "2"
-        DIAMANTE_GENDER_NEUTRAL = "3"
+        DIAMANTE_COMPANY = "3"
         HIVE_GENDER_MALE = "0"
         HIVE_GENDER_FEMALE = "1"
-        HIVE_GENDER_NEUTRAL = "2"
+        HIVE_COMPANY = "2"
 
         if value == DIAMANTE_GENDER_MALE:
             return HIVE_GENDER_MALE
         elif value == DIAMANTE_GENDER_FEMALE:
             return HIVE_GENDER_FEMALE
-        elif value == DIAMANTE_GENDER_NEUTRAL:
-            return HIVE_GENDER_NEUTRAL
+        else:
+            return HIVE_COMPANY
+
+    def column_handler_diamante_anulado_to_hive_status(self, arguments):
+        """
+        Converts a Diamante 2003 status enumeration to a Hive status enumeration
+        
+        @param arguments: List of arguments (1st argument: value to process)
+        @return: Returns the processed value
+        """
+
+        value = arguments[0]
+
+        DIAMANTE_ACTIVE1 = ""
+        DIAMANTE_ACTIVE2 = "0"
+        DIAMANTE_INACTIVE = "1"
+        HIVE_ACTIVE = "0"
+        HIVE_INACTIVE = "1"
+        
+        if value == DIAMANTE_ACTIVE1:
+            return HIVE_ACTIVE
+        elif value == DIAMANTE_ACTIVE2:
+            return HIVE_ACTIVE
+        elif value == DIAMANTE_INACTIVE:
+            return HIVE_INACTIVE
+
+    def column_handler_diamante_paises_to_hive_country(self, arguments):
+        """
+        Converts a Diamante 2003 country enumeration to a Hive country enumeration
+        
+        @param arguments: List of arguments (1st argument: value to process)
+        @return: Returns the processed value
+        """
+
+        value = arguments[0]
+
+        DIAMANTE_PORTUGAL = "01"
+        DIAMANTE_ESPANHA = "02"
+        DIAMANTE_GRECIA = "03"
+        DIAMANTE_HOLANDA = "04"
+        DIAMANTE_BELGICA = "05"
+        DIAMANTE_FRANCA = "06"
+        DIAMANTE_ITALIA = "07"
+        DIAMANTE_ALEMANHA = "08"
+        DIAMANTE_FINLANDIA = "09"
+        DIAMANTE_AUSTRIA = "10"
+        
+        HIVE_PORTUGAL = "Portugal"
+        HIVE_ESPANHA = "Espanha"
+        HIVE_GRECIA = "Grecia"
+        HIVE_HOLANDA = "Holanda"
+        HIVE_BELGICA = "Belgica"
+        HIVE_FRANCA = "Franca"
+        HIVE_ITALIA = "Italia"
+        HIVE_ALEMANHA = "Alemanha"
+        HIVE_FINLANDIA = "Finlandia"
+        HIVE_AUSTRIA = "Austria"
+        HIVE_OTHER = "Other"
+        
+        if value == DIAMANTE_PORTUGAL:
+            return HIVE_PORTUGAL
+        elif value == DIAMANTE_ESPANHA:
+            return HIVE_ESPANHA
+        elif value == DIAMANTE_GRECIA:
+            return HIVE_GRECIA
+        elif value == DIAMANTE_HOLANDA:
+            return HIVE_HOLANDA
+        elif value == DIAMANTE_BELGICA:
+            return HIVE_BELGICA
+        elif value == DIAMANTE_FRANCA:
+            return HIVE_FRANCA
+        elif value == DIAMANTE_ITALIA:
+            return HIVE_ITALIA
+        elif value == DIAMANTE_ALEMANHA:
+            return HIVE_ALEMANHA
+        elif value == DIAMANTE_FINLANDIA:
+            return HIVE_FINLANDIA
+        elif value == DIAMANTE_AUSTRIA:
+            return HIVE_AUSTRIA
+        else:
+            return HIVE_OTHER
+        
+    def row_handler_feitio_to_design_cost(self, arguments):
+        """
+        Converts a Diamante 2003 feitio value to a Hive design cost, based on the calculation method
+        
+        @param arguments: List of arguments (1st argument: value to process)
+        @return: Returns the processed value
+        """
+
+        #get dictionary with all the values of the table line
+        table_line_dic = arguments[0].row
+        
+        #if tipolgpl is not null
+        if table_line_dic.has_key("tiplgp"):
+            
+            #get design cost calculation method
+            tiplgp = table_line_dic["tiplgp"]
+            
+            #get unit weight
+            pesounit = table_line_dic["pesounit"]
+            
+            #get design cost (not processed)
+            feitio = table_line_dic["feitio"]
+        
+            # if the design cost is calculated based on product weight
+            if tiplgp == "1":
+                feitio = pesounit * feitio
+                table_line_dic["feitio"] = feitio
+
+    def row_handler_margin_calculation_type(self, arguments):
+        """
+        Asks the user to identify the margin calculation method
+        
+        @param arguments: List of arguments (1st argument: value to process)
+        @return: Returns the processed value
+        """
+        
+        MARGIN_CALCULATION_TYPE_PERCENTAGE = 0
+        MARGIN_CALCULATION_TYPE_VALUE = 1        
+        
+        #if margin calculation method not already defined
+        if hasattr(arguments[0], "margin_calculation_type") == False:
+            
+            # @todo: SCERIO: utilizar isto
+            # margin_calculation_type = raw_input("Enter margin calculation type (0 - Percentage    1 - Value): ")
+            margin_calculation_type = MARGIN_CALCULATION_TYPE_PERCENTAGE
+            
+            #set margin calculation type to user definition
+            setattr(arguments[0], "margin_calculation_type", margin_calculation_type)
+
+    def row_handler_margin_calculation(self, arguments):
+        """
+        Updates calculation method based on user decision
+        
+        @param arguments: List of arguments (1st argument: value to process)
+        @return: Returns the processed value
+        """
+        
+        MARGIN_CALCULATION_TYPE_PERCENTAGE = 0
+        MARGIN_CALCULATION_TYPE_VALUE = 1   
+        
+        #get internal structure
+        internal_structure = arguments[0].internal_structure
+        
+        margin_calculation_type = arguments[0].margin_calculation_type
+        
+        # @todo: SCERIO: corrigir isto, colocando num handler após linha
+        #creates, if not existent, the entity that represents merchandise/organization relation
+        entity = internal_structure.add_entity("merchandise_contactable_organizational_hierarchy_tree_node")
+        entity_id = entity._id
+        
+        internal_structure.add_field("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin")
+        internal_structure.add_field("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin_type")
+            
+        if margin_calculation_type == MARGIN_CALCULATION_TYPE_VALUE:
+            internal_structure.set_field_value("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin", "69")
+            internal_structure.set_field_value("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin_type", "96")
+        elif margin_calculation_type == MARGIN_CALCULATION_TYPE_PERCENTAGE:
+            internal_structure.set_field_value("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin", "00")
+            internal_structure.set_field_value("merchandise_contactable_organizational_hierarchy_tree_node", entity_id, "margin_type", "77")
+        
