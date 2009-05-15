99dcd9615a31253d63046573c6c96d5dfbdb6a7f
Tiago Silva <tsilva@hive.pt>
2009-05-15 07:37:29 +0000

    removed obsolete data converter projects
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@3095 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.data_converter/src/colony/plugins/data_converter/patches/obsolete/set_card_payment_card_type.sql
+++
@@ -1,9 +0,0 @@
--- copies the name attribute to the card type attribute in the card payment table
-
--- up
-UPDATE CardPayment
-   SET card_type = name;
-
--- down
-UPDATE CardPayment
-   SET card_type = NULL;
