bde363d4b4e7d8b0db1029143246ecf714e6a869
Tiago Silva <tsilva@hive.pt>
2009-05-21 13:38:42 +0000

    added new entity manager intermediate structure i/o adapter
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@3165 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.data_converter/src/colony/plugins/data_converter_intermediate_structure_io_adapter_entity_manager_plugin.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Tiago Silva <tsilva@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 1805 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-03-10 08:56:01 +0000 (Tue, 10 Mar 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.plugins.plugin_system
+
+class DataConverterIntermediateStructureIoAdapterEntityManagerPlugin(colony.plugins.plugin_system.Plugin):
+    """
+    Provides a means to load and save the intermediate structure by using the colony business entity manager.
+    """
+
+    id = "pt.hive.colony.plugins.data_converter.intermediate_structure.io_adapter.entity_manager"
+    name = "Data Converter Intermediate Structure Input Output Adapter Entity Manager plugin"
+    short_name = "Data Converter Intermediate Structure Input Output Adapter Entity Manager"
+    description = "Provides a means to load and save the intermediate structure by using the colony business entity manager"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
+    capabilities = ["data_converter_intermediate_structure_io_adapter.entity_manager"]
+    capabilities_allowed = []
+    dependencies = [colony.plugins.plugin_system.PluginDependency(
+                    "pt.hive.colony.plugins.business.entity_manager", "1.0.0"),
+                    colony.plugins.plugin_system.PluginDependency(
+                    "pt.hive.colony.plugins.main.log", "1.0.0")]
+    events_handled = []
+    events_registrable = []
+
+    io_adapter_entity_manager = None
+    """ The intermediate structure entity manager input output adapter """
+
+    entity_manager_plugin = None
+    """ Business entity manager plugin """
+
+    logger_plugin = None
+    """ Logger plugin """
+
+    def __init__(self, manager):
+        colony.plugins.plugin_system.Plugin.__init__(self, manager)
+
+    def load_plugin(self):
+        colony.plugins.plugin_system.Plugin.load_plugin(self)
+        global data_converter_intermediate_structure_io_adapter_entity_manager
+        import data_converter_intermediate_structure_io_adapter_entity_manager.io_adapter_entity_manager.io_adapter_entity_manager_system
+        self.io_adapter_entity_manager = data_converter_intermediate_structure_io_adapter_entity_manager.io_adapter_entity_manager.io_adapter_entity_manager_system.IoAdapterEntityManager(self)
+
+    def end_load_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.unload_plugin(self)
+        self.io_adapter_entity_manager = None
+        self.entity_manager_plugin = None
+        self.logger_plugin = None
+
+    def end_unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_unload_plugin(self)
+
+    @colony.plugins.decorators.load_allowed("pt.hive.colony.plugins.data_converter.intermediate_structure.io_adapter.entity_manager", "1.0.0")
+    def load_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    @colony.plugins.decorators.unload_allowed("pt.hive.colony.plugins.data_converter.intermediate_structure.io_adapter.entity_manager", "1.0.0")
+    def unload_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.plugins.decorators.inject_dependencies("pt.hive.colony.plugins.data_converter.intermediate_structure.io_adapter.entity_manager", "1.0.0")
+    def dependency_injected(self, plugin):
+        colony.plugins.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def load(self, intermediate_structure, options):
+        """
+        Populates the intermediate structure with data retrieved from the entity manager source specified in the options.
+
+        @type intermediate_structure: IntermediateStructure
+        @param intermediate_structure: Intermediate structure where to load the data into.
+        @type options: Dictionary
+        @param options: Options used to determine how to load data into the provided intermediate structure.
+        """
+
+        return self.io_adapter_entity_manager.load(intermediate_structure, options)
+
+    def save(self, intermediate_structure, options):
+        """
+        Saves the intermediate structure with the entity manager at the location and with characteristics defined in the options.
+
+        @type intermediate_structure: IntermediateStructure
+        @param intermediate_structure: Intermediate structure one wants to save.
+        @type options: Dictionary
+        @param options: Options used to determine how to save the intermediate structure with the entity manager.
+        """
+
+        return self.io_adapter_entity_manager.save(intermediate_structure, options)
+
+    def get_entity_manager_plugin(self):
+        return self.entity_manager_plugin
+
+    @colony.plugins.decorators.plugin_inject("pt.hive.colony.plugins.business.entity_manager")
+    def set_entity_manager_plugin(self, entity_manager_plugin):
+        self.entity_manager_plugin = entity_manager_plugin
+
+    def get_logger_plugin(self):
+        return self.logger_plugin
+
+    @colony.plugins.decorators.plugin_inject("pt.hive.colony.plugins.main.log")
+    def set_logger_plugin(self, logger_plugin):
+        self.logger_plugin = logger_plugin
