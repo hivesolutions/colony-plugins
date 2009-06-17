58fbc3a54587e8340790aabdae6084767650648f
João Magalhães <joamag@hive.pt>
2009-06-17 11:39:44 +0000

    new dummy swing plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@3380 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.dummy/src/colony/plugins/dummy/swing/dummy_swing_system.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 1089 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-01-22 23:19:39 +0000 (qui, 22 Jan 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import javax.swing
+
+class DummySwing:
+    """
+    The dummy swing class.
+    """
+
+    dummmy_swing_plugin = None
+    """ The dummy swing plugin """
+
+    def __init__(self, dummmy_swing_plugin):
+        """
+        Constructor of the class
+
+        @type dummmy_swing_plugin: DummySwingPlugin
+        @param dummmy_swing_plugin: The dummy swing plugin.
+        """
+
+        self.dummmy_swing_plugin = dummmy_swing_plugin
+
+    def start(self):
+        frame = javax.swing.JFrame("Hello Jython")
+        label = javax.swing.JLabel("Hello Jython!", javax.swing.JLabel.CENTER)
+        frame.add(label)
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE)
+        frame.setSize(300, 300)
+        frame.show()
---
+++ pt.hive.colony.plugins.dummy/src/colony/plugins/dummy_plugin_swing.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2311 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-01 17:25:30 +0100 (qua, 01 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import dummy_plugin
+import colony.plugins.plugin_system
+
+class DummyPluginSwind(dummy_plugin.DummyPlugin):
+    """
+    The main class for the Dummy Swing plugin.
+    """
+
+    id = "pt.hive.colony.plugins.dummy.swing"
+    name = "Dummy Plugin Swing"
+    short_name = "Dummy Swing"
+    description = "Dummy Swing Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.plugins.plugin_system.JYTHON_ENVIRONMENT]
+    capabilities = ["startup", "dummy_swing"]
+    capabilities_allowed = []
+    dependencies = []
+    events_handled = []
+    events_registrable = []
+    valid = True
+
+    dummy_swing = None
+
+    def load_plugin(self):
+        dummy_plugin.DummyPlugin.load_plugin(self)
+        print "loading dummy swing..."
+
+    def end_load_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_load_plugin(self)
+        global dummy
+        import dummy.swing.dummy_swing_system
+        self.dummy_swing = dummy.swing.dummy_swing_system.DummySwing(self)
+        self.dummy_swing.start()
+
+    def unload_plugin(self):
+        dummy_plugin.DummyPlugin.unload_plugin(self)
+        print "unloading dummy swing..."
+        self.dummy_swing.stop()
+
+    def end_unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        dummy_plugin.DummyPlugin.load_allowed(self, plugin, capability)
+        print "loading dummy windows forms allowed..."
+
+    def unload_allowed(self, plugin, capability):
+        dummy_plugin.DummyPlugin.unload_allowed(self, plugin, capability)
+        print "unloading dummy windows forms allowed..."
+
+    def dependency_injected(self, plugin):
+        dummy_plugin.DummyPlugin.dependency_injected(self, plugin)
