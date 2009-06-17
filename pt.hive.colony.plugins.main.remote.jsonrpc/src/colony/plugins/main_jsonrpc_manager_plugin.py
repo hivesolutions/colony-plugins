00fafb8c872053e5f49864eea1f5f585758ece45
João Magalhães <joamag@hive.pt>
2009-06-17 11:18:00 +0000

    new jython compatability added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@3379 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_jsonrpc_manager_plugin.py
+++ pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_jsonrpc_manager_plugin.py
@@ -52,7 +52,8 @@ class MainJsonrpcManagerPlugin(colony.plugins.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
+    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT,
+                 colony.plugins.plugin_system.JYTHON_ENVIRONMENT]
     capabilities = ["jsonrpc_manager", "http_python_handler", "rpc_handler"]
     capabilities_allowed = ["rpc_service"]
     dependencies = []
