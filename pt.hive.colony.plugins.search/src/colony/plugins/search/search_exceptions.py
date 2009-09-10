9c353ab3fdb69a17447fa1c5fb27119a7b30e421
Luís Martinho <lmartinho@hive.pt>
2009-09-10 14:00:57 +0000

    added missing exception in search plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@4698 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.search/src/colony/plugins/search/search_exceptions.py
+++ pt.hive.colony.plugins.search/src/colony/plugins/search/search_exceptions.py
@@ -69,3 +69,29 @@ class MissingProperty(SearchException):
         """
 
         return "Missing property: %s" % self.message
+
+class InvalidFunctionRequested(SearchException):
+    """
+    The invalid function request class.
+    """
+
+    def __init__(self, message):
+        """
+        Constructor of the class.
+
+        @type message: String
+        @param message: The message to be printed.
+        """
+
+        SearchException.__init__(self)
+        self.message = message
+
+    def __str__(self):
+        """
+        Returns the string representation of the class.
+
+        @rtype: String
+        @return: The string representation of the class.
+        """
+
+        return "Invalid function requested: %s" % self.message
