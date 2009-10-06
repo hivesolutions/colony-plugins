c399291270193788d64c93d43f1741b3eea16761
João Magalhães <joamag@hive.pt>
2009-10-06 07:24:07 +0000

    new liac example
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@5299 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.language.generator/src/colony/language/liac_example.py
+++ pt.hive.colony.language.generator/src/colony/language/liac_example.py
@@ -3,7 +3,7 @@ from yappy.parser import *
 class ParseReg(Yappy):
      def __init__(self, no_table=0, table='tablereg'):
         grammar ="""
-        E -> E + B {{ self.OrSemRule }} ;
+        E -> E + E {{ self.OrSemRule }} ;
         E -> B {{ self.OrSemRule }} ;
         B -> 1 {{ self.OrSemRule }} ;
         B -> 0 {{ self.OrSemRule }} ;
