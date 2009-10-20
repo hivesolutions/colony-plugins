a2407ecf2359838bb9dacf1120197d9cba1fbbd6
João Magalhães <joamag@hive.pt>
2009-10-20 11:27:12 +0000

    new http file decoding added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@5673 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
+++ pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
@@ -709,8 +709,8 @@ class HttpClientServiceTask:
         # retrieves the received message value
         received_message_value = request.received_message
 
-        # re-encodes the message value in the current default encoding
-        request.received_message = received_message_value.decode(content_type_charset).encode()
+        # decodes the message value into unicode using the given charset
+        request.received_message = received_message_value.decode(content_type_charset)
 
     def retrieve_data(self, request_timeout = REQUEST_TIMEOUT, chunk_size = CHUNK_SIZE):
         try:
