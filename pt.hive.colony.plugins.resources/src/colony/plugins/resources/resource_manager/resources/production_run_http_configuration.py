8c27c39f3395a34a9cb8caf105b9b69a7275d5f4
Luís Martinho <lmartinho@hive.pt>
2009-10-22 11:03:50 +0000

    created production run for the http server
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@5755 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/production_run_http_configuration.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt> & Luís Martinho <lmartinho@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2341 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-01 17:42:37 +0100 (qua, 01 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+configuration = {
+                 "default_socket_provider" : "normal",
+                 "default_port" : 80,
+                 "default_handler" : "file",
+                 "default_encoding" : None,
+                 "default_content_type_charset" : "utf-8",
+                 "contexts" : {
+                     "/colony_web" : {
+                         "handler" : "file",
+                         "request_properties" : {
+                             "base_path" : "$resource{system.path.colony_web}/pt.hive.colony.web",
+                             "default_page" : "index.html"
+                         }
+                     },
+                     "/colony_web/plugins" : {
+                         "handler" : "colony",
+                         "request_properties" : {
+                             "plugin_handler" : "pt.hive.colony.plugins.javascript.file_handler"
+                         }
+                     },
+                     "/colony_manager" : {
+                         "handler" : "colony",
+                         "request_properties" : {}
+                     },
+                     "/colony_mod_python" : {
+                         "handler" : "colony",
+                         "request_properties" : {}
+                     },
+                     "/eclipse" : {
+                         "handler" : "file",
+                         "request_properties" : {
+                             "base_path" : "C:\Users\joamag\workspace\pt.hive.eclipse.plugins.site",
+                             "default_page" : "site.xml"
+                         }
+                     }
+                 }
+             }
