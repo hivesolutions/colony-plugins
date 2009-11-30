c0ae05d91cc2a00cdc8c528646063edf2ead4020
João Magalhães <joamag@hive.pt>
2009-11-30 14:41:15 +0000

    initial version of the mock system
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@6610 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.mock/src/colony/plugins/main_mock/mock/main_mock_system.py
+++ pt.hive.colony.plugins.main.mock/src/colony/plugins/main_mock/mock/main_mock_system.py
@@ -54,3 +54,200 @@ class MainMock:
         """
 
         self.main_mock_plugin = main_mock_plugin
+
+    def generate_mockery(self):
+        return None
+
+    def generate_mock(self, class_reference):
+        pass
+
+class Mockery:
+    """
+    The mockery class used to manage mocks.
+    """
+
+    def __init__(self):
+        pass
+
+    def checking(self):
+        pass
+
+class Expectations:
+    """
+    The expectations class used to control mock expectations.
+    """
+
+    expectations_list = []
+    """ The expectation list """
+
+    def __init__(self):
+        self.expectations_list = []
+
+    def add_expectation(self, expectation):
+        self.expectations_list.append(expectation)
+
+    def remove_exception(self, exception):
+        self.expectations_list.remove(expectation)
+
+class Expectation:
+    """
+    The expectation class used to control mock expectation.
+    """
+
+    def __init__(self):
+        pass
+
+    def verify_expectation(self, mock):
+        """
+        Verifies the given expectation for the given mock.
+
+        @type mock: Mock
+        @param mock: The mock object to verify the expectation.
+        @rtype: bool
+        @return: The result of the verification.
+        """
+
+        return True
+
+class ParametersExpectation(Expectation):
+    """
+    The parameters expectation class.
+    """
+
+    def __init__(self):
+        Expectation.__init__(self)
+
+    def verify_expectation(self, mock):
+        # calls the super object retrieving the result
+        result = Expectation.verify_expectation(self, mock)
+
+        # in case the result id false
+        if not result:
+            # returns false immediately
+            return False
+
+        pass
+
+class ParametersExpectation(Expectation):
+    """
+    The parameters expectation class.
+    """
+
+    methodNamesList = []
+    """ The method names list """
+
+    def __init__(self, methodNamesList):
+        Expectation.__init__(self)
+        self.methodNamesList = methodNamesList
+
+class ParameterExpectation(Expectation):
+    """
+    The parameter expectation class.
+    """
+
+    def __init__(self):
+        Expectation.__init__(self)
+
+class Mock:
+    """
+    The mock class used to encapsulate mock objects.
+    """
+
+    mock_name = None
+    """ The mock name """
+
+    def __init__(self, name, returns = None, returns_iter = None, returns_func = None, raises = None):
+        self.mock_name = name
+        self.mock_returns = returns
+        if returns_iter is not None:
+            returns_iter = iter(returns_iter)
+        self.mock_returns_iter = returns_iter
+        self.mock_returns_func = returns_func
+        self.mock_raises = raises
+        self.mock_attrs = {}
+
+    def __repr__(self):
+        return "<Mock '%s' '%s'>" % (hex(id(self)), self.mock_name)
+
+    def __call__(self, *args, **kw):
+        # @todo
+        # tenho de manter aki o registo de tudo o que se passa em termos de chamada
+        # tenho de poder fazer try catch se necessario
+
+        # retrieves the various parts of the
+        parts = [repr(a) for a in args]
+
+        # extends the parts fot the given kword items
+        parts.extend("%s=%r" % (items) for items in sorted(kw.items()))
+
+        # creates the message
+        message = "Called %s(%s)" % (self.mock_name, ', '.join(parts))
+
+        # prints the message
+        print message
+
+        # returns the mock return
+        return self._mock_return(*args, **kw)
+
+    def _mock_return(self, *args, **kw):
+        if self.mock_raises is not None:
+            raise self.mock_raises
+        elif self.mock_returns is not None:
+            return self.mock_returns
+        elif self.mock_returns_iter is not None:
+            try:
+                return self.mock_returns_iter.next()
+            except StopIteration:
+                raise Exception("No more mock return values are present.")
+        elif self.mock_returns_func is not None:
+            return self.mock_returns_func(*args, **kw)
+        else:
+            return None
+
+    def __getattr__(self, attr):
+        if attr not in self.mock_attrs:
+            if self.mock_name:
+                new_name = self.mock_name + '.' + attr
+            else:
+                new_name = attr
+            self.mock_attrs[attr] = Mock(new_name)
+        return self.mock_attrs[attr]
+
+class MockCall:
+    """
+    Class that represents a mock call.
+    """
+
+    method_name = None
+    """ The method name """
+
+    method_arguments = []
+    """ The method arguments """
+
+    methid_key_arguments = {}
+    """ The method key arguments """
+
+    def __init__(self, method_name, method_arguments, method_key_arguments):
+        self.method_name = method_name
+        self.method_arguments = method_arguments
+        self.methid_key_arguments = method_key_arguments
+
+    def set_method_name(self, method_name):
+        """
+        Sets the method name.
+
+        @type method_name: String
+        @param method_name: The method name.
+        """
+
+        self.method_name = method_name
+
+    def get_method_name(self):
+        """
+        Retrieves the method name.
+
+        @rtype: String
+        @return: The method name.
+        """
+
+        return self.method_name
