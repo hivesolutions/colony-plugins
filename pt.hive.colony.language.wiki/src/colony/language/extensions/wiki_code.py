63d7d757d7a30e810371f480c04850e5257d66e8
João Magalhães <joamag@hive.pt>
2009-12-07 07:39:23 +0000

    tiny code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@6690 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.language.wiki/src/colony/language/extensions/wiki_code.py
+++ pt.hive.colony.language.wiki/src/colony/language/extensions/wiki_code.py
@@ -39,7 +39,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import ply
 
-from python_lexer import *
+from settler_lexer import *
 
 CLASS_DEFINITION = {"CLASS" : "kw4",
                     "FUNCTION" : "kw4",
