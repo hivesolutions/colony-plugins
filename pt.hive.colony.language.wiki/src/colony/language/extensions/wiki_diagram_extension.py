77aa3ef209d7625c48754ae8ab77bacabfa5d9bb
Luís Martinho <lmartinho@hive.pt>
2009-12-16 11:15:36 +0000

    added support for diagrams in wiki
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@6954 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.language.wiki/src/colony/language/extensions/wiki_diagram_extension.py
+++ pt.hive.colony.language.wiki/src/colony/language/extensions/wiki_diagram_extension.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = "João Magalhães <joamag@hive.pt>"
+__author__ = "João Magalhães <joamag@hive.pt> & Luís Martinho <lmartinho@hive.pt>"
 """ The author(s) of the module """
 
 __version__ = "1.0.0"
@@ -40,6 +40,8 @@ __license__ = "GNU General Public License (GPL), Version 3"
 import libs.string_buffer_util
 
 import wiki_extension_system
+
+import wiki_diagram.wiki_diagram_extension_system
 
 GENERATOR_TYPE = "diagram"
 """ The generator type """
@@ -76,6 +78,22 @@ class WikiDiagramExtension(wiki_extension_system.WikiExtension):
     extension_manager = None
     """ The extension manager """
 
+    def __init__(self, manager = None):
+        """
+        Constructor of the class.
+
+        @type manager: ExtensionManager
+        @param manager: The parent extension manager.
+        """
+
+        wiki_extension_system.WikiExtension.__init__(self, manager)
+
+        # creates a new extension manager
+        self.extension_manager = libs.extension_system.ExtensionManager(["./extensions/wiki_diagram/extensions"])
+        self.extension_manager.set_extension_class(wiki_diagram.wiki_diagram_extension_system.WikiDiagramExtension)
+        self.extension_manager.start_logger()
+        self.extension_manager.load_system()
+
     def get_generator_type(self):
         """
         Retrieves the generator type.
@@ -101,20 +119,76 @@ class WikiDiagramExtension(wiki_extension_system.WikiExtension):
         # retrieves the tag contents
         contents = tag_node.contents
 
-        # retrieves the tag attributes map
-        attributes_map = tag_node.attributes_map
-
-        title = attributes_map.get("title", "none")
-
         # creates the string buffer
         string_buffer = libs.string_buffer_util.StringBuffer()
 
+        # retrieves the code highlighting extensions
+        diagram_extensions = self.extension_manager.get_extensions_by_capability("diagram")
+
+        # retrieves the tag name
+        node_tag_name = tag_node.tag_name
+
+        # retrieves the tag attributes map
+        node_attributes_map = tag_node.attributes_map
+
+        # retrieves the tag attributes map keys
+        node_attributes_map_keys = node_attributes_map.keys()
+
+        # retrieves the type
+        node_tag_type = node_attributes_map.get("type", "none")
+
+        # in case the diagram type is not specified
+        if not node_tag_type:
+            # raisers the invalid tag name exception
+            raise wiki_exceptions.InvalidTagName("tag name is not valid: " + node_tag_name)
+
+        # retrieves the title
+        node_tag_title = node_attributes_map.get("title", "none")
+
+        # retrieves the diagram extensions for the given tag
+        node_tag_diagram_extensions = [extension for extension in diagram_extensions if extension.get_diagram_type() == node_tag_type]
+
         # writes the start div diagram tag
-        string_buffer.write("<div class=\"diagram\">")
+        string_buffer.write("<div class=\"diagram\" style=\"display: block;\">")
 
-        string_buffer.write("<h1>" + title + "</h1>")
+        # writes the title
+        string_buffer.write("<h1>" + node_tag_title + "</h1>")
 
-        string_buffer.write(contents)
+        # generates the diagrams using the available extensions
+        for node_tag_diagram_extension in node_tag_diagram_extensions:
+            #string_buffer.write(node_tag_diagram_extension.generate_diagram(contents))
+
+            # retrieves the tokens list
+            graphics_elements = node_tag_diagram_extension.get_graphics_elements(contents)
+
+            # creates the vector graphics support
+            vector_graphics = ScalableVectorGraphics()
+
+            # sets the visitor in the vector graphics
+            vector_graphics.set_visitor(visitor)
+
+            # starts the graphics
+            open_graphics_string = vector_graphics.open_graphics({"width" : 600, "height" : 300})
+
+            # writes the open graphics tag
+            string_buffer.write(open_graphics_string)
+
+            # for each graphics element
+            for graphics_element in graphics_elements:
+                # retrieves the graphics element type and attributes
+                graphics_element_type, graphics_element_attributes = graphics_element
+
+                # creates the graphics for the graphics element
+                graphics_element_string = vector_graphics.generate_element(graphics_element_type, graphics_element_attributes)
+
+                # adds the graphics element string to the string buffer
+                string_buffer.write(graphics_element_string)
+
+            # stops the graphics
+            close_graphics_string = vector_graphics.close_graphics()
+
+            # writes the close graphics tag
+            string_buffer.write(close_graphics_string)
 
         # writes the end div diagram tag
         string_buffer.write("</div>")
@@ -124,3 +198,106 @@ class WikiDiagramExtension(wiki_extension_system.WikiExtension):
 
         # returns the string value
         return string_value
+
+class AbstractVectorGraphics:
+    def __init__(self):
+        pass
+
+    def open_graphics(self):
+        """
+        Opens the graphics.
+        """
+
+        pass
+
+    def close_graphics(self):
+        """
+        Closes the graphics.
+        """
+
+        pass
+
+    def get_visitor(self):
+        pass
+
+    def set_visitor(self, visitor):
+        pass
+
+class ScalableVectorGraphics(AbstractVectorGraphics):
+    def __init__(self):
+        self.graphics_open = False
+        self.visitor = None
+
+    def open_graphics(self, graphics_attributes):
+        """
+        Opens the graphics.
+        """
+
+        # retrieves the width attribute
+        width = graphics_attributes["width"]
+
+        # retrieves the height attribute
+        height = graphics_attributes["height"]
+
+        if self.graphics_open:
+            return
+
+        # creates the string value for the open graphics
+        string_value = "<svg:svg version=\"1.1\" baseProfile=\"full\" width=\"{0}px\" height=\"{1}px\">".format(width, height)
+
+        # signals the graphics tag is open
+        self.graphics_open = True
+
+        return string_value
+
+    def close_graphics(self):
+        """
+        Closes the graphics.
+        """
+
+        if not self.graphics_open:
+            return
+
+        # creates the string value for the close graphics
+        string_value = "</svg:svg>"
+
+        # signals the graphics tag is closed
+        self.graphics_open = False
+
+        return string_value
+
+    def generate_element(self, graphics_element_type, graphics_element_attributes):
+        if graphics_element_type == "rectangle":
+            return self.generate_rectangle(graphics_element_attributes)
+        elif graphics_element_type == "text":
+            return self.generate_text(graphics_element_attributes)
+
+    def generate_rectangle(self, graphics_attributes):
+        x = graphics_attributes["x"]
+        y = graphics_attributes["y"]
+        width = graphics_attributes["width"]
+        height = graphics_attributes["height"]
+        options = graphics_attributes["options"]
+
+        # @todo: build the element string gradually
+        rectangle_element_string = "<svg:rect x=\"{0}%\" y=\"{1}%\" width=\"{2}%\" height=\"{3}%\" fill=\"{4}\" stroke=\"{5}\" stroke-width=\"{6}\"/>".format(x, y, width, height, options.get("fill", ""), options.get("stroke", ""), options.get("stroke_width", ""))
+
+        return rectangle_element_string
+
+    def generate_text(self, graphics_attributes):
+        x = graphics_attributes["x"]
+        y = graphics_attributes["y"]
+        text = graphics_attributes["text"]
+        escaped_text = self.visitor.escape_string_value(text)
+        options = graphics_attributes["options"]
+
+        # @todo: build the element string gradually
+        text_element_string = "<svg:text x=\"{0}%\" y=\"{1}%\" font-family=\"{2}\" font-size=\"{3}\" fill=\"{4}\" text-anchor=\"{5}\">{6}</svg:text>".format(x, y, options.get("font_family", ""), options.get("font_size", ""), options.get("fill", ""), options.get("text_anchor", ""), escaped_text)
+
+        return text_element_string
+
+    def get_visitor(self):
+        return self.visitor
+
+    def set_visitor(self, visitor):
+        self.visitor = visitor
