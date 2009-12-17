fa8a553d41a07e1fcf69f3aade8175b078e654d8
João Magalhães <joamag@hive.pt>
2009-12-17 06:37:52 +0000

    new wiki generator structure
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7008 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.language.wiki/src/colony/language/wiki_generator.py
+++ pt.hive.colony.language.wiki/src/colony/language/wiki_generator.py
@@ -52,6 +52,156 @@ USAGE_MESSAGE = "wiki_generator --file=file_path [--target=target_path] [--gener
 DEFAULT_TARGET_PATH = "generated"
 """ The default target path """
 
+class WikiGenerator:
+    """
+    The wiki generator class.
+    """
+
+    log_level = None
+    """ The log leve """
+
+    extension_manager = None
+    """ The extension manager """
+
+    generation_engine = None
+    """ The generation engine """
+
+    generation_properties = {}
+    """ The generation properties """
+
+    def __init__(self):
+        """
+        Constructor of the class.
+        """
+
+        self.generation_properties = {}
+
+    def start_logger(self, verbose = False, debug = False):
+        """
+        Starts the logger system.
+
+        @type verbose: bool
+        @param verbose: If the log is going to be of type verbose.
+        @type debug: bool
+        @param debug: If the log is going to be of type debug.
+        """
+
+        # in case debug is set
+        if debug:
+            # sets the log level to debug
+            self.log_level = logging.DEBUG
+        # in case debug is verbose
+        elif verbose:
+            # sets the log level to info
+            self.log_level = logging.INFO
+        # otherwise
+        else:
+            # sets the log level to warn
+            self.log_level = logging.WARN
+
+    def start_extension_manager(self):
+        """
+        Starts the extension manager in the wiki generator.
+        """
+
+        # creates a new extension manager
+        self.extension_manager = libs.extension_system.ExtensionManager(["./extensions"])
+        self.extension_manager.set_extension_class(wiki_extension_system.WikiExtension)
+        self.extension_manager.start_logger(self.log_level)
+        self.extension_manager.load_system()
+
+    def process(self):
+        """
+        Processes the wiki generator task.
+        """
+
+        # retrieves the extension manager
+        extension_manager = self._get_extension_manager()
+
+        # retrieves the generation extensions
+        generation_extensions = extension_manager.get_extensions_by_capability("generation")
+
+        # in case generation engine is defined
+        if self.generation_engine:
+            # filters the generation extensions to retrieve just the demanded ones
+            generation_extensions = [generation_extension for generation_extension in generation_extensions if generation_extension.get_generation_type() == self.generation_engine]
+
+        # retrieves the start time
+        start_time = time.time()
+
+        # iterates over all the generation extensions
+        for generation_extension in generation_extensions:
+            # generates the wiki for the given properties
+            generation_extension.generate_wiki(self.generation_properties)
+
+        # retrieves the end time
+        end_time = time.time()
+
+        # calculates the time difference
+        time_difference = end_time - start_time
+
+        # rounds the time difference
+        time_difference_rounded = round(time_difference, 2)
+
+        # prints an info message
+        extension_manager.logger.info("Processing took: %s seconds" % time_difference_rounded)
+
+    def get_generation_engine(self):
+        """
+        Retrieves the generation engine.
+
+        @rtype: String
+        @return: The generation engine.
+        """
+
+        return self.generation_engine
+
+    def set_generation_engine(self, generation_engine):
+        """
+        Sets the generation engine.
+
+        @type generation_engine: String
+        @param generation_engine: The generation engine.
+        """
+
+        self.generation_engine = generation_engine
+
+    def get_generation_properties(self):
+        """
+        Retrieves the generation properties.
+
+        @rtype: Dictionary
+        @return: The generation properties.
+        """
+
+        return self.generation_properties
+
+    def set_generation_properties(self, generation_properties):
+        """
+        Sets the generation properties.
+
+        @type generation_properties: Dictionary
+        @param generation_properties: The generation properties.
+        """
+
+        self.generation_properties = generation_properties
+
+    def _get_extension_manager(self):
+        """
+        Retrieves the extension manager, starting it if necessary.
+
+        @rtype: ExtensionManager
+        @return: The extension manger.
+        """
+
+        # in case the extension manager is not defined
+        if not self.extension_manager:
+            # starts the extension manager
+            self.start_extension_manager()
+
+        # returns the extension manager
+        return self.extension_manager
+
 if __name__ == "__main__":
     # starts the verbose flag as false
     verbose = False
@@ -90,46 +240,20 @@ if __name__ == "__main__":
         print "Usage: " + USAGE_MESSAGE
         sys.exit(2)
 
-    if debug:
-        log_level = logging.DEBUG
-    elif verbose:
-        log_level = logging.INFO
-    else:
-        log_level = logging.WARN
-
-    # creates a new extension manager
-    extension_manager = libs.extension_system.ExtensionManager(["./extensions"])
-    extension_manager.set_extension_class(wiki_extension_system.WikiExtension)
-    extension_manager.start_logger(log_level)
-    extension_manager.load_system()
-
-    # retrieves the generation extensions
-    generation_extensions = extension_manager.get_extensions_by_capability("generation")
-
-    # in case generation is defined
-    if generation:
-        # filters the generation extensions to retrieve just the demanded ones
-        generation_extensions = [generation_extension for generation_extension in generation_extensions if generation_extension.get_generation_type() == generation]
-
     # creates the properties map
     properties = {"file_path" : file_path, "target_path" : target_path}
 
-    # retrieves the start time
-    start_time = time.time()
+    # creates a new wiki generator
+    wiki_generator = WikiGenerator()
 
-    # iterates over all the generation extensions
-    for generation_extension in generation_extensions:
-        # generates the wiki for the given properties
-        generation_extension.generate_wiki(properties)
+    # starts the logger in the wiki generator
+    wiki_generator.start_logger(verbose, debug)
 
-    # retrieves the end time
-    end_time = time.time()
+    # sets the generation engine in the wiki generator
+    wiki_generator.set_generation_engine(generation)
 
-    # calculates the time difference
-    time_difference = end_time - start_time
+    # sets the generation properties in the wiki generator
+    wiki_generator.set_generation_properties(properties)
 
-    # rounds the time difference
-    time_difference_rounded = round(time_difference, 2)
-
-    # prints an info message
-    extension_manager.logger.info("Processing took: %s seconds" % time_difference_rounded)
+    # processes the wiki generator
+    wiki_generator.process()
