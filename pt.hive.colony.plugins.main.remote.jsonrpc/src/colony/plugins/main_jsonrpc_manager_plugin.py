1c75f37759c67aeaa3bb2efeb24987d60836c683
João Magalhães <joamag@hive.pt>
2009-12-20 11:11:35 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7093 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_jsonrpc_manager_plugin.py
+++ pt.hive.colony.plugins.main.remote.jsonrpc/src/colony/plugins/main_jsonrpc_manager_plugin.py
@@ -42,7 +42,7 @@ import colony.plugins.decorators
 
 class MainJsonrpcManagerPlugin(colony.plugins.plugin_system.Plugin):
     """
-    The main class for the Jsonrpc Manager Main plugin
+    The main class for the Jsonrpc Manager Main plugin.
     """
 
     id = "pt.hive.colony.plugins.main.remote.jsonrpc.manager"
