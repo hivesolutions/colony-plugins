b2af6a46237c647a6927b6b4dc44e1af223cbaa0
João Magalhães <joamag@hive.pt>
2009-12-20 13:06:47 +0000

    minor code fix
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7106 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.rest/src/colony/plugins/main_remote_rest/manager/main_rest_manager_system.py
+++ pt.hive.colony.plugins.main.remote.rest/src/colony/plugins/main_remote_rest/manager/main_rest_manager_system.py
@@ -37,9 +37,7 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import Pyro.core
-
-import main_pyro_manager_exceptions
+import main_rest_manager_exceptions
 
 HANDLER_NAME = "rest"
 """ The handler name """
