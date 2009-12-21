d2039a330b151ee9052bc5ed04c099f08cfd5586
João Magalhães <joamag@hive.pt>
2009-12-21 12:22:44 +0000

    new iron python support added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7137 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.threads/src/colony/plugins/thread_pool_manager_plugin.py
+++ pt.hive.colony.plugins.main.threads/src/colony/plugins/thread_pool_manager_plugin.py
@@ -52,7 +52,8 @@ class ThreadPoolManagerPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT,
-                 colony.plugins.plugin_system.JYTHON_ENVIRONMENT]
+                 colony.plugins.plugin_system.JYTHON_ENVIRONMENT,
+                 colony.plugins.plugin_system.IRON_PYTHON_ENVIRONMENT]
     capabilities = ["thread_pool_manager"]
     capabilities_allowed = []
     dependencies = []
