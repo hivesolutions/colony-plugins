b5756220ebbe37263ee5f131b4379f93b8254f33
João Magalhães <joamag@hive.pt>
2009-12-21 12:21:33 +0000

    new iron python support added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7133 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service/src/colony/plugins/main_service_normal_socket_provider_plugin.py
+++ pt.hive.colony.plugins.main.service/src/colony/plugins/main_service_normal_socket_provider_plugin.py
@@ -52,7 +52,8 @@ class MainServiceNormalSocketProviderPlugin(colony.plugins.plugin_system.Plugin)
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT,
-                 colony.plugins.plugin_system.JYTHON_ENVIRONMENT]
+                 colony.plugins.plugin_system.JYTHON_ENVIRONMENT,
+                 colony.plugins.plugin_system.IRON_PYTHON_ENVIRONMENT]
     capabilities = ["socket_provider"]
     capabilities_allowed = []
     dependencies = []
