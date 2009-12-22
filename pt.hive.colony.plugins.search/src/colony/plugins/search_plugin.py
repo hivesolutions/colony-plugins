c61e6662b60b0a1f568dc335028bd91e2cb2042e
João Magalhães <joamag@hive.pt>
2009-12-22 05:56:24 +0000

    minor code fix
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7165 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.search/src/colony/plugins/search_plugin.py
+++ pt.hive.colony.plugins.search/src/colony/plugins/search_plugin.py
@@ -37,8 +37,8 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import colony.plugins.decorators
 import colony.plugins.plugin_system
+import colony.plugins.decorators
 
 class SearchPlugin(colony.plugins.plugin_system.Plugin):
     """
