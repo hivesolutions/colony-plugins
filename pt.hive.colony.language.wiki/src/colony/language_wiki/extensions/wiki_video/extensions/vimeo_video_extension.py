7f560acb7dcd6150662b5cde94b5fa0ca5c5722b
João Magalhães <joamag@hive.pt>
2010-03-03 17:12:18 +0000

    minimal code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7458 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.language.wiki/src/colony/language_wiki/extensions/wiki_video/extensions/vimeo_video_extension.py
+++ pt.hive.colony.language.wiki/src/colony/language_wiki/extensions/wiki_video/extensions/vimeo_video_extension.py
@@ -37,8 +37,6 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import sys
-
 import language_wiki.libs.url_parser
 
 import wiki_video.wiki_video_extension_system
