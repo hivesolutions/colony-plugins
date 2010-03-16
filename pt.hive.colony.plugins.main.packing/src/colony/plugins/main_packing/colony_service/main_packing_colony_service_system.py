a68ba759d43c0a3ee3338f85751db81f17f354ab
João Magalhães <joamag@hive.pt>
2010-03-16 14:18:05 +0000

    new support for plugin description and packing
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7574 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.packing/src/colony/plugins/main_packing/colony_service/main_packing_colony_service_system.py
+++ pt.hive.colony.plugins.main.packing/src/colony/plugins/main_packing/colony_service/main_packing_colony_service_system.py
@@ -38,6 +38,8 @@ __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
 import re
+import zipfile
+import tarfile
 
 import os.path
 
@@ -53,8 +55,35 @@ VERSION_VALUE = "version"
 RESOURCES_VALUE = "resources"
 """ The resources value """
 
-JSON_PLUGIN_REGEX = ".*plugin.json$"
+SPECIFICATION_VALUE = "specification"
+""" The specification value """
+
+RECURSIVE_VALUE = "recursive"
+""" The recursive value """
+
+PLUGIN_REGEX_VALUE = "plugin_regex"
+""" The plugin regex value """
+
+TARGET_PATH_VALUE = "target_path"
+""" The target path value """
+
+JSON_PLUGIN_REGEX = ".+plugin.json$"
 """ The json plugin regex """
+
+ZIP_FILE_MODE = 1
+""" The zip file mode """
+
+TAR_FILE_MODE = 2
+""" The tar file mode """
+
+DEFAULT_COLONY_PLUGIN_FILE_MODE = ZIP_FILE_MODE
+""" The default colony plugin file mode """
+
+DEFAULT_COLONY_PLUGIN_FILE_EXTENSION = ".cpx"
+""" The default colony plugin file extension """
+
+DEFAULT_TARGET_PATH = "colony"
+""" The default target path """
 
 class MainPackingColonyService:
     """
@@ -95,22 +124,25 @@ class MainPackingColonyService:
         @param properties: The properties for the packing.
         """
 
+        # retrieves the recursive property
+        recursive = properties.get(RECURSIVE_VALUE, False)
+
         # compiles the plugin regex
         plugin_regex = re.compile(JSON_PLUGIN_REGEX)
 
-        # retrieves the directory file list
-        directory_file_list = os.listdir(directory_path)
+        # sets the plugin regex in the properties
+        properties[PLUGIN_REGEX_VALUE] = plugin_regex
 
-        # iterates over all the directory file name
-        for directory_file_name in directory_file_list:
-            # in case there is a match in the directory file name
-            if plugin_regex.match(directory_file_name):
-                # creates the full file path as the directory path and
-                # the directory file name
-                full_file_path = directory_path + "/" + directory_file_name
+        # in case the recursion is activated
+        if recursive:
+            # walks throughout the directory path
+            os.path.walk(directory_path, self._pack_directory, properties)
+        else:
+            # retrieves the directory file list
+            directory_file_list = os.listdir(directory_path)
 
-                # processes the plugin file
-                self._process_plugin_file(full_file_path)
+            # packs the directory
+            self._pack_directory(properties, directory_path, directory_file_list)
 
     def pack_files(self, file_paths_list, properties):
         """
@@ -122,9 +154,39 @@ class MainPackingColonyService:
         @param properties: The properties for the packing.
         """
 
-        print "packing random files"
+        # compiles the plugin regex
+        plugin_regex = re.compile(JSON_PLUGIN_REGEX)
 
-    def _process_plugin_file(self, file_path):
+        # retrieves the target path property
+        target_path = properties.get(TARGET_PATH_VALUE, DEFAULT_TARGET_PATH)
+
+        # iterates over all the file path in the
+        # file paths list
+        for file_path in file_paths_list:
+            # in case there is a match in the directory file name
+            if plugin_regex.match(file_path):
+                # processes the plugin file
+                self._process_plugin_file(file_path, target_path)
+
+    def _pack_directory(self, arguments, directory_path, directory_file_list):
+        # retrieves the plugin regex attribute
+        plugin_regex = arguments[PLUGIN_REGEX_VALUE]
+
+        # retrieves the target path attribute
+        target_path = arguments.get(TARGET_PATH_VALUE, DEFAULT_TARGET_PATH)
+
+        # iterates over all the directory file name
+        for directory_file_name in directory_file_list:
+            # in case there is a match in the directory file name
+            if plugin_regex.match(directory_file_name):
+                # creates the full file path as the directory path and
+                # the directory file name
+                full_file_path = directory_path + "/" + directory_file_name
+
+                # processes the plugin file
+                self._process_plugin_file(full_file_path, target_path)
+
+    def _process_plugin_file(self, file_path, target_path):
         # retrieves the plugin specification for the given file
         plugin_specification = self.main_packing_colony_service_plugin.specification_manager_plugin.get_plugin_specification(file_path, {})
 
@@ -142,11 +204,82 @@ class MainPackingColonyService:
             # retrieves the base directory
             base_directory = os.path.dirname(file_path)
 
-            import tarfile
+            # retrieves the base path and extension from the file path
+            _file_base_path, file_extension = os.path.splitext(file_path)
 
-            tar_file = tarfile.open("c:/plugins/" + plugin_id + "_" + plugin_version + ".cpx", "w:bz2")
+            # creates a new compressed file
+            compressed_file = ColonyPluginCompressedFile()
 
+            # opens the compressed file
+            compressed_file.open(target_path + "/" + plugin_id + "_" + plugin_version + DEFAULT_COLONY_PLUGIN_FILE_EXTENSION)
+
+            # iterates over all the plugin resources
             for plugin_resource in plugin_resources:
-                tar_file.add(base_directory + "/" + plugin_resource, plugin_resource)
+                # adds the plugin resource to the compressed file, using
+                # the correct relative paths
+                compressed_file.add(base_directory + "/" + plugin_resource, plugin_resource)
 
-            tar_file.close()
+            # adds the specification file to the compressed file
+            compressed_file.add(file_path, SPECIFICATION_VALUE + file_extension)
+
+            # closes the compressed file
+            compressed_file.close()
+
+class ColonyPluginCompressedFile:
+    """
+    The colony plugin compressed file class, that
+    abstracts the creation of the colony plugin file.
+    """
+
+    mode = DEFAULT_COLONY_PLUGIN_FILE_MODE
+    """ The file mode to be used to organize the plugin file """
+
+    file = None
+    """ The file reference to be used in writing """
+
+    def __init__(self, mode = DEFAULT_COLONY_PLUGIN_FILE_MODE):
+        """
+        Constructor of the class.
+
+        @type mode: int
+        @param mode: The mode to be used in the file.
+        """
+
+        self.mode = mode
+
+    def open(self, file_path):
+        """
+        Opens the file in the given file path.
+
+        @type file_path: String
+        @param file_path: The path to the file to open.
+        """
+
+        if self.mode == ZIP_FILE_MODE:
+            self.file = zipfile.ZipFile(file_path, "w", compression = zipfile.ZIP_DEFLATED)
+        elif self.mode == TAR_FILE_MODE:
+            self.file = tarfile.open(file_path, "w:gz")
+
+    def close(self):
+        """
+        Closes the current file.
+        """
+
+        self.file.close()
+
+    def add(self, file_path, target_file_path):
+        """
+        Adds the file in the given path as the logical (target)
+        file in compressed file.
+
+        @type file_path: String
+        @param file_path: The path to the file to be added.
+        @type target_file_path: String
+        @param target_file_path: The target logical path to be used in
+        the compressed file.
+        """
+
+        if self.mode == ZIP_FILE_MODE:
+            self.file.write(file_path, target_file_path)
+        elif self.mode == TAR_FILE_MODE:
+            self.file.add(file_path, target_file_path)
