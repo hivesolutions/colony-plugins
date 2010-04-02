d01983ddb1b11bcaa065d0ba76cac53757e3c018
João Magalhães <joamag@hive.pt>
2010-04-02 10:03:52 +0000

    initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7811 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.service.openid/src/colony/plugins/service_openid/openid/service_openid_system.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 428 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-11-20 18:42:55 +0000 (Qui, 20 Nov 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+class ServiceOpenid:
+    """
+    The service openid class.
+    """
+
+    service_openid_plugin = None
+    """ The service openid plugin """
+
+    def __init__(self, service_openid_plugin):
+        """
+        Constructor of the class.
+
+        @type service_openid_plugin: ServiceOpenidPlugin
+        @param service_openid_plugin: The service openid plugin.
+        """
+
+        self.service_openid_plugin = service_openid_plugin
+
+    def create_remote_client(self, service_attributes):
+        """
+        Creates a remote client, with the given service attributes.
+
+        @type service_attributes: Dictionary
+        @param service_attributes: The service attributes to be used.
+        @rtype: OpenidClient
+        @return: The created remote client.
+        """
+
+        pass
+
+class OpenidClient:
+    """
+    The class that represent a openid client connection.
+    """
+
+    pass
