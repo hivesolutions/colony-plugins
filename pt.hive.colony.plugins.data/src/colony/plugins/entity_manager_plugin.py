e062670146285a1e0d020300b45a5c1fc0ec3139
João Magalhães <joamag@hive.pt>
2010-04-09 17:03:55 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@7905 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.data/src/colony/plugins/entity_manager_plugin.py
+++ pt.hive.colony.plugins.data/src/colony/plugins/entity_manager_plugin.py
@@ -38,6 +38,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
 import colony.plugins.plugin_system
+import colony.plugins.decorators
 
 class EntityManagerPlugin(colony.plugins.plugin_system.Plugin):
     """
