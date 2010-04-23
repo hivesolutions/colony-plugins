3b89a887dc01bcb14d9507ebfad94509275a8160
Luís Martinho <lmartinho@hive.pt>
2010-04-23 14:20:48 +0000

    removed unused import
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8137 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc/src/colony/plugins/web_mvc_utils/mvc_utils/web_mvc_model.py
+++ pt.hive.colony.plugins.web.mvc/src/colony/plugins/web_mvc_utils/mvc_utils/web_mvc_model.py
@@ -37,8 +37,6 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import types
-
 import web_mvc_utils_exceptions
 
 def _start_model(self):
