be010b376fc06a52764fc99e412a4249a79ad0f4
João Magalhães <joamag@hive.pt>
2010-04-26 13:32:22 +0000

    new import refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8177 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/izpack/izpack_build_automation_extension_system.py
+++ pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/izpack/izpack_build_automation_extension_system.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import os.path
+import os
 
 import izpack_build_automation_extension_exceptions
 
