a6bc5b15ccac6ecd5a38116abb8a956920b2b2e8
João Magalhães <joamag@hive.pt>
2010-04-26 13:33:15 +0000

    new import refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8180 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.javascript.file_handler/src/colony/plugins/javascript_file_handler/file_handler/javascript_file_handler_system.py
+++ pt.hive.colony.plugins.javascript.file_handler/src/colony/plugins/javascript_file_handler/file_handler/javascript_file_handler_system.py
@@ -37,10 +37,9 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import os
 import stat
 import hashlib
-
-import os.path
 
 HANDLER_FILENAME = "none"
 """ The handler filename """
