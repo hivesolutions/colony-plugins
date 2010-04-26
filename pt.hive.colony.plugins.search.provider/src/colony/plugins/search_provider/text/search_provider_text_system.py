35e948ba64bba20d25673427cd7d8c273d8d6e42
João Magalhães <joamag@hive.pt>
2010-04-26 13:35:36 +0000

    new import refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8189 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.search.provider/src/colony/plugins/search_provider/text/search_provider_text_system.py
+++ pt.hive.colony.plugins.search.provider/src/colony/plugins/search_provider/text/search_provider_text_system.py
@@ -37,9 +37,8 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import os
 import re
-
-import os.path
 
 import search_provider_text_exceptions
 
