2ac963016419095dac32a8e121ac7fb87fec87a0
João Magalhães <joamag@hive.pt>
2010-05-02 18:11:12 +0000

    new parameters for the socket provider
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8336 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
+++ pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
@@ -246,8 +246,11 @@ class MainServiceDns:
 
                 # in case the names are the same
                 if socket_provider_plugin_provider_name == socket_provider:
+                    # the parameters for the socket provider
+                    parameters = {"server_side" : True, "do_handshake_on_connect" : False}
+
                     # creates a new dns socket with the socket provider plugin
-                    self.dns_connection = socket_provider_plugin.provide_socket()
+                    self.dns_connection = socket_provider_plugin.provide_socket_parameters(parameters)
 
             # in case the socket was not created, no socket provider found
             if not self.dns_connection:
