94a34a96cf0ee1bb005501aa7089360b217fdc65
João Magalhães <joamag@hive.pt>
2010-05-12 16:44:13 +0000

    ´new build automation file
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8482 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation/src/colony/plugins/build_automation_plugin.py
+++ pt.hive.colony.plugins.build.automation/src/colony/plugins/build_automation_plugin.py
@@ -53,7 +53,8 @@ class BuildAutomationPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["build_automation", "console_command_extension"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/build_automation/automation/resources/baf.xml"}
+    capabilities = ["build_automation", "console_command_extension", "build_automation_item"]
     capabilities_allowed = ["build_automation_extension", "build_automation_item"]
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.resources.resource_manager", "1.0.0"),
