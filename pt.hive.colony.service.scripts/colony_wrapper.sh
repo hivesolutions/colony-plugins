92967d711f3b99112956ae8d38d56ae363b5887f
João Magalhães <joamag@hive.pt>
2010-05-12 17:24:22 +0000

    initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8487 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.service.scripts/colony_wrapper.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+# __author__    = Luís Martinho <lmartinho@hive.pt>
+# __version__   = 1.0.0
+# __revision__  = $LastChangedRevision$
+# __date__      = $LastChangedDate$
+# __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+# __license__   = GNU General Public License (GPL), Version 3
+
+# the initial variables
+COLONY_HOME=/home/lmartinho/hive-main/pt.hive.colony/trunk/pt.hive.colony/src
+PID_FILE=/var/run/colony_wrapper.pid
+LOG_FILE_STDOUT=/var/log/colony_wrapper.stdout.log
+LOG_FILE_STDERR=/var/log/colony_wrapper.stderr.log
+
+# changes the directory to the colony home
+cd $COLONY_HOME
+
+# launches the colony and redirects the standard output and error
+setsid python main.py --layout=repository_svn --run_mode=development 1> $LOG_FILE_STDOUT 2> $LOG_FILE_STDERR &
+
+# touches the pid file with the current pid value
+echo $! > $PID_FILE
