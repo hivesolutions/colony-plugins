02703feb4bafbcd220286baf238d24557a19527f
João Magalhães <joamag@hive.pt>
2010-05-13 13:38:37 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8497 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
+++ pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
@@ -52,7 +52,7 @@ class PackagingArPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["startup", "packaging.ar"]
+    capabilities = ["packaging.ar"]
     capabilities_allowed = []
     dependencies = []
     events_handled = []
