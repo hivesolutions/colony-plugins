0a3d35fda4ae0deb31c4c0a2dc971c4fc1af9786
João Magalhães <joamag@hive.pt>
2010-06-02 08:22:03 +0000

    new colony handler
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8605 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http_colony_handler_plugin.py
+++ pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http_colony_handler_plugin.py
@@ -55,7 +55,8 @@ class MainServiceHttpColonyHandlerPlugin(colony.plugins.plugin_system.Plugin):
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT,
                  colony.plugins.plugin_system.JYTHON_ENVIRONMENT,
                  colony.plugins.plugin_system.IRON_PYTHON_ENVIRONMENT]
-    capabilities = ["http_service_handler"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_service_http_colony_handler/colony_handler/resources/baf.xml"}
+    capabilities = ["http_service_handler", "build_automation_item"]
     capabilities_allowed = ["http_python_handler"]
     dependencies = []
     events_handled = []
