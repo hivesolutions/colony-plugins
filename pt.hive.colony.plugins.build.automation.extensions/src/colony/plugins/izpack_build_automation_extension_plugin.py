2d7f4f137cacc7b8ba0998f43f25b33e59dab310
João Magalhães <joamag@hive.pt>
2010-06-02 12:39:11 +0000

    new build automation item
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8620 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/izpack_build_automation_extension_plugin.py
+++ pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/izpack_build_automation_extension_plugin.py
@@ -53,7 +53,8 @@ class IzpackBuildAutomationExtensionPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["build_automation_extension"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/build_automation_extensions/izpack/resources/baf.xml"}
+    capabilities = ["build_automation_extension", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.resources.resource_manager", "1.0.0"),
