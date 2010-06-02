c0eb2f90cccb3fb2e0e812bda952747d82877c85
João Magalhães <joamag@hive.pt>
2010-06-02 12:45:37 +0000

    new build automation
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8622 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.business/src/colony/plugins/business_helper_plugin.py
+++ pt.hive.colony.plugins.business/src/colony/plugins/business_helper_plugin.py
@@ -52,7 +52,8 @@ class BusinessHelperPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["business_helper"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/business/helper/resources/baf.xml"}
+    capabilities = ["business_helper", "build_automation_item"]
     capabilities_allowed = []
     dependencies = []
     events_handled = []
