5311945219fbecf79167ea5d55aa7275fbbe4585
Tiago Silva <tsilva@hive.pt>
2010-06-03 13:00:34 +0000

    added build automation to pt.hive.colony.plugins.main.tasks
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8674 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.tasks/src/colony/plugins/task_manager_plugin.py
+++ pt.hive.colony.plugins.main.tasks/src/colony/plugins/task_manager_plugin.py
@@ -52,7 +52,8 @@ class TaskManagerPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["task_manager", "task_information"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_tasks/task_manager/resources/baf.xml"}
+    capabilities = ["task_manager", "task_information", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.main.threads.thread_pool_manager", "1.0.0")]
