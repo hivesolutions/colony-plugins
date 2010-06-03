0bdf422586ffa0cf2a682cbd5a614eaf2640df48
Tiago Silva <tsilva@hive.pt>
2010-06-03 17:20:04 +0000

    added build automation to pt.hive.colony.plugins.web.entity_manager_administration
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8717 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.entity_manager_administration/src/colony/plugins/web_entity_manager_administration_plugin.py
+++ pt.hive.colony.plugins.web.entity_manager_administration/src/colony/plugins/web_entity_manager_administration_plugin.py
@@ -53,7 +53,8 @@ class WebEntityManagerAdministrationPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["web.entity_manager_administration", "rest_service"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/web_entity_manager_administration/administration/resources/baf.xml"}
+    capabilities = ["web.entity_manager_administration", "rest_service", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.resources.resource_manager", "1.0.0"),
