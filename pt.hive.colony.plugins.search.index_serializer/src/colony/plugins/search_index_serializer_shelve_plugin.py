2cf9a6046a7622cf0b01e944d93f4307b4974632
Tiago Silva <tsilva@hive.pt>
2010-06-03 14:15:55 +0000

    added build automation to pt.hive.colony.plugins.search.index_serializer
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8682 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.search.index_serializer/src/colony/plugins/search_index_serializer_shelve_plugin.py
+++ pt.hive.colony.plugins.search.index_serializer/src/colony/plugins/search_index_serializer_shelve_plugin.py
@@ -52,7 +52,8 @@ class SearchIndexSerializerCpicklePlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["search_index_serializer"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/search_index_serializer/shelve/resources/baf.xml"}
+    capabilities = ["search_index_serializer", "build_automation_item"]
     capabilities_allowed = []
     dependencies = []
     events_handled = []
