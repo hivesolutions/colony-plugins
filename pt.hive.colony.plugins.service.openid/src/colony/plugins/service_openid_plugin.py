ca876f28e7e07e82764ad8bddd5409699ef867a1
Tiago Silva <tsilva@hive.pt>
2010-06-03 15:09:03 +0000

    added build automation to pt.hive.colony.plugins.service.openid
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8694 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.service.openid/src/colony/plugins/service_openid_plugin.py
+++ pt.hive.colony.plugins.service.openid/src/colony/plugins/service_openid_plugin.py
@@ -52,7 +52,8 @@ class ServiceOpenidPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["service.openid"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/service_openid/openid/resources/baf.xml"}
+    capabilities = ["service.openid", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.main.client.http", "1.0.0"),
