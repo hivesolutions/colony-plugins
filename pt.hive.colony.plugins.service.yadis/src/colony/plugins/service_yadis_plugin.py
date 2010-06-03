40c648d91a454bb9d7e15cea729b44728e2a76c7
Tiago Silva <tsilva@hive.pt>
2010-06-03 15:09:58 +0000

    added build automation to pt.hive.colony.plugins.service.yadis
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8696 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.service.yadis/src/colony/plugins/service_yadis_plugin.py
+++ pt.hive.colony.plugins.service.yadis/src/colony/plugins/service_yadis_plugin.py
@@ -52,7 +52,8 @@ class ServiceYadisPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["service.yadis"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/service_yadis/yadis/resources/baf.xml"}
+    capabilities = ["service.yadis", "build_automation_item"]
     capabilities_allowed = []
     dependencies = []
     events_handled = []
