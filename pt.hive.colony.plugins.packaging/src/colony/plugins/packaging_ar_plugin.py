192eb9683daf9ab7fcf999a711fc283521f02b24
Tiago Silva <tsilva@hive.pt>
2010-06-03 17:34:40 +0000

    added build automation to pt.hive.colony.plugins.packaging
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8719 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
+++ pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
@@ -52,7 +52,8 @@ class PackagingArPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["packaging.ar"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/packaging/ar/resources/baf.xml"}
+    capabilities = ["packaging.ar", "build_automation_item"]
     capabilities_allowed = []
     dependencies = []
     events_handled = []
--- pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_deb_plugin.py
+++ pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_deb_plugin.py
@@ -52,7 +52,8 @@ class PackagingDebPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["packaging.deb"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/packaging/deb/resources/baf.xml"}
+    capabilities = ["packaging.deb", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.packaging.ar", "1.0.0")]
