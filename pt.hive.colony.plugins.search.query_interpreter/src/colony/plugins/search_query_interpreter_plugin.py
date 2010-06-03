5b1bf10f36fbbab55755861d2ee782ddaef139ab
Tiago Silva <tsilva@hive.pt>
2010-06-03 14:19:58 +0000

    added build automation to pt.hive.colony.plugins.search.query_interpreter
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8686 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.search.query_interpreter/src/colony/plugins/search_query_interpreter_plugin.py
+++ pt.hive.colony.plugins.search.query_interpreter/src/colony/plugins/search_query_interpreter_plugin.py
@@ -53,7 +53,8 @@ class SearchQueryInterpreterPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["search_query_interpreter"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/search_query_interpreter/query_interpreter/resources/baf.xml"}
+    capabilities = ["search_query_interpreter", "build_automation_item"]
     capabilities_allowed = []
     dependencies = [colony.plugins.plugin_system.PackageDependency(
                     "PLY", "ply", "2.5.x", "http://www.dabeaz.com/ply")]
