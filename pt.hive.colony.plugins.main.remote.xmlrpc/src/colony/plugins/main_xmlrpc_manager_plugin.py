2b9c01937c1aa4341b095a059c241f55190c4738
Tiago Silva <tsilva@hive.pt>
2010-06-03 10:33:01 +0000

    added build automation to pt.hive.colony.plugins.main.remote.xmlrpc
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8661 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.xmlrpc/src/colony/plugins/main_xmlrpc_manager_plugin.py
+++ pt.hive.colony.plugins.main.remote.xmlrpc/src/colony/plugins/main_xmlrpc_manager_plugin.py
@@ -53,7 +53,8 @@ class MainXmlrpcManagerPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["xmlrpc_manager", "http_python_handler", "rpc_handler"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_remote_xmlrpc/manager/resources/baf.xml"}
+    capabilities = ["xmlrpc_manager", "http_python_handler", "rpc_handler", "build_automation_item"]
     capabilities_allowed = ["rpc_service"]
     dependencies = []
     events_handled = []
