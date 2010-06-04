c556d2f2284fdec88b2c470007e8662ff024005c
João Magalhães <joamag@hive.pt>
2010-06-04 13:36:26 +0000

    initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8747 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.web.mvc.monitor_item/src/colony/plugins/web_mvc_monitor_item_colony_plugin.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 684 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-12-08 15:16:55 +0000 (Seg, 08 Dez 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.plugins.plugin_system
+import colony.plugins.decorators
+
+class WebMvcPanelItemDidYouKnowPlugin(colony.plugins.plugin_system.Plugin):
+    """
+    The main class for the Web Mvc Panel Item Did You Know plugin.
+    """
+
+    id = "pt.hive.colony.plugins.web.mvc.panel_item.did_you_know"
+    name = "Web Mvc Panel Item Did You Know Plugin"
+    short_name = "Web Mvc Panel Item Did You Know"
+    description = "The plugin that offers the web mvc panel item did you know"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
+    capabilities = ["web.mvc_service.panel_item"]
+    capabilities_allowed = []
+    dependencies = [colony.plugins.plugin_system.PluginDependency(
+                    "pt.hive.colony.plugins.web.mvc.utils", "1.0.0")]
+    events_handled = []
+    events_registrable = []
+    main_modules = ["web_mvc_panel_item.did_you_know.web_mvc_panel_item_did_you_know_system"]
+
+    web_mvc_panel_item_did_you_know = None
+
+    web_mvc_utils_plugin = None
+
+    def load_plugin(self):
+        colony.plugins.plugin_system.Plugin.load_plugin(self)
+        global web_mvc_panel_item
+        import web_mvc_panel_item.did_you_know.web_mvc_panel_item_did_you_know_system
+        self.web_mvc_panel_item_did_you_know = web_mvc_panel_item.did_you_know.web_mvc_panel_item_did_you_know_system.WebMvcPanelItemDidYouKnow(self)
+
+    def end_load_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_load_plugin(self)
+        self.web_mvc_panel_item_did_you_know.load_components()
+
+    def unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.plugins.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.plugins.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.plugins.decorators.inject_dependencies("pt.hive.colony.plugins.web.mvc.panel_item.did_you_know", "1.0.0")
+    def dependency_injected(self, plugin):
+        colony.plugins.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_monitor_item(self, parameters):
+        return self.web_mvc_panel_item_did_you_know.get_panel_item(parameters)
+
+    def get_web_mvc_utils_plugin(self):
+        return self.web_mvc_utils_plugin
+
+    @colony.plugins.decorators.plugin_inject("pt.hive.colony.plugins.web.mvc.utils")
+    def set_web_mvc_utils_plugin(self, web_mvc_utils_plugin):
+        self.web_mvc_utils_plugin = web_mvc_utils_plugin
