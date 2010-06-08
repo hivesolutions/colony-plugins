2477523dc250b24aa0e5eaac27dfe3b2c58b70e9
Tiago Silva <tsilva@hive.pt>
2010-06-08 10:54:55 +0000

    added build automation to pt.hive.colony.plugins.web.mvc.manager
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8821 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc.manager/src/colony/plugins/web_mvc_manager_plugin.py
+++ pt.hive.colony.plugins.web.mvc.manager/src/colony/plugins/web_mvc_manager_plugin.py
@@ -53,7 +53,8 @@ class WebMvcManagerPlugin(colony.plugins.plugin_system.Plugin):
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.plugins.plugin_system.EAGER_LOADING_TYPE
     platforms = [colony.plugins.plugin_system.CPYTHON_ENVIRONMENT]
-    capabilities = ["web.mvc_service"]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/web_mvc_manager/manager/resources/baf.xml"}
+    capabilities = ["web.mvc_service", "build_automation_item"]
     capabilities_allowed = ["web.mvc.panel_item"]
     dependencies = [colony.plugins.plugin_system.PluginDependency(
                     "pt.hive.colony.plugins.web.mvc.utils", "1.0.0"),
