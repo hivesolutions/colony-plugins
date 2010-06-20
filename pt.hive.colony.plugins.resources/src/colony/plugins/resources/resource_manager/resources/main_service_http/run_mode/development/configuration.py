5fe071214dd784ce362f940ba277513fdb09f0e4
João Magalhães <joamag@hive.pt>
2010-06-20 12:21:31 +0000

    new support for push communication
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@8966 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/main_service_http/run_mode/development/configuration.py
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/main_service_http/run_mode/development/configuration.py
@@ -62,7 +62,11 @@ configuration = {
                      }
                  },
                  "redirections" : {
-                     "resolution_order" : ["/wiki", "/manager", "/blog", "/colony_site", "/take_the_bill", "/a_la_carte", "/"],
+                     "resolution_order" : ["/push", "/wiki", "/manager", "/blog", "/colony_site", "/take_the_bill", "/a_la_carte", "/"],
+                     "/push" : {
+                         "target" : "/colony_mod_python/rest/mvc/web_mvc_communication_push/",
+                         "recursive_redirection" : True
+                     },
                      "/wiki" : {
                          "target" : "/colony_mod_python/rest/mvc/wiki/",
                          "recursive_redirection" : True
