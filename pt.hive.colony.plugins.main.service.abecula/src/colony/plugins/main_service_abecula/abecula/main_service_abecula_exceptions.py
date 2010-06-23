8dff250b3b7ad3e70dda3e711b188226b3796c1b
João Magalhães <joamag@hive.pt>
2010-06-23 14:44:55 +0000

    removed unsued exception
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@9032 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.abecula/src/colony/plugins/main_service_abecula/abecula/main_service_abecula_exceptions.py
+++ pt.hive.colony.plugins.main.service.abecula/src/colony/plugins/main_service_abecula/abecula/main_service_abecula_exceptions.py
@@ -45,32 +45,6 @@ class MainServiceAbeculaException(Exception):
     message = None
     """ The exception's message """
 
-class SocketProviderNotFound(MainServiceAbeculaException):
-    """
-    The socket provider not found class.
-    """
-
-    def __init__(self, message):
-        """
-        Constructor of the class.
-
-        @type message: String
-        @param message: The message to be printed.
-        """
-
-        MainServiceAbeculaException.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        """
-        Returns the string representation of the class.
-
-        @rtype: String
-        @return: The string representation of the class.
-        """
-
-        return "Socket provider not found: %s" % self.message
-
 class ServerRequestTimeout(MainServiceAbeculaException):
     """
     The server request timeout class.
