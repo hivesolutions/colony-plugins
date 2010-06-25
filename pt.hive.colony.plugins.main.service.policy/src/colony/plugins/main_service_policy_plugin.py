772e99483775ede4b28f17f648faeaab53959701
João Magalhães <joamag@hive.pt>
2010-06-25 08:52:59 +0000

    tiny code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@9046 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy_plugin.py
+++ pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy_plugin.py
@@ -62,7 +62,7 @@ class MainServicePolicyPlugin(colony.plugins.plugin_system.Plugin):
                     "pt.hive.colony.plugins.main.threads.thread_pool_manager", "1.0.0")]
     events_handled = []
     events_registrable = []
-    main_modules = ["main_service_policy.policy.main_service_policy_system", "main_service_policy.policy.main_service_policy_exceptions"]
+    main_modules = ["main_service_policy.policy.main_service_policy_exceptions", "main_service_policy.policy.main_service_policy_system"]
 
     main_service_policy = None
 
