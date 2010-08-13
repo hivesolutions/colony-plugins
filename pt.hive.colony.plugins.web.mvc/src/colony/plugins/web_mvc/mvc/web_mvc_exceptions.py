722a2775621b5680902b97b061ca5202f4fdcc61
João Magalhães <joamag@hive.pt>
2010-08-13 09:57:28 +0000

    new exception system
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@9751 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc/src/colony/plugins/web_mvc/mvc/web_mvc_exceptions.py
+++ pt.hive.colony.plugins.web.mvc/src/colony/plugins/web_mvc/mvc/web_mvc_exceptions.py
@@ -37,7 +37,9 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-class ServiceException(Exception):
+import colony.plugins.plugin_system_exceptions
+
+class ServiceException(colony.plugins.plugin_system_exceptions.ColonyException):
     """
     The service exception class.
     """
