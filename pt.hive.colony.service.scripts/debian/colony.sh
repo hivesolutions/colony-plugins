8dfee4f2ebe3ab3062449f1ff3a24b9b8be4084f
João Magalhães <joamag@hive.pt>
2010-08-31 04:41:15 +0000

    minor code fix
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@9939 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.service.scripts/debian/colony.sh
+++ pt.hive.colony.service.scripts/debian/colony.sh
@@ -73,7 +73,7 @@ do_start() {
     fi
 
     # tests the daemonn to check if it is already running
-    start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON --test > /dev/null || (rm -f $PIDFILE && return 1)
+    start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON --test > /dev/null || return 1
 
     # touches the pid file to lock the starting process
     touch $PIDFILE
