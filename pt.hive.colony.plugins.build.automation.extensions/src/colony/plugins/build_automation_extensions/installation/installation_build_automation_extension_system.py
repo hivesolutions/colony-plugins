d0c6d8286c120f5e6f2f0f9085cc4094577aaf20
João Magalhães <joamag@hive.pt>
2010-09-01 13:14:30 +0000

    initial installation build automation plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@9962 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/installation/installation_build_automation_extension_system.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Ter, 21 Out 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+TARGET_DIRECTORY_VALUE = "target_directory"
+""" The target directory value """
+
+class InstallationBuildAutomationExtension:
+    """
+    The installation build automation extension class.
+    """
+
+    installation_build_automation_extension_plugin = None
+    """ The installation build automation extension plugin """
+
+    def __init__(self, installation_build_automation_extension_plugin):
+        """
+        Constructor of the class.
+
+        @type installation_build_automation_extension_plugin: InstallationBuildAutomationExtensionPlugin
+        @param installation_build_automation_extension_plugin: The installation build automation extension plugin.
+        """
+
+        self.installation_build_automation_extension_plugin = installation_build_automation_extension_plugin
+
+    def run_automation(self, plugin, stage, parameters, build_automation_structure):
+        # retrieves the installation manager plugin
+        installation_manager_plugin = self.installation_build_automation_extension_plugin.installation_manager_plugin
+
+        # retrieves the build properties
+        build_properties = build_automation_structure.get_all_build_properties()
+
+        # retrieves the formats list
+        formats = parameters["formats"].split(",")
+
+        # iterates over all the formats to generate the installation files
+        for format in formats:
+            # creates the installation parameters map
+            installation_parameters = {"installation_adapter" : format,
+                                       "file_path" : build_properties[TARGET_DIRECTORY_VALUE] + "/" + parameters["base_name"] + "." + format,
+                                       "package" : parameters["package"],
+                                       "contents" : parameters["contents"]}
+
+            # generates the installation file
+            installation_manager_plugin.generate_installation_file(installation_parameters)
