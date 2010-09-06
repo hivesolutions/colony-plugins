6856a75a4e464369e2ed179b28de5a4faec23d2d
João Magalhães <joamag@hive.pt>
2010-09-06 17:21:27 +0000

    initial commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10058 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.repository/src/colony/plugins/repository/apt_generator/repository_apt_generator_system.py
@@ -0,0 +1,60 @@
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Ter, 21 Out 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+class RepositoryAptGenerator:
+    """
+    The repository apt generator class.
+    """
+
+    repository_apt_generator_plugin = None
+    """ The repository apt generator plugin """
+
+    def __init__(self, repository_apt_generator_plugin):
+        """
+        Constructor of the class.
+
+        @type repository_apt_generator_plugin: RepositoryAptGeneratorPlugin
+        @param repository_apt_generator_plugin: The repository apt generator plugin.
+        """
+
+        self.repository_apt_generator_plugin = repository_apt_generator_plugin
+
+    def generate_repository(self, parameters):
+        """
+        Generates a repository for the given parameters.
+
+        @type parameters: Dictionary
+        @param parameters: The parameters for the repository generation.
+        """
+
+        pass
---
+++ pt.hive.colony.plugins.repository/src/colony/plugins/repository_apt_generator_plugin.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 8461 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2010-05-12 06:45:34 +0100 (qua, 12 Mai 2010) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+
+class RepositoryAptGeneratorPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Repository Apt Generator plugin.
+    """
+
+    id = "pt.hive.colony.plugins.repository.apt_generator"
+    name = "Repository Apt Generator Plugin"
+    short_name = "Repository Apt Generator"
+    description = "A plugin to generate apt repositories"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/packaging/deb/resources/baf.xml"}
+    capabilities = ["repository.generator", "build_automation_item"]
+    capabilities_allowed = []
+    dependencies = []
+    events_handled = []
+    events_registrable = []
+    main_modules = ["packaging.deb.packaging_deb_exceptions", "packaging.deb.packaging_deb_system"]
+
+    repository_apt_generator = None
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        global repository
+        import repository.apt_generator.repository_apt_generator_system
+        self.repository_apt_generator = repository.apt_generator.repository_apt_generator_system.RepositoryAptGenerator(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def generate_repository(self, parameters):
+        """
+        Generates a repository for the given parameters.
+
+        @type parameters: Dictionary
+        @param parameters: The parameters for the repository generation.
+        """
+
+        return self.repository_apt_generator.generate_repository(parameters)
