c2e4ce6d705291713faf83787e8551ddcf99b915
João Magalhães <joamag@hive.pt>
2010-09-08 13:24:57 +0000

    new deploy directory added and new support for post install
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10141 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation.items/src/colony/plugins/build_automation_items/colony_base_plugins/resources/postinst.sh
+++ pt.hive.colony.plugins.build.automation.items/src/colony/plugins/build_automation_items/colony_base_plugins/resources/postinst.sh
@@ -1,1 +1,6 @@
-echo "FOI TUDO INSTALADO"
+for plugin in $(ls /usr/lib/colony/deploy); do
+    echo "Installing $plugin"
+    colony_deploy /usr/lib/colony/deploy/$plugin
+done
+
+echo "Finished installing plugins"
