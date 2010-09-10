d3f484d0383642c26ead02a8dacef16a90d2dc56
Tiago Silva <tsilva@hive.pt>
2010-09-10 10:39:52 +0000

    added system file to main service irc starter plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10273 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.irc/src/colony/plugins/main_service_irc_starter_plugin.py
+++ pt.hive.colony.plugins.main.service.irc/src/colony/plugins/main_service_irc_starter_plugin.py
@@ -61,6 +61,7 @@ class MainServiceIrcStarterPlugin(colony.base.plugin_system.Plugin):
                     "pt.hive.colony.plugins.main.service.irc", "1.0.0")]
     events_handled = []
     events_registrable = []
+    main_modules = ["main_service_irc_starter.irc_starter.main_service_irc_starter_system"]
 
     main_service_irc_plugin = None
 
