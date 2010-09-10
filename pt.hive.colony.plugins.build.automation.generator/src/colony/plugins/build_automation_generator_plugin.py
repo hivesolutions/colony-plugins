67f19dbc340b231cfc03f00ef735d94079f9ed7d
Tiago Silva <tsilva@hive.pt>
2010-09-10 12:16:02 +0000

    fixed build automation generator plugin class name
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10293 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation.generator/src/colony/plugins/build_automation_generator_plugin.py
+++ pt.hive.colony.plugins.build.automation.generator/src/colony/plugins/build_automation_generator_plugin.py
@@ -40,7 +40,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 import colony.base.plugin_system
 import colony.base.decorators
 
-class BuildAutomationGeratorPlugin(colony.base.plugin_system.Plugin):
+class BuildAutomationGeneratorPlugin(colony.base.plugin_system.Plugin):
     """
     The main class for the Build Automation Generator plugin.
     """
