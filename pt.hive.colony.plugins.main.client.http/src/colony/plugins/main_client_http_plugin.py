20b2a3712bc5eb6e647331ecaf9511135afd50eb
Tiago Silva <tsilva@hive.pt>
2010-09-10 12:18:52 +0000

    fixed main client http plugin class name
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10300 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.client.http/src/colony/plugins/main_client_http_plugin.py
+++ pt.hive.colony.plugins.main.client.http/src/colony/plugins/main_client_http_plugin.py
@@ -40,7 +40,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 import colony.base.plugin_system
 import colony.base.decorators
 
-class MainClienthttpPlugin(colony.base.plugin_system.Plugin):
+class MainClientHttpPlugin(colony.base.plugin_system.Plugin):
     """
     The main class for the Http Client Main plugin.
     """
