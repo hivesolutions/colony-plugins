7c13ef8b7e9011bc6d31ce8e584b52373c3792bd
João Magalhães <joamag@hive.pt>
2010-09-21 17:09:06 +0000

    new directory and link removal trick
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10578 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/continuous_integration/continuous_integration_build_automation_extension_system.py
+++ pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins/build_automation_extensions/continuous_integration/continuous_integration_build_automation_extension_system.py
@@ -140,13 +140,14 @@ class ContinuousIntegrationBuildAutomationExtension:
         # creates the latest version path
         latest_version_path = deployment_path + "/" + LATEST_DIRECTORY_NAME
 
-        # in case there is an existent directory in the latest path
-        if os.path.isdir(latest_version_path):
-            # removes the latest version path (directory)
-            colony.libs.path_util.remove_directory(latest_version_path)
-        elif os.path.exists(latest_version_path):
+        # in case there is an existent link in the latest path
+        if os.path.islink(latest_version_path):
             # removes the latest version path
             os.remove(latest_version_path)
+        # in case there is an existent directory in the latest path
+        elif os.path.isdir(latest_version_path):
+            # removes the latest version path (directory)
+            colony.libs.path_util.remove_directory(latest_version_path)
 
         # creates a symbolic link between the deployment version path and the latest
         # version path
