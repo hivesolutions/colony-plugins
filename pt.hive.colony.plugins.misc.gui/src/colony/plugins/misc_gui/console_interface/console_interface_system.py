6b899b8229c944ee31d4a45b9f9266c491d74ac2
João Magalhães <joamag@hive.pt>
2010-09-27 04:47:45 +0000

    minor comment refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10696 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.misc.gui/src/colony/plugins/misc_gui/console_interface/console_interface_system.py
+++ pt.hive.colony.plugins.misc.gui/src/colony/plugins/misc_gui/console_interface/console_interface_system.py
@@ -37,21 +37,38 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import wx
-
 import console_window
 
 INTRO_MESSAGE = "Welcome to the plugin system console"
+""" The intro message to be printed at the beginning of the console """
 
 class ConsoleInterface:
+    """
+    The console interface class.
+    """
 
     console_interface_plugin = None
+    """ The console interface plugin """
 
     def __init__(self, console_interface_plugin):
+        """
+        Constructor of the class.
+
+        @type console_interface_plugin: ConsoleInterfacePlugin
+        @param console_interface_plugin: The console interface plugin.
+        """
+
         self.console_interface_plugin = console_interface_plugin
 
     def do_panel(self, parent):
+        # creates the console window from the given intro message
         console_panel = console_window.ConsoleWindow(parent, intro_message = INTRO_MESSAGE)
+
+        # sets the display of line numbers in the console panel
         console_panel.set_display_line_numbers(True)
+
+        # sets the console plugin in the console panel
         console_panel.set_console_plugin(self.console_interface_plugin.console_plugin)
+
+        # returns the control panel
         return console_panel
