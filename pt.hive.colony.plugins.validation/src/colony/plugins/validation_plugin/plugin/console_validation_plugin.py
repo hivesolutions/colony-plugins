3601df044bafa9f1fbea385a4bf35d0e5db09225
João Magalhães <joamag@hive.pt>
2010-10-07 11:47:20 +0000

    initial project import
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@10888 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.validation/src/colony/plugins/validation_plugin/plugin/console_validation_plugin.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Tue, 21 Oct 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+CONSOLE_EXTENSION_NAME = "build_automation_validator"
+""" The console extension name """
+
+INVALID_NUMBER_ARGUMENTS_MESSAGE = "invalid number of arguments"
+""" The invalid number of arguments message """
+
+HELP_TEXT = "### PLUGIN VALIDATION HELP ###\n\
+validate_plugin [plugin_id] - validates plugin(s)"
+""" The help text """
+
+class ConsoleBuildAutomationValidator:
+    """
+    The console build automation validator class.
+    """
+
+    build_automation_validator_plugin = None
+    """ The build automation validator plugin """
+
+    commands = ["validate_plugin"]
+    """ The commands list """
+
+    def __init__(self, build_automation_validator_plugin):
+        """
+        Constructor of the class.
+
+        @type build_automation_validator_plugin: BuildAutomationValidatorPlugin
+        @param build_automation_validator_plugin: The build automation validator plugin.
+        """
+
+        self.build_automation_validator_plugin = build_automation_validator_plugin
+
+    def get_console_extension_name(self):
+        return CONSOLE_EXTENSION_NAME
+
+    def get_all_commands(self):
+        return self.commands
+
+    def get_handler_command(self, command):
+        if command in self.commands:
+            method_name = "process_" + command
+            attribute = getattr(self, method_name)
+            return attribute
+
+    def get_help(self):
+        return HELP_TEXT
+
+    def process_validate_plugin(self, args, output_method):
+        # returns in case the not enough arguments were provided
+        if len(args) > 1:
+            output_method(INVALID_NUMBER_ARGUMENTS_MESSAGE)
+            return
+
+        # validates all build automatin plugins in case no argument was provided
+        if len(args) == 0:
+            import build_automation_validator_exceptions
+
+            try:
+                self.build_automation_validator_plugin.validate_build_automation_plugins()
+            except build_automation_validator_exceptions.BuildAutomationValidationFailed, exception:
+                for validation_error in exception.validation_errors:
+                    print validation_error["plugin_id"]
+                    print validation_error["message"]
+                    print "------------------------"
+
+            return
+
+        # retrieves the plugin's id
+        plugin_id = args[0]
+
+        # validates the specified build automation plugin
+        self.build_automation_validator_plugin.validate_build_automation_plugin(plugin_id)
