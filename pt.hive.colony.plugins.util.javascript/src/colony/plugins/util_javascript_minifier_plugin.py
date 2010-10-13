38ac31e102a7ba3596a500cf4b368787cdf1680c
João Magalhães <joamag@hive.pt>
2010-10-13 18:03:39 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@11079 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.util.javascript/src/colony/plugins/util_javascript_minifier_plugin.py
+++ pt.hive.colony.plugins.util.javascript/src/colony/plugins/util_javascript_minifier_plugin.py
@@ -39,7 +39,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import colony.base.plugin_system
 
-class JavascriptMinifierUtilPlugin(colony.base.plugin_system.Plugin):
+class UtilJavascriptMinifierPlugin(colony.base.plugin_system.Plugin):
     """
     The main class for the Javascript Minifier Util plugin.
     """
