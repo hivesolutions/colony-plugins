780545f3dcf3d6291c52aa4216c4fc335ff8c605
João Magalhães <joamag@hive.pt>
2010-10-19 21:50:48 +0000

    minor code refactor for test
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@11214 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service/src/colony/plugins/main_service_utils/utils/main_service_utils_system.py
+++ pt.hive.colony.plugins.main.service/src/colony/plugins/main_service_utils/utils/main_service_utils_system.py
@@ -82,7 +82,7 @@ POLL_TIMEOUT = 1
 REQUEST_TIMEOUT = 100
 """ The request timeout """
 
-RESPONSE_TIMEOUT = 10
+RESPONSE_TIMEOUT = 1
 """ The response timeout """
 
 CONNECTION_TIMEOUT = 600
@@ -992,12 +992,12 @@ class AbstractServiceConnectionHandler:
         # address and port
         connection_socket, _connection_address, _connection_port = work_reference
 
-        try:
-            # removes the connection using the socket as reference
-            self.remove_connection_socket(connection_socket)
-        except Exception, exception:
+        #try:
+        # removes the connection using the socket as reference
+        self.remove_connection_socket(connection_socket)
+        #except Exception, exception:
             # prints an error for not being able to remove connection
-            self.service.main_service_utils_plugin.error("Problem while removing connection from service connection handler: %s" % unicode(exception))
+        #    self.service.main_service_utils_plugin.error("Problem while removing connection from service connection handler: %s" % unicode(exception))
 
     def add_connection(self, connection_socket, connection_address, connection_port):
         """
