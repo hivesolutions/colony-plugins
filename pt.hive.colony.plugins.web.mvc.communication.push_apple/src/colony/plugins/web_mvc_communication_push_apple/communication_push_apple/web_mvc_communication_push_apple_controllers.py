7ee876697345f1ecfe3372e53a0dfcc35a922879
João Magalhães <joamag@hive.pt>
2010-10-31 18:18:01 +0000

    better support for communication push apple
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@11457 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc.communication.push_apple/src/colony/plugins/web_mvc_communication_push_apple/communication_push_apple/web_mvc_communication_push_apple_controllers.py
+++ pt.hive.colony.plugins.web.mvc.communication.push_apple/src/colony/plugins/web_mvc_communication_push_apple/communication_push_apple/web_mvc_communication_push_apple_controllers.py
@@ -58,6 +58,39 @@ GUID_VALUE = "guid"
 DEVICE_ID_VALUE = "device_id"
 """ The device id value """
 
+NOTIFICATION_HANDLER_NAME_VALUE = "notification_handler_name"
+""" The notification handler name value """
+
+ALERT_VALUE = "alert"
+""" The alert value """
+
+BADGE_VALUE = "badge"
+""" The badge value """
+
+SOUND_VALUE = "sound"
+""" The sound value """
+
+DEFAULT_VALUE = "default"
+""" The default value """
+
+APS_VALUE = "aps"
+""" The aps value """
+
+PARAMETERS_VALUE = "parameters"
+""" The parameters value """
+
+HOSTNAME_VALUE = "hostname"
+""" The hostname value """
+
+PORT_VALUE = "port"
+""" The port value """
+
+KEY_FILE_PATH_VALUE = "key_file_path"
+""" The key file path value """
+
+CERTIFICATE_FILE_PATH_VALUE = "certificate_file_path"
+""" The certificate file path value """
+
 class WebMvcCommunicationPushAppleController:
     """
     The web mvc communication push apple controller.
@@ -189,13 +222,31 @@ class WebMvcCommunicationPushAppleController:
         # returns true
         return True
 
-    def generate_handler(self, device_id):
+    def default_notification_handler(self, notification):
+        # retrieves the notification message
+        message = notification.get_message()
+
+        # sets the push notification values
+        alert = message
+        badge = None
+        sound = DEFAULT_VALUE
+
+        # creates the apple push notification tuple with
+        # the alert the badge and the sound
+        apple_push_notification_tuple = (alert, badge, sound)
+
+        # returns the apple push notification tuple
+        return apple_push_notification_tuple
+
+    def generate_handler(self, device_id, notification_handler_name = None):
         """
         Generates a communication handler for the
         given request.
 
         @type device_id: String
         @param device_id: The id of the device to be notified.
+        @type notification_handler_name: String
+        @param notification_handler_name: The notification handler name.
         @rtype: Function
         @return: The generated communication handler
         """
@@ -228,18 +279,25 @@ class WebMvcCommunicationPushAppleController:
                           GUID_VALUE : guid,
                           MESSAGE_CONTENTS_VALUE : message}
 
+            # in case the notification handler name is defined
+            if notification_handler_name:
+                pass
+            else:
+                # sets the notification handler as the default one
+                notification_handler = self.default_notification_handler
+
             # retrieves the aps values
-            alert, badge, sound = escolinhas_handler(notification)
+            alert, badge, sound = notification_handler(notification)
 
             # creates the aps map
-            aps = {"alert" : alert, "badge" : badge, "sound" : sound}
+            aps = {ALERT_VALUE : alert, BADGE_VALUE : badge, SOUND_VALUE : sound}
 
             # creates the payload map
             payload = {}
 
             # populates the payload map
-            payload["aps"] = aps
-            payload["parameters"] = parameters
+            payload[APS_VALUE] = aps
+            payload[PARAMETERS_VALUE] = parameters
 
             # serializes the payload into json
             payload_serialized = json_plugin.dumps(payload)
@@ -257,35 +315,20 @@ class WebMvcCommunicationPushAppleController:
             apple_push_configuration_map = self.web_mvc_communication_push_apple.apple_push_configuration_map
 
             # retrieves the apple push configuration values
-            hostname = apple_push_configuration_map.get("hostname")
-            port = apple_push_configuration_map.get("port")
-            certificate_file_path = apple_push_configuration_map.get("certificate_file_path")
+            hostname = apple_push_configuration_map.get(HOSTNAME_VALUE)
+            port = apple_push_configuration_map.get(PORT_VALUE)
+            certificate_file_path = apple_push_configuration_map.get(CERTIFICATE_FILE_PATH_VALUE)
 
             # creates the socket parameters map
-            socket_parameters = {"key_file_path" : None,
-                                 "certificate_file_path" : certificate_file_path}
+            socket_parameters = {KEY_FILE_PATH_VALUE : None,
+                                 CERTIFICATE_FILE_PATH_VALUE : certificate_file_path}
 
             try:
+                # sends the notification (payload) to the device
                 apple_push_client.notify_device(hostname, port, device_id, payload_serialized_encoded, socket_parameters = socket_parameters)
             finally:
                 # closes the apple push client
                 apple_push_client.close({})
-
-        def escolinhas_handler(notification):
-            # retrieves the notification message
-            message = notification.get_message()
-
-            # sets the push notification values
-            alert = message
-            badge = 1
-            sound = "default"
-
-            # creates the apple push notification tuple with
-            # the alert the badge and the sound
-            apple_push_notification_tuple = (alert, badge, sound)
-
-            # returns the apple push notification tuple
-            return apple_push_notification_tuple
 
         # returns the communication handler
         return communication_handler
@@ -301,9 +344,10 @@ class WebMvcCommunicationPushAppleController:
         communication_handler_name = form_data_map[COMMUNICATION_HANDLER_NAME_VALUE]
         communication_name = form_data_map[COMMUNICATION_NAME_VALUE]
         device_id = form_data_map[DEVICE_ID_VALUE]
+        notification_handler_name = form_data_map.get(NOTIFICATION_HANDLER_NAME_VALUE, None)
 
-        # generates a communication handler for the given device id
-        generated_communication_handler = self.generate_handler(device_id)
+        # generates a communication handler for the given device id and notification handler name
+        generated_communication_handler = self.generate_handler(device_id, notification_handler_name)
 
         # creates the service connection name tuple
         service_connection_name_tuple = (communication_handler_name, device_id, communication_name)
@@ -313,6 +357,30 @@ class WebMvcCommunicationPushAppleController:
 
         # adds a new communication handler
         communication_push_plugin.add_communication_handler(communication_name, communication_handler_name, generated_communication_handler)
+
+    def _unregister(self, rest_request):
+        # retrieves the communication push plugin
+        communication_push_plugin = self.web_mvc_communication_push_plugin.communication_push_plugin
+
+        # processes the form data
+        form_data_map = self.process_form_data(rest_request, DEFAULT_ENCODING)
+
+        # retrieves the form data attributes
+        communication_handler_name = form_data_map[COMMUNICATION_HANDLER_NAME_VALUE]
+        communication_name = form_data_map[COMMUNICATION_NAME_VALUE]
+        device_id = form_data_map[DEVICE_ID_VALUE]
+
+        # creates the service connection name tuple
+        service_connection_name_tuple = (communication_handler_name, device_id, communication_name)
+
+        # retrieves the generated communication handler for the service connection and communication name
+        generated_communication_handler = self.service_connection_name_communication_handler_map[service_connection_name_tuple]
+
+        # removes the communication handler
+        communication_push_plugin.remove_communication_handler(communication_name, communication_handler_name, generated_communication_handler)
+
+        # removes the service connection name from the service connection name communication handler map
+        del self.service_connection_name_communication_handler_map[service_connection_name_tuple]
 
     def _load_profile(self, rest_request):
         # retrieves the communication push plugin
@@ -325,12 +393,13 @@ class WebMvcCommunicationPushAppleController:
         communication_handler_name = form_data_map[COMMUNICATION_HANDLER_NAME_VALUE]
         communication_profile_name = form_data_map[COMMUNICATION_PROFILE_NAME_VALUE]
         device_id = form_data_map[DEVICE_ID_VALUE]
+        notification_handler_name = form_data_map.get(NOTIFICATION_HANDLER_NAME_VALUE, None)
 
-        # generates a communication handler for the given device id
-        generated_communication_handler = self.generate_handler(device_id)
+        # generates a communication handler for the given device id and notification handler name
+        generated_communication_handler = self.generate_handler(device_id, notification_handler_name)
 
         # creates the service connection profile name tuple
-        service_connection_profile_name_tuple = (communication_handler_name, device_id, generated_communication_handler)
+        service_connection_profile_name_tuple = (communication_handler_name, device_id, communication_profile_name)
 
         # sets the generated communication handler in the service connection profile name communication handler map
         self.service_connection_profile_name_communication_handler_map[service_connection_profile_name_tuple] = generated_communication_handler
