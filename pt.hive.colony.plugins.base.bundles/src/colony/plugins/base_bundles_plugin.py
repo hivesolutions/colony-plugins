0f515d6af293358c451306265edcac2397314aeb
João Magalhães <joamag@hive.pt>
2010-11-08 18:26:44 +0000

    initial code commit
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@11747 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.base.bundles/src/colony/plugins/base_bundles_plugin.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2300 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-01 17:10:15 +0100 (qua, 01 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+
+class BaseBundlesPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Base Bundles plugin.
+    """
+
+    id = "pt.hive.colony.plugins.base.bundles"
+    name = "Base Bundles Plugin"
+    short_name = "Base Bundles"
+    description = "Base Bundles Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/base_bundles/bundles/resources/baf.xml"}
+    capabilities = ["build_automation_item"]
+    capabilities_allowed = []
+    dependencies = []
+    events_handled = []
+    events_registrable = []
+    main_modules = ["base_bundles.bundles.base_bundles_system"]
+
+    base_bundles = None
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        global base_bundles
+        import base_bundles.bundles.base_bundles_system
+        self.base_bundles = base_bundles.bundles.base_bundles_system.BaseBundles(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
