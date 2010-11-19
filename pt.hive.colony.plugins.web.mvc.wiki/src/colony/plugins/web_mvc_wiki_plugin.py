9b401a0de60a115d19d78dd8cebc77856b02e9dd
Tiago Silva <tsilva@hive.pt>
2010-11-19 10:28:30 +0000

    fixed web mvc wiki plugin validation errors
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@11968 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc.wiki/src/colony/plugins/web_mvc_wiki_plugin.py
+++ pt.hive.colony.plugins.web.mvc.wiki/src/colony/plugins/web_mvc_wiki_plugin.py
@@ -67,7 +67,7 @@ class WebMvcWikiPlugin(colony.base.plugin_system.Plugin):
                     "pt.hive.colony.plugins.misc.string_normalization", "1.0.0")]
     events_handled = []
     events_registrable = []
-    main_modules = ["web_mvc_wiki.mvc_wiki.web_mvc_wiki_exception",
+    main_modules = ["web_mvc_wiki.mvc_wiki.web_mvc_wiki_exceptions",
                     "web_mvc_wiki.mvc_wiki.web_mvc_wiki_system"]
 
     web_mvc_wiki = None
