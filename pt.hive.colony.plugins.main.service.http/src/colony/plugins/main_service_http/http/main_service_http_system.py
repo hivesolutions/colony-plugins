3de922dc4e985d02f79c0695a1709d21e50ab30e
João Magalhães <joamag@hive.pt>
2010-11-26 12:36:56 +0000

    new error values added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12078 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
+++ pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
@@ -127,7 +127,11 @@ STATUS_CODE_VALUES = {100 : "Continue", 101 : "Switching Protocols",
                       204 : "No Content", 205 : "Reset Content", 206 : "Partial Content", 207 : "Multi-Status",
                       301 : "Moved permanently", 302 : "Found", 303 : "See Other", 304 : "Not Modified",
                       305 : "Use Proxy", 306 : "(Unused)", 307 : "Temporary Redirect",
-                      400 : "Bad Request", 401 : "Unauthorized", 402 : "Payment Required", 403 : "Forbidden", 404 : "Not Found",
+                      400 : "Bad Request", 401 : "Unauthorized", 402 : "Payment Required", 403 : "Forbidden",
+                      404 : "Not Found", 405 : "Method Not Allowed", 406 : "Not Acceptable", 407 : "Proxy Authentication Required",
+                      408 : "Request Timeout", 409 : "Conflict", 410 : "Gone", 411 : "Length Required", 412 : "Precondition Failed",
+                      413 : "Request Entity Too Large", 414 : "Request-URI Too Long", 415 : "Unsupported Media Type",
+                      416 : "Requested Range Not Satisfiable", 417 : "Expectation Failed",
                       500 : "Internal Server Error", 501 : "Not Implemented", 502 : "Bad Gateway",
                       503 : "Service Unavailable", 504 : "Gateway Timeout", 505 : "HTTP Version Not Supported"}
 """ The status code values map """
