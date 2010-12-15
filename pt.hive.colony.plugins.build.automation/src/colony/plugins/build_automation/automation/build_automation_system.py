0aefca41653271c15c8826f2a0ff9021fe9e62ff
João Magalhães <joamag@hive.pt>
2010-12-15 19:32:57 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12309 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.build.automation/src/colony/plugins/build_automation/automation/build_automation_system.py
+++ pt.hive.colony.plugins.build.automation/src/colony/plugins/build_automation/automation/build_automation_system.py
@@ -513,13 +513,6 @@ class BuildAutomation:
                     # returns false (invalid)
                     return False
             except Exception, exception:
-                import sys
-                import traceback
-
-                print "Exception in user code:"
-                print '-'*60
-                traceback.print_exc(file=sys.stdout)
-
                 # prints an error message
                 logger.error("Problem while executing build automation '%s'" % unicode(exception))
 
