497044542382d54f57f1bb72f0f30ae33a0e5fb5
João Magalhães <joamag@hive.pt>
2010-12-15 18:05:36 +0000

    minor configuration change
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12300 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/scheduler/run_mode/integration/configuration.py
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/scheduler/run_mode/integration/configuration.py
@@ -46,5 +46,17 @@ configuration = {
                              "method" : "run_automation",
                              "arguments" : ["pt.hive.colony.plugins.build.automation.items.colony_integration", "1.0.0", "install", 1],
                              "recursion_list" : [0, 0, 5, 0, 0]
+                             }, {
+                             "type" : "plugin",
+                             "plugin_id" : "pt.hive.hive_development.plugins.media_dashboard.updater.revision_control",
+                             "plugin_version" : "1.0.0",
+                             "method" : "update_media_dashboard",
+                             "arguments" : [{
+                                             "adapter_name" : "svn",
+                                             "revision_control_parameters" : {},
+                                             "repository_path" : "http://svn.hive.pt/hive-main",
+                                             "base_url" : "http://localhost:8080/media_dashboard/"
+                                             }],
+                             "recursion_list" : [0, 0, 1, 0, 0]
                              }]
              }
