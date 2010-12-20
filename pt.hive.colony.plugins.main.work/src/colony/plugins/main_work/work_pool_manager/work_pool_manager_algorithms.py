d9d36bf9999b28efa978f1eb697b053a08c63ddb
João Magalhães <joamag@hive.pt>
2010-12-20 19:18:14 +0000

    new round robin smart algorithm
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12347 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.work/src/colony/plugins/main_work/work_pool_manager/work_pool_manager_algorithms.py
+++ pt.hive.colony.plugins.main.work/src/colony/plugins/main_work/work_pool_manager/work_pool_manager_algorithms.py
@@ -173,3 +173,108 @@ class RoundRobinAlgorithm(WorkPoolManagerAlgorithm):
 
         # returns the work task
         return work_task
+
+class RoundRobinSmartAlgorithm(WorkPoolManagerAlgorithm):
+    """
+    The round robin smart algorithm for work
+    pool manager.
+    """
+
+    current_index = 0
+    """ The current index """
+
+    work_tasks_list_lock = None
+    """ The lock to control the access to the work tasks list """
+
+    def __init__(self, work_pool):
+        """
+        Constructor of the class.
+
+        @type work_pool: WorkPool
+        @param work_pool: The work pool associated with the algorithm.
+        """
+
+        WorkPoolManagerAlgorithm.__init__(self, work_pool)
+
+        self.work_tasks_list_lock = threading.Lock()
+
+    def work_added(self, work_task, work_reference):
+        """
+        Called upon new work added to a work task.
+
+        @type work_task: WorkTask
+        @param work_task: The work task that added a new
+        work reference.
+        @type work_reference: Object
+        @param work_reference: The added work reference.
+        """
+
+        pass
+
+    def work_removed(self, work_task, work_reference):
+        """
+        Called upon new work removed from a work task.
+
+        @type work_task: WorkTask
+        @param work_task: The work task that removed a new
+        work reference.
+        @type work_reference: Object
+        @param work_reference: The removed work reference.
+        """
+
+        pass
+
+    def get_next(self):
+        """
+        Retrieves the next element of the work
+        pool to be retrieved, according to the algorithm.
+
+        @rtype: Object
+        @return: The next element to be retrieved,
+        according to the algorithm.
+        """
+
+        # acquires the lock
+        self.work_tasks_list_lock.acquire()
+
+        # retrieves the work tasks list
+        work_tasks_list = self.work_pool.work_tasks_list
+
+        # retrieves the work tasks list length
+        work_tasks_list_length = len(work_tasks_list)
+
+        # retrieves the initial current index
+        initial_current_index = self.current_index
+
+        # iterates continuously
+        while True:
+            # in case the current index contains the same
+            # value as the work tasks list length
+            if self.current_index == work_tasks_list_length:
+                # resets the value of the current index
+                self.current_index = 0
+
+            # retrieves the worker task from the worker threads list
+            work_task = self.work_pool.work_tasks_list[self.current_index]
+
+            # increments the current index
+            self.current_index += 1
+
+            # in case the work task conditions are met
+            if self.work_pool._check_conditions(work_task):
+                # breaks the cycle
+                break
+
+            # in case all the task have been checked (and invalidated)
+            if self.current_index == initial_current_index:
+                # invalidates the work task (no work task available)
+                work_task = None
+
+                # breaks the cycle
+                break
+
+        # releases the lock
+        self.work_tasks_list_lock.release()
+
+        # returns the work task
+        return work_task
