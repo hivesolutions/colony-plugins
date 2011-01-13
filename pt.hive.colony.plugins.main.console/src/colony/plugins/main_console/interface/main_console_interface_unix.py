13c01736e56336b32b80aee887bec742b3824451
João Magalhães <joamag@hive.pt>
2011-01-13 13:15:12 +0000

    tiny refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12672 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.console/src/colony/plugins/main_console/interface/main_console_interface_unix.py
+++ pt.hive.colony.plugins.main.console/src/colony/plugins/main_console/interface/main_console_interface_unix.py
@@ -187,7 +187,7 @@ class MainConsoleInterfaceUnix:
             # converts the character to ordinal
             character_ordinal = ord(character)
 
-            print character_ordinal
+            print character_ordinal,
 
             # processes the character
             if self.main_console_interface_character.process_character(character, character_ordinal):
