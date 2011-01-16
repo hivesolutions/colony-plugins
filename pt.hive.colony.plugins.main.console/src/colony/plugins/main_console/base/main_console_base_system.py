01a81ac8325404f1d342f5bd38e0e5da96fbeb85
João Magalhães <joamag@hive.pt>
2011-01-16 15:24:28 +0000

    tiny comment refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12733 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.console/src/colony/plugins/main_console/base/main_console_base_system.py
+++ pt.hive.colony.plugins.main.console/src/colony/plugins/main_console/base/main_console_base_system.py
@@ -234,6 +234,7 @@ class MainConsoleBase:
         # retrieves the plugin strings from the plugin manager instances
         plugins_string, replicas_string, instances_string = self.get_plugin_strings(plugin_manager_instances)
 
+        # prints the status values
         output_method("uid:          " + plugin_manager_uid)
         output_method("version:      " + plugin_manager_version)
         output_method("release:      " + plugin_manager_release)
