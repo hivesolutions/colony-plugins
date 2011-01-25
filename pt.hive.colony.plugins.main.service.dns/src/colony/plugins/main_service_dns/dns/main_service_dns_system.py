9143333ad69c6cb83032676b4f9acde892d57abb
João Magalhães <joamag@hive.pt>
2011-01-25 17:14:11 +0000

    new destroy parameters support
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12864 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
+++ pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
@@ -183,6 +183,9 @@ class MainServiceDns:
         @param parameters: The parameters to stop the service.
         """
 
+        # destroys the parameters
+        self._destroy_service_parameters(parameters)
+
         # starts the dns service
         self.dns_service.stop_service()
 
@@ -320,6 +323,18 @@ class MainServiceDns:
 
         # returns the parameters
         return parameters
+
+    def _destroy_service_parameters(self, parameters):
+        """
+        Destroys the service parameters map from the base parameters
+        map.
+
+        @type parameters: Dictionary
+        @param parameters: The base parameters map to be used to destroy
+        the final service parameters map.
+        """
+
+        pass
 
 class DnsClientServiceHandler:
     """
