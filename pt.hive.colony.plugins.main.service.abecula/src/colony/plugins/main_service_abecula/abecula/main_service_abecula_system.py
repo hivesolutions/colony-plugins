98671674f5453c4574b88da39570497ddaee1efa
João Magalhães <joamag@hive.pt>
2011-01-25 17:13:54 +0000

    new destroy parameters support
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@12863 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.abecula/src/colony/plugins/main_service_abecula/abecula/main_service_abecula_system.py
+++ pt.hive.colony.plugins.main.service.abecula/src/colony/plugins/main_service_abecula/abecula/main_service_abecula_system.py
@@ -181,6 +181,9 @@ class MainServiceAbecula:
         @param parameters: The parameters to stop the service.
         """
 
+        # destroys the parameters
+        self._destroy_service_parameters(parameters)
+
         # starts the abecula service
         self.abecula_service.stop_service()
 
@@ -318,6 +321,18 @@ class MainServiceAbecula:
 
         # returns the parameters
         return parameters
+
+    def _destroy_service_parameters(self, parameters):
+        """
+        Destroys the service parameters map from the base parameters
+        map.
+
+        @type parameters: Dictionary
+        @param parameters: The base parameters map to be used to destroy
+        the final service parameters map.
+        """
+
+        pass
 
 class AbeculaClientServiceHandler:
     """
