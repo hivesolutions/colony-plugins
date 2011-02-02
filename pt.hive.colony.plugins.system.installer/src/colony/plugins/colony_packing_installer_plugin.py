de3170ffeac0395d3b476adbfa368ada97c37655
João Magalhães <joamag@hive.pt>
2011-02-02 18:14:53 +0000

    new system installer structure
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13009 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.system.installer/src/colony/plugins/colony_packing_installer_plugin.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 7715 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2010-03-26 07:31:00 +0000 (sex, 26 Mar 2010) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+import colony.base.decorators
+
+class ColonyPackingInstallerPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Colony Packing Installer plugin.
+    """
+
+    id = "pt.hive.colony.plugins.system.installer.colony_packing_installer"
+    name = "Colony Packing Installer Plugin"
+    short_name = "Colony Packing Installer"
+    description = "Colony Packing Installer Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
+    attributes = {"build_automation_file_path" : "$base{plugin_directory}/system_updater/colony_packing/resources/baf.xml"}
+    capabilities = ["installer", "build_automation_item"]
+    capabilities_allowed = []
+    dependencies = [colony.base.plugin_system.PluginDependency(
+                    "pt.hive.colony.plugins.main.packing.manager", "1.0.0")]
+    events_handled = []
+    events_registrable = []
+    main_modules = ["system_updater.colony_packing.colony_packing_installer_exceptions",
+                    "system_updater.colony_packing.colony_packing_installer_system"]
+
+    colony_packing_installer = None
+
+    packing_manager_plugin = None
+    """ Plugin to for packing of files """
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        global system_updater
+        import system_updater.colony_packing.colony_packing_installer_system
+        self.colony_packing_installer = system_updater.colony_packing.colony_packing_installer_system.ColonyPackingInstaller(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.base.decorators.inject_dependencies("pt.hive.colony.plugins.system.updater.colony_packing_installer", "1.0.0")
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_installer_type(self):
+        return self.colony_packing_installer.get_installer_type()
+
+    def install_bundle(self, bundle_id, bundle_version, contents_file):
+        """
+        Method called upon deployment of the bundle with
+        the given id, version and contents file.
+
+        @type bundle_id: String
+        @param bundle_id: The id of the bundle to be installed.
+        @type bundle_version: String
+        @param bundle_version: The version of the bundle to be installed.
+        @type contents_file: ContentsFile
+        @param contents_file: The contents file of the bundle to
+        be installed.
+        """
+
+        return self.colony_packing_installer.install_bundle(bundle_id, bundle_version, contents_file)
+
+    def install_plugin(self, plugin_id, plugin_version, contents_file):
+        """
+        Method called upon installation of the plugin with
+        the given id, version and contents file.
+
+        @type plugin_id: String
+        @param plugin_id: The id of the plugin to be installed.
+        @type plugin_version: String
+        @param plugin_version: The version of the plugin to be installed.
+        @type contents_file: ContentsFile
+        @param contents_file: The contents file of the plugin to
+        be installed.
+        """
+
+        return self.colony_packing_installer.install_plugin(plugin_id, plugin_version, contents_file)
+
+    def get_packing_manager_plugin(self):
+        return self.packing_manager_plugin
+
+    @colony.base.decorators.plugin_inject("pt.hive.colony.plugins.main.packing.manager")
+    def set_packing_manager_plugin(self, packing_manager_plugin):
+        self.packing_manager_plugin = packing_manager_plugin
---
+++ pt.hive.colony.plugins.system.installer/src/colony/plugins/system_installer/colony_installer/colony_packing_installer_system.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2349 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-01 17:52:01 +0100 (qua, 01 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+INSTALLER_TYPE = "colony_packing"
+""" The installer type """
+
+class ColonyPackingInstaller:
+    """
+    The colony packing installer class.
+    """
+
+    colony_packing_installer_plugin = None
+    """ The colony packing installer plugin """
+
+    def __init__(self, colony_packing_installer_plugin):
+        """
+        Constructor of the class.
+
+        @type colony_packing_installer_plugin: ColonyPackingInstallerPlugin
+        @param colony_packing_installer_plugin: The colony packing installer plugin.
+        """
+
+        self.colony_packing_installer_plugin = colony_packing_installer_plugin
+
+    def load_installer(self):
+        """
+        Method called upon load of the installer.
+        """
+
+        self.colony_packing_installer_plugin.info("Loading colony packing installer")
+
+    def get_installer_type(self):
+        """
+        Retrieves the type of deployer.
+
+        @rtype: String
+        @return: The type of deployer.
+        """
+
+        return INSTALLER_TYPE
+
+    def deploy_bundle(self, bundle_id, bundle_version, contents_file):
+        """
+        Method called upon deployment of the bundle with
+        the given id, version and contents file.
+
+        @type bundle_id: String
+        @param bundle_id: The id of the bundle to be deployed.
+        @type bundle_version: String
+        @param bundle_version: The version of the bundle to be deployed.
+        @type contents_file: ContentsFile
+        @param contents_file: The contents file of the bundle to
+        be deployed.
+        """
+
+        pass
+
+#        # retrieves the plugin manager
+#        plugin_manager = self.colony_packing_deployer_plugin.manager
+#
+#        # retrieves the packing manager plugin
+#        packing_manager_plugin = self.colony_packing_deployer_plugin.packing_manager_plugin
+#
+#        # retrieves the main plugin path as the plugin path
+#        # for deployment
+#        plugin_path = plugin_manager.get_main_plugin_path()
+#
+#        # creates the properties map for the file unpacking packing
+#        properties = {TARGET_PATH_VALUE : plugin_path}
+#
+#        # unpacks the files using the colony service
+#        packing_manager_plugin.unpack_files([contents_file.name], properties, COLONY_VALUE)
+
+    def deploy_plugin(self, plugin_id, plugin_version, contents_file):
+        """
+        Method called upon deployment of the plugin with
+        the given id, version and contents file.
+
+        @type plugin_id: String
+        @param plugin_id: The id of the plugin to be deployed.
+        @type plugin_version: String
+        @param plugin_version: The version of the plugin to be deployed.
+        @type contents_file: ContentsFile
+        @param contents_file: The contents file of the plugin to
+        be deployed.
+        """
+
+        pass
+
+#        # retrieves the plugin manager
+#        plugin_manager = self.colony_packing_deployer_plugin.manager
+#
+#        # retrieves the packing manager plugin
+#        packing_manager_plugin = self.colony_packing_deployer_plugin.packing_manager_plugin
+#
+#        # retrieves the main plugin path as the plugin path
+#        # for deployment
+#        plugin_path = plugin_manager.get_main_plugin_path()
+#
+#        # creates the properties map for the file unpacking packing
+#        properties = {TARGET_PATH_VALUE : plugin_path}
+#
+#        # unpacks the files using the colony service
+#        packing_manager_plugin.unpack_files([contents_file.name], properties, COLONY_VALUE)
