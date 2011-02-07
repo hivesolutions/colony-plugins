dbb2d0213290579ca0d33bd569bf6b70c06633c6
João Magalhães <joamag@hive.pt>
2011-02-07 22:49:12 +0000

    new system installer structure
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13113 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.system.installer/src/colony/plugins/system_installer/colony_packing/colony_packing_installer_system.py
+++ pt.hive.colony.plugins.system.installer/src/colony/plugins/system_installer/colony_packing/colony_packing_installer_system.py
@@ -40,9 +40,8 @@ __license__ = "GNU General Public License (GPL), Version 3"
 import os
 import time
 import datetime
-import threading
 
-import colony.libs.path_util
+import colony.libs.file_util
 
 import colony_packing_installer_exceptions
 
@@ -54,6 +53,21 @@ ID_VALUE = "id"
 
 VERSION_VALUE = "version"
 """ The version value """
+
+TIMESTAMP_VALUE = "timestamp"
+""" The timestamp value """
+
+UPGRADE_VALUE = "upgrade"
+""" The upgrade value """
+
+FORCE_VALUE = "force"
+""" The force value """
+
+LAST_MODIFIED_TIMESTAMP_VALUE = "last_modified_timestamp"
+""" The last modified timestamp value """
+
+LAST_MODIFIED_DATE_VALUE = "last_modified_date"
+""" The last modified date value """
 
 class ColonyPackingInstaller:
     """
@@ -137,20 +151,14 @@ class ColonyPackingInstaller:
         plugins_file_path = os.path.join(manager_path, "var/plugins.json")
 
         # creates a new file transaction context
-        file_context = file_context or FileTransactionContext("c:\\transactions\\")
+        file_context = file_context or colony.libs.file_util.FileTransactionContext("c:\\transactions\\")
 
         # opens a new transaction in the file context
         file_context.open()
 
         try:
-            # ------ HARDCODES ----------
-
-            CPX_PATH = "c:\\test.cpx"
-
-            # ----------------------------
-
             # retrieves the packing information
-            packing_information = packing_manager_plugin.get_packing_information(CPX_PATH, {}, "colony")
+            packing_information = packing_manager_plugin.get_packing_information(file_path, {}, "colony")
 
             # retrieves the plugin id
             plugin_id = packing_information.get_property(ID_VALUE)
@@ -167,15 +175,9 @@ class ColonyPackingInstaller:
             # tenho tb de verificar conflicto de plgugins
             # se tiver a mesma versao so com force posso eu fazer deploy
 
-            # HARDCODES
-
-            # -----------------------------------------
-
             # retrieves the flag properties values
-            upgrade = properties.get("upgrade", True)
-            force = properties.get("force", False)
-
-            # -----------------------------------------------------
+            upgrade = properties.get(UPGRADE_VALUE, True)
+            force = properties.get(FORCE_VALUE, False)
 
             # reads the plugin file contents
             plugins_file_contents = file_context.read_file(plugins_file_path)
@@ -196,7 +198,7 @@ class ColonyPackingInstaller:
                 raise colony_packing_installer_exceptions.PluginInstallationError("plugin already installed")
 
             # retrieves the installed plugin version
-            installed_plugin_version = installed_plugin.get("version", None)
+            installed_plugin_version = installed_plugin.get(VERSION_VALUE, None)
 
             # in case the installed plugin version is the same as the
             # plugin version and the force flag is not set
@@ -214,7 +216,7 @@ class ColonyPackingInstaller:
             main_plugin_virtual_path = file_context.get_file_path(main_plugin_path)
 
             # deploys the package using the main plugin "virtual" path
-            self._deploy_package(CPX_PATH, main_plugin_virtual_path)
+            self._deploy_package(file_path, main_plugin_virtual_path)
 
             # --------------------------------------
 
@@ -228,12 +230,12 @@ class ColonyPackingInstaller:
             current_date_time_formated = current_date_time.strftime("%d-%m-%Y %H:%M:%S")
 
             # sets the installed plugin map
-            installed_plugins[plugin_id] = {"version" : plugin_version, "timestamp" : current_time}
+            installed_plugins[plugin_id] = {VERSION_VALUE : plugin_version, TIMESTAMP_VALUE : current_time}
 
             # updates the plugins map with the current time
             # and date time values
-            plugins["last_modified_timestamp"] = current_time
-            plugins["last_modified_date"] = current_date_time_formated
+            plugins[LAST_MODIFIED_TIMESTAMP_VALUE] = current_time
+            plugins[LAST_MODIFIED_DATE_VALUE] = current_date_time_formated
 
             # serializes the plugins (in pretty mode)
             plugins_serialized = json_plugin.dumps_pretty(plugins)
@@ -268,238 +270,3 @@ class ColonyPackingInstaller:
 
         # unpacks the files using the colony service
         packing_manager_plugin.unpack_files([package_path], properties, "colony")
-
-class FileContext:
-    """
-    The file context class used to read and write
-    contents from files.
-    """
-
-    def __init__(self):
-        """
-        Constructor of the class.
-        """
-
-        pass
-
-    def read_file(self, file_path):
-        # open the file
-        file = open(file_path, "rb")
-
-        try:
-            # reads the file contents
-            file_contents = file.read()
-        finally:
-            # closes the file
-            file.close()
-
-        # returns the file contents
-        return file_contents
-
-    def write_file(self, file_path, file_contents):
-        # creates the directory for the file path
-        self._create_directory(file_path)
-
-        # open the file
-        file = open(file_path, "wb")
-
-        try:
-            # writes the file contents
-            file.write(file_contents)
-        finally:
-            # closes the file
-            file.close()
-
-    def get_file_path(self, file_path):
-        # returns the file path
-        return file_path
-
-    def _create_directory(self, file_path):
-        # retrieves the directory path for the file path
-        directory_path = os.path.dirname(file_path)
-
-        # in case the directory path exists
-        if os.path.exists(directory_path):
-            # returns immediately
-            return
-
-        # creates the various required directories
-        os.makedirs(directory_path)
-
-class FileTransactionContext(FileContext):
-    """
-    The file transaction context class that controls
-    a transaction involving the file system.
-    """
-
-    transaction_level = 0
-    """ The current transaction level in use """
-
-    temporary_path = None
-    """ The temporary path to be used in the file transaction """
-
-    target_path = None
-    """ The target path """
-
-    path_tuples_list = []
-    """ The list of path tuples associated with the transaction """
-
-    access_lock = None
-    """ The lock controlling the access to the file transaction """
-
-    def __init__(self, temporary_path):
-        """
-        Constructor of the class.
-
-        @type temporary_path: String
-        @param temporary_path: The temporary path to be used
-        for the transaction temporary files.
-        """
-
-        FileContext.__init__(self)
-        self.temporary_path = temporary_path
-
-        self.path_tuples_list = []
-        self.access_lock = threading.RLock()
-
-    def write_file(self, file_path, file_contents):
-        # retrieves the virtual file path for the file path
-        virtual_file_path = self._get_virtual_file_path(file_path)
-
-        # writes the file using the file context (virtual file path used)
-        FileContext.write_file(self, virtual_file_path, file_contents)
-
-        # creates a path tuple with the virtual file path
-        # and the file path
-        path_tuple = (virtual_file_path, file_path)
-
-        # adds the path tuple
-        self._add_path_tuple(path_tuple)
-
-    def get_file_path(self, file_path):
-        # retrieves the virtual file path for the file path
-        virtual_file_path = self._get_virtual_file_path(file_path)
-
-        # creates a path tuple with the virtual file path
-        # and the file path
-        path_tuple = (virtual_file_path, file_path)
-
-        # adds the path tuple
-        self._add_path_tuple(path_tuple)
-
-        # returns the virtual file path
-        return virtual_file_path
-
-    def open(self):
-        # acquires the access lock
-        self.access_lock.acquire()
-
-        try:
-            # increments the transaction level
-            self.transaction_level += 1
-        finally:
-            # releases the access lock
-            self.access_lock.release()
-
-    def commit(self):
-        # acquires the access lock
-        self.access_lock.acquire()
-
-        try:
-            # decrements the transaction level
-            self.transaction_level -= 1
-
-            # in case the transaction level is positive
-            if self.transaction_level > 0:
-                # returns immediately
-                return
-            # in case the transaction level is negative
-            elif self.transaction_level < 0:
-                # raises the runtime error
-                raise RuntimeError("Invalid transaction level")
-
-            # iterates over all the path tuples in
-            # path tuples list
-            for path_tuple in self.path_tuples_list:
-                # unpacks the path tuple
-                virtual_file_path, file_path = path_tuple
-
-                # in case the virtual file path is a directory
-                if os.path.isdir(virtual_file_path):
-                    # copies the directory in the virtual path to the directory in the file path
-                    colony.libs.path_util.copy_directory(virtual_file_path, file_path)
-                # otherwise it must be a "normal" file
-                else:
-                    # copies the file in the virtual path to the file in the file path
-                    colony.libs.path_util.copy_file(virtual_file_path, file_path)
-
-            # runs the cleanup
-            self._cleanup()
-        finally:
-            # empties the path tuples list
-            self.path_tuples_list = []
-
-            # resets the transaction level
-            self.transaction_level = 0
-
-            # releases the access lock
-            self.access_lock.release()
-
-    def rollback(self):
-        # acquires the access lock
-        self.access_lock.acquire()
-
-        try:
-            # runs the cleanup
-            self._cleanup()
-        finally:
-            # empties the path tuples list
-            self.path_tuples_list = []
-
-            # resets the transaction level
-            self.transaction_level = 0
-
-            # releases the access lock
-            self.access_lock.release()
-
-    def _cleanup(self):
-        # retrieves the temporary path items
-        temporary_path_items = os.listdir(self.temporary_path)
-
-        # iterates over all the temporary path items
-        for temporary_path_item in temporary_path_items:
-            # creates the temporary complete path item, by joining the
-            # temporary path and the temporary path item
-            temporary_complete_path_item = os.path.join(self.temporary_path, temporary_path_item)
-
-            # in case the temporary item is a directory
-            if os.path.isdir(temporary_complete_path_item):
-                # removes the directory in the temporary
-                # complete path item
-                colony.libs.path_util.remove_directory(temporary_complete_path_item)
-            # otherwise it must be a "normal" file
-            else:
-                # removes the temporary complete path item
-                os.remove(temporary_complete_path_item)
-
-    def _add_path_tuple(self, path_tuple):
-        # adds the path tuple to the path tuples list
-        self.path_tuples_list.append(path_tuple)
-
-    def _get_virtual_file_path(self, file_path):
-        # splits the file path into drive and
-        # base file path
-        _drive, base_file_path = os.path.splitdrive(file_path)
-
-        # strips the base file path
-        base_file_path = base_file_path.lstrip("\\/")
-
-        # joins the temporary path and the
-        # base file path
-        virtual_file_path = os.path.join(self.temporary_path, base_file_path)
-
-        # normalizes the virtual file path
-        virtual_file_path = os.path.normpath(virtual_file_path)
-
-        # returns the virtual file path
-        return virtual_file_path
