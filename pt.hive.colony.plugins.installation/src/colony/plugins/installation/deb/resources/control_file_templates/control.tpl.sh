39bebe0cc8e4b21da661d9586fd9aa5d005e95ec
João Magalhães <joamag@hive.pt>
2011-02-11 17:09:04 +0000

    new code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13150 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.installation/src/colony/plugins/installation/deb/resources/control_file_templates/control.tpl.sh
+++ pt.hive.colony.plugins.installation/src/colony/plugins/installation/deb/resources/control_file_templates/control.tpl.sh
@@ -1,1 +1,1 @@
-${out_map value=package key_map=package_keys key_separator=": " item_separator="\n" allow_empty=False /}
+${out_map value=package key_map=package_keys key_separator=": " allow_empty=False /}
