e7645e78fd2863bc2eede73b57ee194bff9a0e1d
João Magalhães <joamag@hive.pt>
2011-04-25 17:29:21 +0000

    removed unused configuration values
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13534 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/main_service_http/run_mode/test/configuration.py
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/main_service_http/run_mode/test/configuration.py
@@ -54,33 +54,5 @@ configuration = {
     "default_maximum_number_work_threads" : 15,
     "default_work_scheduling_algorithm" : 3,
     "contexts" : {
-        "/colony_web" : {
-            "handler" : "file",
-            "request_properties" : {
-                "base_path" : "$resource{system.path.colony_web}/pt.hive.colony.web",
-                "default_page" : "index.html"
-            }
-        },
-        "/colony_web/plugins" : {
-            "handler" : "colony",
-            "request_properties" : {
-                "plugin_handler" : "pt.hive.colony.plugins.javascript.file_handler"
-            }
-        },
-        "/colony_manager" : {
-            "handler" : "colony",
-            "request_properties" : {}
-        },
-        "/colony_mod_python" : {
-            "handler" : "colony",
-            "request_properties" : {}
-        },
-        "/eclipse" : {
-            "handler" : "file",
-            "request_properties" : {
-                "base_path" : "$resource{system.path.colony}/pt.hive.eclipse.plugins.site",
-                "default_page" : "site.xml"
-            }
-        }
     }
 }
