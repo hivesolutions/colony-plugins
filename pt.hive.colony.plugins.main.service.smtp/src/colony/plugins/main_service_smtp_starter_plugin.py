f2d4aa5662aeb095ac856f91c0c2acdb4e67bf0c
Tiago Silva <tsilva@hive.pt>
2011-04-27 18:43:39 +0000

    formatted maps in pt.hive.colony.plugins.main.service.smtp
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13591 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.smtp/src/colony/plugins/main_service_smtp_starter_plugin.py
+++ pt.hive.colony.plugins.main.service.smtp/src/colony/plugins/main_service_smtp_starter_plugin.py
@@ -77,7 +77,13 @@ class MainServiceSmtpStarterPlugin(colony.base.plugin_system.Plugin):
         # notifies the ready semaphore
         self.release_ready_semaphore()
 
-        self.main_service_smtp_plugin.start_service({"socket_provider" : "normal", "port" : 25})
+        # defines the parameters
+        parameters = {
+            "socket_provider" : "normal",
+            "port" : 25
+        }
+
+        self.main_service_smtp_plugin.start_service(parameters)
 
     def unload_plugin(self):
         colony.base.plugin_system.Plugin.unload_plugin(self)
