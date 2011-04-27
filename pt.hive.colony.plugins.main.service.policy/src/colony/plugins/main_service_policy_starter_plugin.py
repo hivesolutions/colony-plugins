eee0b51f90b8c2c07c8455d24d33ece060aa07ac
Tiago Silva <tsilva@hive.pt>
2011-04-27 18:44:15 +0000

    formatted maps in pt.hive.colony.plugins.main.service.policy
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13593 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy_starter_plugin.py
+++ pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy_starter_plugin.py
@@ -78,7 +78,13 @@ class MainServicePolicyStarterPlugin(colony.base.plugin_system.Plugin):
         # notifies the ready semaphore
         self.release_ready_semaphore()
 
-        self.main_service_policy_plugin.start_service({"socket_provider" : "normal", "port" : 843})
+        # defines the parameters
+        parameters = {
+            "socket_provider" : "normal",
+            "port" : 843
+        }
+
+        self.main_service_policy_plugin.start_service(parameters)
 
     def unload_plugin(self):
         colony.base.plugin_system.Plugin.unload_plugin(self)
