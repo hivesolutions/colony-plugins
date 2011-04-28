606bb1e270587e6a9f68309fbc691d8d9a53d4e3
Tiago Silva <tsilva@hive.pt>
2011-04-28 18:37:32 +0000

    formatted plugins in pt.hive.colony.plugins.main.remote.rest.encoder
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13676 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.remote.rest.encoder/src/colony/plugins/main_rest_encoder_json_plugin.py
+++ pt.hive.colony.plugins.main.remote.rest.encoder/src/colony/plugins/main_rest_encoder_json_plugin.py
@@ -52,19 +52,28 @@ class MainRestEncoderJsonPlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_remote_rest_encoder/json/resources/baf.xml"}
-    capabilities = ["rest_encoder", "build_automation_item"]
-    capabilities_allowed = []
-    dependencies = [colony.base.plugin_system.PluginDependency(
-                    "pt.hive.colony.plugins.misc.json", "1.0.0")]
-    events_handled = []
-    events_registrable = []
-    main_modules = ["main_remote_rest_encoder.json.main_rest_encoder_json_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/main_remote_rest_encoder/json/resources/baf.xml"
+    }
+    capabilities = [
+        "rest_encoder",
+        "build_automation_item"
+    ]
+    dependencies = [
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.misc.json", "1.0.0")
+    ]
+    main_modules = [
+        "main_remote_rest_encoder.json.main_rest_encoder_json_system"
+    ]
 
     main_rest_encoder_json = None
+    """ The main rest encoder json """
 
     json_plugin = None
+    """ The json plugin """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
--- pt.hive.colony.plugins.main.remote.rest.encoder/src/colony/plugins/main_rest_encoder_xml_plugin.py
+++ pt.hive.colony.plugins.main.remote.rest.encoder/src/colony/plugins/main_rest_encoder_xml_plugin.py
@@ -51,16 +51,22 @@ class MainRestEncoderXmlPlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_remote_rest_encoder/xml/resources/baf.xml"}
-    capabilities = ["rest_encoder", "build_automation_item"]
-    capabilities_allowed = []
-    dependencies = []
-    events_handled = []
-    events_registrable = []
-    main_modules = ["main_remote_rest_encoder.xml.main_rest_encoder_xml_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/main_remote_rest_encoder/xml/resources/baf.xml"
+    }
+    capabilities = [
+        "rest_encoder",
+        "build_automation_item"
+    ]
+    main_modules = [
+        "main_remote_rest_encoder.xml.main_rest_encoder_xml_system"
+    ]
 
     main_rest_encoder_xml = None
+    """ The main rest encoder xml """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
