dbcfd890964a5de60aaedbfb11f9b3ad539782d8
Tiago Silva <tsilva@hive.pt>
2011-04-28 17:57:08 +0000

    formatted plugins pt.hive.colony.plugins.packaging
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13650 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
+++ pt.hive.colony.plugins.packaging/src/colony/plugins/packaging_ar_plugin.py
@@ -51,17 +51,23 @@ class PackagingArPlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/packaging/ar/resources/baf.xml"}
-    capabilities = ["packaging.ar", "build_automation_item"]
-    capabilities_allowed = []
-    dependencies = []
-    events_handled = []
-    events_registrable = []
-    main_modules = ["packaging.ar.packaging_ar_exceptions",
-                    "packaging.ar.packaging_ar_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/packaging/ar/resources/baf.xml"
+    }
+    capabilities = [
+        "packaging.ar",
+        "build_automation_item"
+    ]
+    main_modules = [
+        "packaging.ar.packaging_ar_exceptions",
+        "packaging.ar.packaging_ar_system"
+    ]
 
     packaging_ar = None
+    """ The packaging ar """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
