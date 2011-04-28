7cb766a5ba650aa84c848807cb4adc8bd7355ea0
Tiago Silva <tsilva@hive.pt>
2011-04-28 18:39:08 +0000

    formatted plugins in pt.hive.colony.plugins.main.packing
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13680 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.packing/src/colony/plugins/main_packing_manager_plugin.py
+++ pt.hive.colony.plugins.main.packing/src/colony/plugins/main_packing_manager_plugin.py
@@ -52,21 +52,34 @@ class MainPackingManagerPlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/main_packing/manager/resources/baf.xml"}
-    capabilities = ["packing_manager", "_console_command_extension", "build_automation_item"]
-    capabilities_allowed = ["packing_service"]
-    dependencies = []
-    events_handled = []
-    events_registrable = []
-    main_modules = ["main_packing.manager.console_main_packing_manager",
-                    "main_packing.manager.main_packing_manager_exceptions",
-                    "main_packing.manager.main_packing_manager_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/main_packing/manager/resources/baf.xml"
+    }
+    capabilities = [
+        "packing_manager",
+        "_console_command_extension",
+        "build_automation_item"
+    ]
+    capabilities_allowed = [
+        "packing_service"
+    ]
+    main_modules = [
+        "main_packing.manager.console_main_packing_manager",
+        "main_packing.manager.main_packing_manager_exceptions",
+        "main_packing.manager.main_packing_manager_system"
+    ]
 
     main_packing_manager = None
+    """ The main packing manager """
+
     console_main_packing_manager = None
+    """ The console main packing manager """
 
     packing_service_plugins = []
+    """ The packing service plugins """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
