d29ab363bbc91b57a50c1cfaa7ce382b31fee61b
Tiago Silva <tsilva@hive.pt>
2011-04-29 14:17:42 +0000

    formatted dns storage database plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13725 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.dns.storage/src/colony/plugins/dns_storage_database_plugin.py
+++ pt.hive.colony.plugins.dns.storage/src/colony/plugins/dns_storage_database_plugin.py
@@ -51,21 +51,30 @@ class DnsStorageDatabasePlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/dns_storage_database/database/resources/baf.xml"}
-    capabilities = ["dns_storage", "build_automation_item"]
-    capabilities_allowed = []
-    dependencies = [colony.base.plugin_system.PluginDependency(
-                    "pt.hive.colony.plugins.data.entity_manager_helper", "1.0.0")]
-    events_handled = []
-    events_registrable = []
-    main_modules = ["dns_storage_database.database.dns_storage_database_entities",
-                    "dns_storage_database.database.dns_storage_database_exceptions",
-                    "dns_storage_database.database.dns_storage_database_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/dns_storage_database/database/resources/baf.xml"
+    }
+    capabilities = [
+        "dns_storage",
+        "build_automation_item"
+    ]
+    dependencies = [
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.data.entity_manager_helper", "1.0.0")
+    ]
+    main_modules = [
+        "dns_storage_database.database.dns_storage_database_entities",
+        "dns_storage_database.database.dns_storage_database_exceptions",
+        "dns_storage_database.database.dns_storage_database_system"
+    ]
 
     dns_storage_database = None
+    """ The dns storage database """
 
     entity_manager_helper_plugin = None
+    """ The entity manager helper plugin """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
