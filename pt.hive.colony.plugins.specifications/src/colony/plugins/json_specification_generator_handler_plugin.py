e7559f2a67fb9f77084d993302321519cbbc5d06
Tiago Silva <tsilva@hive.pt>
2011-04-29 14:21:28 +0000

    formatted plugins in pt.hive.colony.plugins.specifications
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13739 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.specifications/src/colony/plugins/json_specification_generator_handler_plugin.py
+++ pt.hive.colony.plugins.specifications/src/colony/plugins/json_specification_generator_handler_plugin.py
@@ -52,19 +52,28 @@ class JsonSpecificationGeneratorHandlerPlugin(colony.base.plugin_system.Plugin):
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
     loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
-    platforms = [colony.base.plugin_system.CPYTHON_ENVIRONMENT]
-    attributes = {"build_automation_file_path" : "$base{plugin_directory}/specifications/json_specification_generator_handler/resources/baf.xml"}
-    capabilities = ["specification_generator_handler", "build_automation_item"]
-    capabilities_allowed = []
-    dependencies = [colony.base.plugin_system.PluginDependency(
-                    "pt.hive.colony.plugins.template_engine.manager", "1.0.0")]
-    events_handled = []
-    events_registrable = []
-    main_modules = ["specifications.json_specification_generator_handler.json_specification_generator_handler_system"]
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/specifications/json_specification_generator_handler/resources/baf.xml"
+    }
+    capabilities = [
+        "specification_generator_handler",
+        "build_automation_item"
+    ]
+    dependencies = [
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.template_engine.manager", "1.0.0")
+    ]
+    main_modules = [
+        "specifications.json_specification_generator_handler.json_specification_generator_handler_system"
+    ]
 
     json_specification_generator_handler = None
+    """ The json specification generator handler """
 
     template_engine_manager_plugin = None
+    """ The template engine manager plugin """
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
