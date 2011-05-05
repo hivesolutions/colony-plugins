adcf902a569be47ec95d09f5a4d3523faad53530
João Magalhães <joamag@hive.pt>
2011-05-05 18:22:14 +0000

    new plugin added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@13969 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.system.registry/src/colony/plugins/system_registry/registry/system_registry_system.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 13947 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2011-05-05 18:29:17 +0100 (qui, 05 Mai 2011) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+class SystemRegistry:
+    """
+    The system registry class.
+    """
+
+    system_registry_plugin = None
+    """ The system registry plugin """
+
+
+    def __init__(self, system_registry_plugin):
+        """
+        Constructor of the class.
+
+        @type system_registry_plugin: SystemRegistryPlugin
+        @param system_registry_plugin: The system registry plugin.
+        """
+
+        self.system_registry_plugin = system_registry_plugin
