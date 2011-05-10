10b91c00d59f1f8ba3e6377a3e6ad14a444051ab
João Magalhães <joamag@hive.pt>
2011-05-10 17:30:07 +0000

    new user information added
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14057 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/information_user/configuration.py
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/information_user/configuration.py
@@ -41,28 +41,134 @@ configuration = {
     "user_information" : {
         "joamag" : {
             "name" : u"João Magalhães",
+            "first_name" : u"João",
+            "last_name" : u"Magalhães",
             "email" : u"joamag@hive.pt",
-            "username" : u"joamag"
+            "username" : u"joamag",
+            "website" : u"http://joaomagalhaes.eu",
+            "images" : {
+                "gravatar" : u"http://www.gravatar.com/avatar/639590e6cd348a5581826da23b78511a"
+            },
+            "social" : {
+                "gravatar" : u"http://gravatar.com/joamag",
+                "facebook" : u"http://facebook.com/joamag",
+                "twitter" : u"http://twitter.com/joamag",
+            },
+            "work" : {
+                "company" : "Hive Solutions",
+                "position" : u"Software Developer",
+                "email" : u"joamag@hive.pt",
+                "address" : {
+                    "street" : u"Rua 31 de Janeiro, 190A 3º Dto.",
+                    "zip_code" : u"4000-542",
+                    "city" : u"Porto",
+                    "country" : u"Portugal",
+                }
+            }
         },
         "tsilva" : {
             "name" : u"Tiago Silva",
+            "first_name" : u"Tiago",
+            "last_name" : u"Silva",
             "email" : u"tsilva@hive.pt",
-            "username" : u"tsilva"
+            "username" : u"tsilva",
+            "website" : u"http://tiagosilva.me",
+            "images" : {
+                "gravatar" : u"http://www.gravatar.com/avatar/b2e064935878cfd489107331aaa296d3"
+            },
+            "social" : {
+                "facebook" : u"http://facebook.com/eng.tiago.silva",
+                "twitter" : u"http://twitter.com/tiagosilva",
+            },
+            "work" : {
+                "company" : "Hive Solutions",
+                "position" : u"Software Developer",
+                "email" : u"tsilva@hive.pt",
+                "address" : {
+                    "street" : u"Rua 31 de Janeiro, 190A 3º Dto.",
+                    "zip_code" : u"4000-542",
+                    "city" : u"Porto",
+                    "country" : u"Portugal",
+                }
+            }
         },
         "lmartinho" : {
             "name" : u"Luis Martinho",
+            "first_name" : u"Luis",
+            "last_name" : u"Martinho",
             "email" : u"lmartinho@hive.pt",
-            "username" : u"lmartinho"
+            "username" : u"lmartinho",
+            "website" : u"http://luismartinho.com",
+            "images" : {
+                "gravatar" : u"http://www.gravatar.com/avatar/8262b6b2a430566de13df6da0c481846"
+            },
+            "social" : {
+                "facebook" : u"http://facebook.com/lmartinho",
+                "twitter" : u"http://twitter.com/lmartinho",
+            },
+            "work" : {
+                "company" : "Hive Solutions",
+                "position" : u"Software Developer",
+                "email" : u"lmartinho@hive.pt",
+                "address" : {
+                    "street" : u"Rua 31 de Janeiro, 190A 3º Dto.",
+                    "zip_code" : u"4000-542",
+                    "city" : u"Porto",
+                    "country" : u"Portugal",
+                }
+            }
         },
         "v-fcastro" : {
             "name" : u"Francisco Castro",
+            "first_name" : u"Francisco",
+            "last_name" : u"Castro",
             "email" : u"v-fcastro@hive.pt",
-            "username" : u"v-fcastro"
+            "username" : u"v-fcastro",
+            "images" : {
+                "gravatar" : u"http://www.gravatar.com/avatar/e8f4b05866847b6d9e9638c40ce77d93"
+            },
+            "social" : {
+                "facebook" : u"http://facebook.com/castromatic",
+                "twitter" : u"http://twitter.com/frcastro",
+            },
+            "work" : {
+                "company" : "Hive Solutions",
+                "position" : u"Designer",
+                "email" : u"v-fcastro@hive.pt",
+                "address" : {
+                    "street" : u"Rua 31 de Janeiro, 190A 3º Dto.",
+                    "zip_code" : u"4000-542",
+                    "city" : u"Porto",
+                    "country" : u"Portugal",
+                }
+            }
         },
         "v-msousa" : {
             "name" : u"Marco Sousa",
+            "first_name" : u"Marco",
+            "last_name" : u"Sousa",
             "email" : u"v-msousa@hive.pt",
-            "username" : u"v-msousa"
+            "username" : u"v-msousa",
+            "website" : u"http://marcosousa.me",
+            "images" : {
+                "gravatar" : u"http://www.gravatar.com/avatar/e7f7880c089f0d104df7f088dbc213e9"
+            },
+            "social" : {
+                "facebook" : u"http://facebook.com/sousa.marco",
+                "twitter" : u"http://twitter.com/h1brd",
+                "dribbble" : u"http://dribbble.com/h1brd",
+            },
+            "work" : {
+                "company" : "Hive Solutions",
+                "position" : u"Designer",
+                "email" : u"v-msousa@hive.pt",
+                "address" : {
+                    "street" : u"Rua 31 de Janeiro, 190A 3º Dto.",
+                    "zip_code" : u"4000-542",
+                    "city" : u"Porto",
+                    "country" : u"Portugal",
+                }
+            }
         }
     }
 }
