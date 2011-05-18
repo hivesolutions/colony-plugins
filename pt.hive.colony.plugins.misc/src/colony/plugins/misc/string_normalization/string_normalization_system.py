feaae1abf3145ddc85fbc1dc23fa4c78072d9ae3
João Magalhães <joamag@hive.pt>
2011-05-18 23:15:28 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14244 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.misc/src/colony/plugins/misc/string_normalization/string_normalization_system.py
+++ pt.hive.colony.plugins.misc/src/colony/plugins/misc/string_normalization/string_normalization_system.py
@@ -37,10 +37,9 @@ __copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import os
 import sys
 import stat
-
-import os.path
 
 import colony.libs.string_buffer_util
 
