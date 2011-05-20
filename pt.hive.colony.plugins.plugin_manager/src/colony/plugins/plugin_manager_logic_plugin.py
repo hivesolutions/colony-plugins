4ff9772ca34d3ff52b326a577e47dfc05cf869f2
Tiago Silva <tsilva@hive.pt>
2011-05-20 09:20:29 +0000

    removed globals from plugin manager logic plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14307 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.plugin_manager/src/colony/plugins/plugin_manager_logic_plugin.py
+++ pt.hive.colony.plugins.plugin_manager/src/colony/plugins/plugin_manager_logic_plugin.py
@@ -75,7 +75,6 @@ class PluginManagerLogicPlugin(colony.base.plugin_system.Plugin):
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
-        global plugin_manager_logic
         import plugin_manager_logic.logic.plugin_manager_logic_system
         self.plugin_manager_logic = plugin_manager_logic.logic.plugin_manager_logic_system.PluginManagerLogic(self)
 
