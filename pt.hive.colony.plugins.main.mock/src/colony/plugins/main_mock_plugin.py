bbd6f23907c6a87e92381e73907c99b9ae8acb6c
Tiago Silva <tsilva@hive.pt>
2011-05-20 09:30:18 +0000

    removed globals from main mock plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14332 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.mock/src/colony/plugins/main_mock_plugin.py
+++ pt.hive.colony.plugins.main.mock/src/colony/plugins/main_mock_plugin.py
@@ -72,7 +72,6 @@ class MainMockPlugin(colony.base.plugin_system.Plugin):
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
-        global main_mock
         import main_mock.mock.main_mock_system
         self.main_mock = main_mock.mock.main_mock_system.MainMock(self)
 
