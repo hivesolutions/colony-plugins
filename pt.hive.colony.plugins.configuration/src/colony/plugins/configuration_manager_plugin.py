803f24c74ae91339ad2b533c969dbf59db61c0db
Tiago Silva <tsilva@hive.pt>
2011-05-20 09:32:49 +0000

    removed globals from configuration manager plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14339 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.configuration/src/colony/plugins/configuration_manager_plugin.py
+++ pt.hive.colony.plugins.configuration/src/colony/plugins/configuration_manager_plugin.py
@@ -78,7 +78,6 @@ class ConfigurationManagerPlugin(colony.base.plugin_system.Plugin):
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
-        global configuration_manager
         import configuration_manager.manager.configuration_manager_system
         self.configuration_manager = configuration_manager.manager.configuration_manager_system.ConfigurationManager(self)
 
