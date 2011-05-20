3e0759bc69311d004162bd1a2a2fb02f4def22b9
Tiago Silva <tsilva@hive.pt>
2011-05-20 09:29:28 +0000

    removed globals from main storage buffer manager plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14329 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.storage/src/colony/plugins/main_storage_buffer_manager_plugin.py
+++ pt.hive.colony.plugins.main.storage/src/colony/plugins/main_storage_buffer_manager_plugin.py
@@ -71,7 +71,6 @@ class MainStorageBufferManagerPlugin(colony.base.plugin_system.Plugin):
 
     def load_plugin(self):
         colony.base.plugin_system.Plugin.load_plugin(self)
-        global main_storage_buffer_manager
         import main_storage_buffer_manager.buffer_manager.main_storage_buffer_manager_system
         self.main_storage_buffer_manager = main_storage_buffer_manager.buffer_manager.main_storage_buffer_manager_system.MainStorageBufferManager(self)
 
