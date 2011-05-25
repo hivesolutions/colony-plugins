3b5748a3ca9dfffd2faa51d8eecd439a64e98ba0
João Magalhães <joamag@hive.pt>
2011-05-25 12:21:24 +0000

    new yaml and conf plugins
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14377 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.resources/src/colony/plugins/conf_resource_parser_plugin.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2688 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-16 12:24:34 +0100 (qui, 16 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+import colony.base.decorators
+
+class confResourceParserPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Conf Resource Parser plugin.
+    """
+
+    id = "pt.hive.colony.plugins.resources.conf_resource_parser"
+    name = "Conf Resource Parser Plugin"
+    short_name = "Conf Resource Parser"
+    description = "A plugin to parse conf resources"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/resources/conf_resource_parser/resources/baf.xml"
+    }
+    capabilities = [
+        "resource_parser",
+        "build_automation_item"
+    ]
+    dependencies = [
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.misc.conf", "1.0.0")
+    ]
+    main_modules = [
+        "resources.conf_resource_parser.conf_resource_parser_system"
+    ]
+
+    conf_resource_parser = None
+    """ The conf resource parser """
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        import resources.conf_resource_parser.conf_resource_parser_system
+        self.conf_resource_parser = resources.conf_resource_parser.conf_resource_parser_system.ConfResourceParser(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.base.decorators.inject_dependencies("pt.hive.colony.plugins.resources.conf_resource_parser", "1.0.0")
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_resource_parser_name(self):
+        return self.conf_resource_parser.get_resource_parser_name()
+
+    def parse_resource(self, resource):
+        return self.conf_resource_parser.parse_resource(resource)
