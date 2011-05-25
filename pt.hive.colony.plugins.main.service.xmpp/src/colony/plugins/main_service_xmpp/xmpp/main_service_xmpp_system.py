dd7f018586f397c058121c37b3200cf3396cea54
João Magalhães <joamag@hive.pt>
2011-05-25 11:28:03 +0000

    minimal code change
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14376 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.xmpp/src/colony/plugins/main_service_xmpp/xmpp/main_service_xmpp_system.py
+++ pt.hive.colony.plugins.main.service.xmpp/src/colony/plugins/main_service_xmpp/xmpp/main_service_xmpp_system.py
@@ -582,6 +582,7 @@ class XmppClientServiceHandler:
         # in case the traceback list is valid
         if traceback_list:
             formated_traceback = traceback.format_tb(traceback_list)
+        # otherwise there is no traceback list
         else:
             formated_traceback = ()
 
