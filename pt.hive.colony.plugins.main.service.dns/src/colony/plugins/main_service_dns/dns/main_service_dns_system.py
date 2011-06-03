62c6fe69c3c5ef797491029df59ddb41db9b1fc5
Tiago Silva <tsilva@hive.pt>
2011-06-03 16:34:16 +0000

    fixed invalid comment in service dns
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14565 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
+++ pt.hive.colony.plugins.main.service.dns/src/colony/plugins/main_service_dns/dns/main_service_dns_system.py
@@ -495,7 +495,7 @@ class DnsClientServiceHandler:
     """ The service configuration """
 
     service_utils_exception_class = None
-    """" The service utils exception class """
+    """ The service utils exception class """
 
     def __init__(self, service_plugin, service_connection_handler, service_configuration, service_utils_exception_class, extra_parameters):
         """
