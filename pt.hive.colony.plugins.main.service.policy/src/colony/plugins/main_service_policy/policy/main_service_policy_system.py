ebdf6547c8414cc9161812784fc4e11d220b2f88
Tiago Silva <tsilva@hive.pt>
2011-06-03 16:50:09 +0000

    fixed invalid comment in service policy
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14568 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy/policy/main_service_policy_system.py
+++ pt.hive.colony.plugins.main.service.policy/src/colony/plugins/main_service_policy/policy/main_service_policy_system.py
@@ -320,7 +320,7 @@ class PolicyClientServiceHandler:
     """ The service configuration """
 
     service_utils_exception_class = None
-    """" The service utils exception class """
+    """ The service utils exception class """
 
     def __init__(self, service_plugin, service_connection_handler, service_configuration, service_utils_exception_class, extra_parameters):
         """
