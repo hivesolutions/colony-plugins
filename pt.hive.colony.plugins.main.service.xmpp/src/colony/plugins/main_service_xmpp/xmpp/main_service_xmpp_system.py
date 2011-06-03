9963d888c9af74eb25350767f35126b29fb98dbe
Tiago Silva <tsilva@hive.pt>
2011-06-03 16:51:34 +0000

    fixed invalid comment in service xmpp
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14572 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.xmpp/src/colony/plugins/main_service_xmpp/xmpp/main_service_xmpp_system.py
+++ pt.hive.colony.plugins.main.service.xmpp/src/colony/plugins/main_service_xmpp/xmpp/main_service_xmpp_system.py
@@ -334,7 +334,7 @@ class XmppClientServiceHandler:
     """ The service configuration """
 
     service_utils_exception_class = None
-    """" The service utils exception class """
+    """ The service utils exception class """
 
     def __init__(self, service_plugin, service_connection_handler, service_configuration, service_utils_exception_class, extra_parameters):
         """
