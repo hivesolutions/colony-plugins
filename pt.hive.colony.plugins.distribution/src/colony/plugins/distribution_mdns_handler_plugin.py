840439a3e7487f6d65b792d4901b0bc1fe03e617
João Magalhães <joamag@hive.pt>
2011-06-08 13:11:19 +0000

    new mdns handler plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14654 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.distribution/src/colony/plugins/distribution_mdns_handler_plugin.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 72 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Tue, 21 Oct 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+
+class DistributionMdnsHandlerPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Distribution Mdns Handler plugin.
+    """
+
+    id = "pt.hive.colony.plugins.distribution.mdns.handler"
+    name = "Distribution Mdns Handler Plugin"
+    short_name = "Distribution Mdns Handler"
+    description = "Distribution Mdns Handler Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/distribution/mdns_handler/resources/baf.xml"
+    }
+    capabilities = [
+        "mdns_service_name_handler",
+        "build_automation_item"
+    ]
+    main_modules = [
+        "distribution.mdns_handler.distribution_mdns_handler_system"
+    ]
+
+    distribution_mdns_handler = None
+    """ The distribution mdns handler """
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        import distribution.mdns_handler.distribution_mdns_handler_system
+        self.distribution_mdns_handler = distribution.mdns_handler.distribution_mdns_handler_system.DistributionMdnsHandler(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_handler_name(self):
+        """
+        Retrieves the handler name.
+
+        @rtype: String
+        @return: The handler name.
+        """
+
+        return self.distribution_mdns_handler.get_handler_name()
+
+    def handle_request(self, request, arguments):
+        """
+        Handles the given mdns request.
+
+        @type request: MdnsRequest
+        @param request: The mdns request to be handled.
+        @type arguments: Dictionary
+        @param arguments: The arguments to the mdns handling.
+        """
+
+        return self.distribution_mdns_handler.handle_request(request, arguments)
