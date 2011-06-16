b108d1d7c5251f596e414bd617b5c29e721f909b
João Magalhães <joamag@hive.pt>
2011-06-16 12:41:54 +0000

    major decorator refactor to avoid plugin version and id
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14787 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.business.dummy/src/colony/plugins/business_dummy_business_logic_plugin.py
+++ pt.hive.colony.plugins.business.dummy/src/colony/plugins/business_dummy_business_logic_plugin.py
@@ -103,7 +103,7 @@ class BusinessDummyBusinessLogicPlugin(colony.base.plugin_system.Plugin):
     def unload_allowed(self, plugin, capability):
         colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
 
-    @colony.base.decorators.inject_dependencies("pt.hive.colony.plugins.business.dummy.business_logic", "1.0.0")
+    @colony.base.decorators.inject_dependencies
     def dependency_injected(self, plugin):
         colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
 
