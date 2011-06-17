82da08638976179fd0096ba38ba5ccc30c4bc1ad
João Magalhães <joamag@hive.pt>
2011-06-17 15:56:07 +0000

    new manager plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14843 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.web.mvc.manager/src/colony/plugins/web_mvc_manager_repository_plugin.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+# -*- coding: Cp1252 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 684 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2008-12-08 15:16:55 +0000 (Seg, 08 Dez 2008) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+import colony.base.decorators
+
+class WebMvcManagerRepositoryPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Web Mvc Manager Repository plugin.
+    """
+
+    id = "pt.hive.colony.plugins.web.mvc.manager.repository"
+    name = "Web Mvc Manager Repository Plugin"
+    short_name = "Web Mvc Manager Repository"
+    description = "The plugin that offers the web mvc manager repository"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/web_mvc_manager/repository/resources/baf.xml"
+    }
+    capabilities = [
+        "web.mvc_service",
+        "web.mvc.manager.page_item_bundle",
+        "build_automation_item"
+    ]
+    dependencies = [
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.web.mvc.utils", "1.0.0"),
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.system.updater", "1.0.0"),
+        colony.base.plugin_system.PluginDependency("pt.hive.colony.plugins.misc.json", "1.0.0")
+    ]
+    main_modules = [
+        "web_mvc_manager.repository.web_mvc_manager_repository_controllers",
+        "web_mvc_manager.repository.web_mvc_manager_repository_exceptions",
+        "web_mvc_manager.repository.web_mvc_manager_repository_system"
+    ]
+
+    web_mvc_manager_repository = None
+    """ The web mvc manager repository """
+
+    web_mvc_utils_plugin = None
+    """ The web mvc utils plugin """
+
+    system_updater_plugin = None
+    """ The system updater plugin """
+
+    json_plugin = None
+    """ The json plugin """
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        import web_mvc_manager.repository.web_mvc_manager_repository_system
+        self.web_mvc_manager_repository = web_mvc_manager.repository.web_mvc_manager_repository_system.WebMvcManagerRepository(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+        self.web_mvc_manager_repository.load_components()
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    @colony.base.decorators.inject_dependencies
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_patterns(self):
+        """
+        Retrieves the tuple of regular expressions to be used as patterns,
+        to the web mvc service. The tuple should relate the route with the handler
+        method/function.
+
+        @rtype: Tuple
+        @return: The tuple of regular expressions to be used as patterns,
+        to the web mvc service.
+        """
+
+        return self.web_mvc_manager_repository.get_patterns()
+
+    def get_communication_patterns(self):
+        """
+        Retrieves the tuple of regular expressions to be used as communication patterns,
+        to the web mvc service. The tuple should relate the route with a tuple
+        containing the data handler, the connection changed handler and the name
+        of the connection.
+
+        @rtype: Tuple
+        @return: The tuple of regular expressions to be used as communication patterns,
+        to the web mvc service.
+        """
+
+        return self.web_mvc_manager_repository.get_communication_patterns()
+
+    def get_resource_patterns(self):
+        """
+        Retrieves the tuple of regular expressions to be used as resource patterns,
+        to the web mvc service. The tuple should relate the route with the base
+        file system path to be used.
+
+        @rtype: Tuple
+        @return: The tuple of regular expressions to be used as resource patterns,
+        to the web mvc service.
+        """
+
+        return self.web_mvc_manager_repository.get_resource_patterns()
+
+    def get_page_item_bundle(self, parameters):
+        """
+        Retrieves a bundle containing all the maps with information
+        on all the page items. The maps should contain information
+        about the composition of the page item.
+
+        @type parameters: Dictionary
+        @param parameters: The parameters to retrieve the page
+        item bundle.
+        @rtype: List
+        @return: A list containing information on all page items.
+        """
+
+        return self.web_mvc_manager_repository.get_page_item_bundle(parameters)
+
+    def get_web_mvc_utils_plugin(self):
+        return self.web_mvc_utils_plugin
+
+    @colony.base.decorators.plugin_inject("pt.hive.colony.plugins.web.mvc.utils")
+    def set_web_mvc_utils_plugin(self, web_mvc_utils_plugin):
+        self.web_mvc_utils_plugin = web_mvc_utils_plugin
+
+    def get_system_updater_plugin(self):
+        return self.system_updater_plugin
+
+    @colony.base.decorators.plugin_inject("pt.hive.colony.plugins.system.updater")
+    def set_system_updater_plugin(self, system_updater_plugin):
+        self.system_updater_plugin = system_updater_plugin
+
+    def get_json_plugin(self):
+        return self.json_plugin
+
+    @colony.base.decorators.plugin_inject("pt.hive.colony.plugins.misc.json")
+    def set_json_plugin(self, json_plugin):
+        self.json_plugin = json_plugin
