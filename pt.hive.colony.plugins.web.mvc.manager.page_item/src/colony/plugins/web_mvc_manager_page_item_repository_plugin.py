c64d7922ab7e1830104ef8232901be752bc788e0
João Magalhães <joamag@hive.pt>
2011-06-17 15:27:17 +0000

    new repository code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk@14834 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.web.mvc.manager.page_item/src/colony/plugins/web_mvc_manager_page_item_repository_plugin.py
+++ pt.hive.colony.plugins.web.mvc.manager.page_item/src/colony/plugins/web_mvc_manager_page_item_repository_plugin.py
@@ -59,6 +59,7 @@ class WebMvcManagerPageItemRepositoryPlugin(colony.base.plugin_system.Plugin):
         "build_automation_file_path" : "$base{plugin_directory}/web_mvc_manager_page_item/repository/resources/baf.xml"
     }
     capabilities = [
+        "web.mvc_service",
         "web.mvc.manager.page_item_bundle",
         "build_automation_item"
     ]
@@ -109,6 +110,33 @@ class WebMvcManagerPageItemRepositoryPlugin(colony.base.plugin_system.Plugin):
     @colony.base.decorators.inject_dependencies
     def dependency_injected(self, plugin):
         colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def get_patterns(self):
+        """
+        Retrieves the tuple of regular expressions to be used as patterns,
+        to the web mvc service. The tuple should relate the route with the handler
+        method/function.
+
+        @rtype: Tuple
+        @return: The tuple of regular expressions to be used as patterns,
+        to the web mvc service.
+        """
+
+        return self.web_mvc_manager_page_item_repository.get_patterns()
+
+    def get_communication_patterns(self):
+        """
+        Retrieves the tuple of regular expressions to be used as communication patterns,
+        to the web mvc service. The tuple should relate the route with a tuple
+        containing the data handler, the connection changed handler and the name
+        of the connection.
+
+        @rtype: Tuple
+        @return: The tuple of regular expressions to be used as communication patterns,
+        to the web mvc service.
+        """
+
+        return self.web_mvc_manager_page_item_repository.get_communication_patterns()
 
     def get_resource_patterns(self):
         """
