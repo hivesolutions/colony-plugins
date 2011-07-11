ff3f7fd33c7a8d66c7afb0df32e02aa418281cd1
João Magalhães <joamag@hive.pt>
2011-07-11 12:33:09 +0000

    new recursion list
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony.plugins/trunk@15125 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/scheduler/run_mode/integration/configuration.py
+++ pt.hive.colony.plugins.resources/src/colony/plugins/resources/resource_manager/resources/scheduler/run_mode/integration/configuration.py
@@ -52,7 +52,7 @@ configuration = {
                 1
             ],
             "recursion_list" : [
-                0, 0, 5, 0, 0
+                0, 0, 1, 0, 0
             ]
         },
         {
