25c797486430061eaeb61ccd02a2ef8bc32ba517
João Magalhães <joamag@hive.pt>
2011-07-12 21:52:26 +0000

    removed test change for build automation
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony.plugins/trunk@15179 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
+++ pt.hive.colony.plugins.main.service.http/src/colony/plugins/main_service_http/http/main_service_http_system.py
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
 
-
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
