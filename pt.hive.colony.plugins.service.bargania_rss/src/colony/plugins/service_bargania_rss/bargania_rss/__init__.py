c4943658930ec0607e6f90c4c8efb194cac795c2
Luís Martinho <lmartinho@hive.pt>
2011-08-11 10:23:29 +0000

    license fix
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony.plugins/trunk@16220 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.service.bargania_rss/src/colony/plugins/service_bargania_rss/bargania_rss/__init__.py
+++ pt.hive.colony.plugins.service.bargania_rss/src/colony/plugins/service_bargania_rss/bargania_rss/__init__.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Bargania Website
-# Copyright (C) 2010 Bargania Lda.
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
 #
-# This file is part of Bargania Website.
+# This file is part of Hive Colony Framework.
 #
-# Bargania Website is confidential and property of Bargania Lda.,
-# its usage is constrained by the terms of the Hive Solutions
-# Confidential Usage License.
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Bargania Website should not be distributed under any circumstances,
-# violation of this may imply legal action.
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# If you have any questions regarding the terms of this license please
-# refer to <http://www.hive.pt/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = "1.0.0"
 """ The version of the module """
 
-__revision__ = "$LastChangedRevision: 421 $"
+__revision__ = "$LastChangedRevision: 72 $"
 """ The revision number of the module """
 
-__date__ = "$LastChangedDate: 2008-11-20 15:16:53 +0000 (Qui, 20 Nov 2008) $"
+__date__ = "$LastChangedDate: 2008-10-21 23:29:54 +0100 (Ter, 21 Out 2008) $"
 """ The last change date of the module """
 
-__copyright__ = "Copyright (c) 2010 Bargania Lda."
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
 """ The copyright for the module """
 
-__license__ = "Hive Solutions Confidential Usage License (HSCUL)"
+__license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
