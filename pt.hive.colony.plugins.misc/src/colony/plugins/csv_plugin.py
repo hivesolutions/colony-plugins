8fe84b1260cfdb4f75a6184375a27b38ed4cbec1
João Magalhães <joamag@hive.pt>
2011-09-16 13:05:02 +0000

    initial support for the csv plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony.plugins/trunk@16498 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

---
+++ pt.hive.colony.plugins.misc/src/colony/plugins/csv_plugin.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 2688 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-04-16 12:24:34 +0100 (qui, 16 Abr 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony.base.plugin_system
+
+class CsvPlugin(colony.base.plugin_system.Plugin):
+    """
+    The main class for the Csv plugin.
+    """
+
+    id = "pt.hive.colony.plugins.misc.csv"
+    name = "Csv Plugin"
+    short_name = "Csv"
+    description = "A plugin to serialize and unserialize csv files"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    loading_type = colony.base.plugin_system.EAGER_LOADING_TYPE
+    platforms = [
+        colony.base.plugin_system.CPYTHON_ENVIRONMENT,
+        colony.base.plugin_system.JYTHON_ENVIRONMENT
+    ]
+    attributes = {
+        "build_automation_file_path" : "$base{plugin_directory}/misc/csv/resources/baf.xml"
+    }
+    capabilities = [
+        "serializer.csv",
+        "build_automation_item"
+    ]
+    main_modules = [
+        "misc.csv.csv_exceptions",
+        "misc.csv.csv_serializer",
+        "misc.csv.csv_system"
+    ]
+
+    csv_system = None
+    """ The csv system """
+
+    def load_plugin(self):
+        colony.base.plugin_system.Plugin.load_plugin(self)
+        import misc.csv.csv_system
+        self.csv_system = misc.csv.csv_system.Csv(self)
+
+    def end_load_plugin(self):
+        colony.base.plugin_system.Plugin.end_load_plugin(self)
+
+    def unload_plugin(self):
+        colony.base.plugin_system.Plugin.unload_plugin(self)
+
+    def end_unload_plugin(self):
+        colony.base.plugin_system.Plugin.end_unload_plugin(self)
+
+    def load_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.load_allowed(self, plugin, capability)
+
+    def unload_allowed(self, plugin, capability):
+        colony.base.plugin_system.Plugin.unload_allowed(self, plugin, capability)
+
+    def dependency_injected(self, plugin):
+        colony.base.plugin_system.Plugin.dependency_injected(self, plugin)
+
+    def dumps(self, object):
+        return self.csv_system.dumps(object)
+
+    def loads(self, csv_string):
+        return self.csv_system.loads(csv_string)
+
+    def load_file(self, csv_file):
+        return self.csv_system.load_file(csv_file)
+
+    def load_file_encoding(self, csv_file, encoding):
+        return self.csv_system.load_file(csv_file, encoding)
---
+++ pt.hive.colony.plugins.misc/src/colony/plugins/misc/csv/csv_serializer.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Colony Framework
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision: 5720 $"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate: 2009-10-21 17:36:22 +0100 (qua, 21 Out 2009) $"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import types
+
+import colony.libs.string_buffer_util
+
+import csv_exceptions
+
+DEFAULT_ENCODING = "Cp1252"
+""" The default encoding for csv files """
+
+def dumps(object):
+    # creates a new string buffer
+    string_buffer = colony.libs.string_buffer_util.StringBuffer()
+
+    # "chunks" the object into the string buffer
+    _chunk(object, string_buffer)
+
+    # retrieves the string value from the
+    # string buffer
+    string_value = string_buffer.get_value()
+
+    # returns the string value
+    return string_value
+
+def _chunk(object, string_buffer):
+    # retrieves the object type
+    object_type = type(object)
+
+    # in case the object type is an instance
+    if object_type == types.InstanceType:
+        # converts the object into a list
+        object = [object]
+    elif not object_type == types.ListType:
+        # raises the csv encoder exception
+        csv_exceptions.CsvEncodeException("invalid object type")
+
+    # retrieves the first element (for reference)
+    object_item = object[0]
+
+    # retrieves the attribute names in order to
+    # create the header value
+    attribute_names = colony.libs.object_util.object_attribute_names(object_item)
+    header_value = ";".join(attribute_names) + "\n"
+
+    # writes the header value to the string buffer
+    string_buffer.write(header_value)
+
+    # iterates over all the object (items)
+    # in the object list for serialization
+    for object_item in object:
+        # retrieves the various object items
+        # attribute values
+        attribute_values = colony.libs.object_util.object_attribute_values(object_item)
+
+        # iterates over all the
+        for attribute_value in attribute_values:
+            # retrieves the attribute value type
+            attribute_value_type = type(attribute_value)
+
+            # encodes the attribute value using the default encoding
+            attribute_value_encoded = attribute_value_type == types.UnicodeType and attribute_value.encode(DEFAULT_ENCODING) or str(attribute_value)
+
+            # writes the encoded attribute value
+            string_buffer.write(attribute_value_encoded + ";")
+
+        # writes the new line in the string buffer
+        string_buffer.write("\n")
+
+def loads(data):
+    pass
+
+def _dechunk(chunks):
+    pass
