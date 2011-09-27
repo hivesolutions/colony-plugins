2b3676c3b93b860e1ea4c3a8aa2c83dd52a65e6d
João Magalhães <joamag@hive.pt>
2011-09-27 12:03:53 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony.plugins/trunk@16694 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- pt.hive.colony.plugins.main.client.http/src/colony/plugins/main_client_http/http/main_client_http_system.py
+++ pt.hive.colony.plugins.main.client.http/src/colony/plugins/main_client_http/http/main_client_http_system.py
@@ -632,7 +632,7 @@ class HttpClient:
             # receives the data
             data = self.client_connection.receive(response_timeout, CHUNK_SIZE)
 
-            print "RECEBEU_A_B_C_D_E_F_g: %s" % data
+            print "RECEBEU_A_B_C_D_E_F_g:_I %s" % data
 
             # in case no valid data was received
             if data == "":
