e74d1e1adf64e428ecfb2a7a68a6ebc7c1bcda5f
João Magalhães <joamag@gmail.com>
2012-07-12 19:25:50 +0100

    new header information

--- service_http/src/service_http/colony/exceptions.py
+++ service_http/src/service_http/colony/exceptions.py
@@ -25,10 +25,10 @@ __author__ = "João Magalhães <joamag@hive.pt>"
 __version__ = "1.0.0"
 """ The version of the module """
 
-__revision__ = "$LastChangedRevision: 421 $"
+__revision__ = "$LastChangedRevision$"
 """ The revision number of the module """
 
-__date__ = "$LastChangedDate: 2008-11-20 15:16:53 +0000 (Qui, 20 Nov 2008) $"
+__date__ = "$LastChangedDate$"
 """ The last change date of the module """
 
 __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
