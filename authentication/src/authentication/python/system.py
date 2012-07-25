e0bbd5a4206ed2a7aaf8f0d489928f4574360a29
João Magalhães <joamag@hive.pt>
2012-07-25 23:23:15 +0100

    small code simplification

--- authentication/src/authentication/python/system.py
+++ authentication/src/authentication/python/system.py
@@ -170,11 +170,11 @@ class AuthenticationPython(colony.base.system.System):
         # resolves the configuration file path
         configuration_file_path = plugin_manager.resolve_file_path("%configuration:" + plugin_id + "%/authentication.py", True)
 
-        # retrieves the authentication python handler plugin path
-        authentication_python_handler_plugin_path = plugin_manager.get_plugin_path_by_id(plugin_id)
+        # retrieves the authentication plugin path
+        plugin_path = plugin_manager.get_plugin_path_by_id(plugin_id)
 
         # creates the authentication configuration file path
-        authentication_configuration_file_path = authentication_python_handler_plugin_path + "/" +  CONFIGURATION_PATH + "/authentication_configuration.py"
+        authentication_configuration_file_path = plugin_path + "/" +  CONFIGURATION_PATH + "/authentication_configuration.py"
 
         # ensures that the configuration file path exists and contains the default contents
         colony.libs.path_util.ensure_file_path(configuration_file_path, authentication_configuration_file_path)
