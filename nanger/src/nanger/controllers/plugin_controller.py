a247746f95a04ecab51e73aaa242981a7ec0821f
João Magalhães <joamag@gmail.com>
2012-07-26 13:49:54 +0100

    new plugins listing support

--- nanger/src/nanger/controllers/plugin_controller.py
+++ nanger/src/nanger/controllers/plugin_controller.py
@@ -57,4 +57,60 @@ class PluginController(controllers.Controller):
         @param parameters: The handler parameters.
         """
 
-        pass
+        # retrieves the reference to the plugin manager running
+        # in the current context
+        plugin_manager = self.plugin.manager
+
+        # retrieves the json plugin for the encoding of the
+        # response value (serialized value)
+        json_plugin = self.plugin.json_plugin
+
+        # retrieves the various fields that are going to be used to
+        # perform the query over the plugins
+        filter = self.get_field(rest_request, "filter_string", "")
+        start_record = self.get_field(rest_request, "start_record", 0, int)
+        number_records = self.get_field(rest_request, "number_records", 9, int)
+
+        # ues the plugin manager to retrieve the list of all loaded plugin
+        # instances (this is a list of object and as such is not serializable)
+        loaded_plugins = plugin_manager.get_all_loaded_plugins()
+
+        # creates the list that will hold the various plugin description maps
+        # to be generates for the filtered plugins
+        plugins = []
+
+        # converts the filter into a lower cased representation to be able to
+        # perform a case insensitive comparison
+        _filter = filter.lower();
+
+        # iterates over all the loaded plugins to be able to filter the ones
+        # that comply with the provided query
+        for loaded_plugin in loaded_plugins:
+            # converts the plugin identifier and name into lower cased
+            # values for the comparison and then performs it in case
+            # the comparison fails continues the loop immediately
+            id = loaded_plugin.id.lower()
+            name = loaded_plugin.name.lower()
+            if not id.startswith(_filter) and not name.startswith(_filter): continue
+
+            # creates the plugin map containing the identifier and the
+            # name of the plugin and then adds it to the plugins list
+            plugin = {
+                "id" : loaded_plugin.id,
+                "name" : loaded_plugin.name
+            }
+            plugins.append(plugin)
+
+        # sorts the plugins list using the default alphabetic order and
+        # then retrieves the request chunk of data
+        plugins.sort()
+        plugins = plugins[start_record:start_record + number_records]
+
+        # serializes the list of plugin map with json to create the
+        # final result contents, should retrieve the appropriate mime type
+        result = json_plugin.dumps(plugins)
+        mime_type = json_plugin.get_mime_type()
+
+        # sets the (resulting) contents in the rest request and sets the
+        # appropriate mime type according to the serialization
+        self.set_contents(rest_request, result, content_type = mime_type)
---
+++ nanger/src/nanger/resources/templates/general/plugins.html.tpl
@@ -0,0 +1,14 @@
+<ul class="filter entities-list">
+    <input type="text" class="text-field section-input filter-input" data-original_value="Search plugins" />
+    <div class="data-source" data-url="${out_none value=base_path /}plugins/list" data-type="json"></div>
+    <li class="template clear">
+        <div class="name"><a href="#">%[name]</a></div>
+        <div class="description">%[id]</div>
+    </li>
+    <div class="filter-no-results quote">
+        No results found
+    </div>
+    <div class="filter-more">
+        <span class="button">Load more</span>
+    </div>
+</ul>
