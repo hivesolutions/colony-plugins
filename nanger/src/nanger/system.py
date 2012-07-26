8ec69260ff659f384eaa623a1877c7e0c0820c5b
João Magalhães <joamag@gmail.com>
2012-07-26 09:48:49 +0100

    new support for the handle init action

--- nanger/src/nanger/system.py
+++ nanger/src/nanger/system.py
@@ -82,6 +82,7 @@ class Nanger(colony.base.system.System):
             (r"^nanger/index$", self.main_controller.handle_index, "get"),
             (r"^nanger/console$", self.main_controller.handle_console, "get"),
             (r"^nanger/about$", self.main_controller.handle_about, "get"),
+            (r"^nanger/console/init$", self.console_controller.handle_init, ("get", "post")),
             (r"^nanger/console/execute$", self.console_controller.handle_execute, ("get", "post")),
             (r"^nanger/console/autocomplete$", self.console_controller.handle_autocomplete, ("get", "post"))
         )
