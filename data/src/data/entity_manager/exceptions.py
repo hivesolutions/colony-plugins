adc46b736e43a21c785e34e09c0991647b416b06
João Magalhães <joamag@gmail.com>
2012-09-20 14:09:18 +0100

    new serializer exception

--- data/src/data/entity_manager/exceptions.py
+++ data/src/data/entity_manager/exceptions.py
@@ -176,3 +176,29 @@ class RelationValidationError(ValidationError):
         """
 
         return "Relation validation error - %s" % self.message
+
+class InvalidSerializerError(ValidationError):
+    """
+    The invalid serializer error class.
+    """
+
+    def __init__(self, message):
+        """
+        Constructor of the class.
+
+        @type message: String
+        @param message: The message to be printed.
+        """
+
+        EntityManagerException.__init__(self)
+        self.message = message
+
+    def __str__(self):
+        """
+        Returns the string representation of the class.
+
+        @rtype: String
+        @return: The string representation of the class.
+        """
+
+        return "Invalid serializer error - %s" % self.message
