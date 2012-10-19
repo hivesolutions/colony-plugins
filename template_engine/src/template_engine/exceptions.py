63f54de3760484430f0f87e6559396c2f5a78bb6
João Magalhães <joamag@gmail.com>
2012-10-19 16:48:05 +0100

    new exceptions

--- template_engine/src/template_engine/exceptions.py
+++ template_engine/src/template_engine/exceptions.py
@@ -176,3 +176,29 @@ class InvalidBooleanValue(TemplateEngineException):
         """
 
         return "Invalid boolean value - %s" % self.message
+
+class InvalidSerializer(TemplateEngineException):
+    """
+    The invalid serializer class.
+    """
+
+    def __init__(self, message):
+        """
+        Constructor of the class.
+
+        @type message: String
+        @param message: The message to be printed.
+        """
+
+        TemplateEngineException.__init__(self)
+        self.message = message
+
+    def __str__(self):
+        """
+        Returns the string representation of the class.
+
+        @rtype: String
+        @return: The string representation of the class.
+        """
+
+        return "Invalid serializer - %s" % self.message
