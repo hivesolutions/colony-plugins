3c2a66146bf264ab0811d65823384405b3d72c14
joamag <joamag@gmail.com>
2013-07-26 21:27:19 +0100

    removed unused loading type attribute

--- format/src/ber_plugin.py
+++ format/src/ber_plugin.py
@@ -49,7 +49,6 @@ class BerPlugin(colony.base.system.Plugin):
     description = "The plugin that offers the ber (basic encoding rules) format support"
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
-    loading_type = colony.base.system.EAGER_LOADING_TYPE
     platforms = [
         colony.base.system.CPYTHON_ENVIRONMENT,
         colony.base.system.JYTHON_ENVIRONMENT,
