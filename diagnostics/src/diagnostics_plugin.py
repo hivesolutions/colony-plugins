af606b828f7a76656e58bb653904ac618efb4ed9
joamag <joamag@gmail.com>
2014-09-24 11:47:31 +0100

    new diagnostics plugin

---
+++ diagnostics/src/diagnostics_plugin.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Colony Framework
+# Copyright (c) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import colony
+
+class DiagnosticsPlugin(colony.Plugin):
+    """
+    The main class for plugin responsible for the gathering
+    and processing of diagnostics information to be used
+    both for profiling and debugging.
+    """
+
+    id = "pt.hive.colony.plugins.diagnostics"
+    name = "Diagnostics Engine"
+    description = "Diagnostics Engine Plugin"
+    version = "1.0.0"
+    author = "Hive Solutions Lda. <development@hive.pt>"
+    platforms = [
+        colony.CPYTHON_ENVIRONMENT,
+        colony.JYTHON_ENVIRONMENT,
+        colony.IRON_PYTHON_ENVIRONMENT
+    ]
+    capabilities = [
+        "diagnostics"
+    ]
+    main_modules = [
+        "diagnostics"
+    ]
+
+    def load_plugin(self):
+        colony.Plugin.load_plugin(self)
+        import diagnostics
+        self.diagnostics = diagnostics.Diagnostics(self)
