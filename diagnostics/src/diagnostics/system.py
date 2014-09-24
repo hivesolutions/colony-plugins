ace9c1abc062b3bcde27b54a416d471d341bd9d2
joamag <joamag@gmail.com>
2014-09-24 13:38:40 +0100

    better event handling

--- diagnostics/src/diagnostics/system.py
+++ diagnostics/src/diagnostics/system.py
@@ -46,66 +46,123 @@ class Diagnostics(colony.System):
     def __init__(self, plugin):
         colony.System.__init__(self, plugin)
         self.data = dict()
-        self.currents = dict()
+        self.state = dict()
         self.diagnostics = colony.conf("DIAGNOSTICS", False, cast = bool)
 
     def start(self):
         if not self.diagnostics: return
-        colony.register_g("request.start", self.request_start)
-        colony.register_g("request.stop", self.request_stop)
+        colony.register_g("request.begin", self.request_begin)
+        colony.register_g("request.end", self.request_end)
+        colony.register_g("orm.begin", self.orm_begin)
+        colony.register_g("orm.end", self.orm_end)
         colony.register_g("sql.executed", self.sql_executed)
 
     def stop(self):
         if not self.diagnostics: return
-        colony.unregister_g("request.start", self.request_start)
-        colony.unregister_g("request.stop", self.request_stop)
+        colony.unregister_g("request.begin", self.request_begin)
+        colony.unregister_g("request.end", self.request_end)
+        colony.unregister_g("orm.begin", self.orm_begin)
+        colony.unregister_g("orm.end", self.orm_end)
         colony.unregister_g("sql.executed", self.sql_executed)
 
     def get_data(self):
         return self.data
 
-    def request_start(self, request):
-        requests = self.data.get("requests", {})
-        requests_l = self.data.get("requests_l", [])
-        data = dict(
-            id = request._generation_time,
-            initial = request._generation_time,
-            path = request.get_path(),
-            operations = dict()
-        )
-        requests[request] = data
-        requests_l.append(request)
-        self.data["requests"] = requests
-        self.data["requests_l"] = requests_l
-        self.currents["request"] = data
+    def push_state(self, name, value):
+        stack = self.state.get(name, [])
+        stack.append(value)
+        self.state[name] = stack
 
-    def request_stop(self, request):
-        requests = self.data.get("requests", {})
-        data = requests[request]
+    def pop_state(self, name):
+        stack = self.state[name]
+        return stack.pop()
+
+    def peek_state(self, name, default = None):
+        stack = self.state.get(name, None)
+        if not stack: return default
+        return stack[-1]
+
+    def add_operation(self, name, target, data):
+        state = self.peek_state(target)
+        if not state: return
+        totals = state["totals"]
+        operations = state["operations"]
+        sequence = operations.get(name, [])
+        sequence.append(data)
+        total = totals.get(name, 0)
+        time = data.get("time", 0)
+        total += time
+        operations[name] = sequence
+        totals[name] = total
+
+    def set_final(self, data):
         initial = data["initial"]
         final = time.time()
         delta = int((final - initial) * 1000)
         data["final"] = final
         data["time"] = delta
-        self.currents["request"] = None
+
+    def request_begin(self, request):
+        identifier = id(request)
+        requests = self.data.get("requests", {})
+        requests_l = self.data.get("requests_l", [])
+        data = dict(
+            id = identifier,
+            initial = time.time(),
+            path = request.get_path(),
+            operations = dict(),
+            totals = dict()
+        )
+        requests[identifier] = data
+        requests_l.append(data)
+        self.data["requests"] = requests
+        self.data["requests_l"] = requests_l
+        self.push_state("request", data)
+
+    def request_end(self, request):
+        identifier = id(request)
+        requests = self.data.get("requests", {})
+        data = requests[identifier]
+        self.set_final(data)
+        self.pop_state("request")
 
         import pprint
         pprint.pprint(data)
 
-    def sql_executed(self, query, time, engine):
+    def orm_begin(self, operation, options):
+        identifier = id(operation)
+        orms = self.data.get("orms", {})
+        orms_l = self.data.get("orms_l", [])
+        data = dict(
+            id = identifier,
+            initial = time.time(),
+            operation = operation,
+            operations = dict(),
+            totals = dict()
+        )
+        orms[identifier] = data
+        orms_l.append(data)
+        self.data["orms"] = orms
+        self.data["orms_l"] = orms_l
+        self.push_state("orm", data)
+
+    def orm_end(self, operation, options, count):
+        identifier = id(operation)
+        orms = self.data.get("orms", {})
+        data = orms[identifier]
+        self.set_final(data)
+        self.pop_state("orm")
+        self.add_operation("orm", "request", data)
+
+    def sql_executed(self, query, engine, time):
         data = dict(
             query = query,
-            time = time,
-            engine = engine
+            engine = engine,
+            time = time
         )
         sql = self.data.get("sql", [])
         sql.append(data)
         self.data["sql"] = sql
 
-        request_data = self.currents.get("request", None)
-        if not request_data: return
-
-        operations = request_data["operations"]
-        sql = operations.get("sql", [])
-        sql.append(data)
-        operations["sql"] = sql
+        self.add_operation("sql", "request", data)
+        self.add_operation("sql", "orm", data)
