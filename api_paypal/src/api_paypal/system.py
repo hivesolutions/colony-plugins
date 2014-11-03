8de719161eae90033cf18878d373e1fa71a1d5fc
joamag <joamag@gmail.com>
2014-11-03 17:19:05 +0000

    new python3 support

--- api_paypal/src/api_paypal/system.py
+++ api_paypal/src/api_paypal/system.py
@@ -39,7 +39,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import colony
 
-import exceptions
+from api_paypal import exceptions
 
 DEFAULT_CHARSET = "utf-8"
 """ The default charset """
