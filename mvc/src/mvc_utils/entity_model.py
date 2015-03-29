109051d22e377913b4a838ee823f93214f9f25f0
joamag <joamag@gmail.com>
2015-03-29 16:08:56 +0100

    removed extra imports

--- mvc/src/mvc_utils/entity_model.py
+++ mvc/src/mvc_utils/entity_model.py
@@ -37,8 +37,6 @@ __copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import types
-import inspect
 import datetime
 import itertools
 
