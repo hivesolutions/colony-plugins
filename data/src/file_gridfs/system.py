0ee209cb25ae2ef4174c7aa081b6286db92e998c
joamag <joamag@gmail.com>
2015-04-08 00:13:54 +0100

    small code fix

--- data/src/file_gridfs/system.py
+++ data/src/file_gridfs/system.py
@@ -81,7 +81,7 @@ class FileGridfs(colony.System):
         # creates a new mongo connection
         # for file insertion and then retrieves the correct
         # database from it (as the connection)
-        is_new = int(pymongo.version) >= 3
+        is_new = int(pymongo.version[0]) >= 3
         if is_new: connection = pymongo.MongoClient(hostname, port)
         else: connection = pymongo.Connection(hostname, port)
         connection_database = connection[database]
