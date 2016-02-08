b23683367028617c80dea4a546725973da7cc31b
joamag <joamag@hive.pt>
2016-02-08 14:30:59 +0000

    new license value

--- service_http/src/service_http_system_information/resources/js/main.js
+++ service_http/src/service_http_system_information/resources/js/main.js
@@ -1,24 +1,24 @@
-// Hive Colony Web Framework
+// Hive Colony Framework
 // Copyright (c) 2008-2016 Hive Solutions Lda.
 //
-// This file is part of Hive Colony Web Framework.
+// This file is part of Hive Colony Framework.
 //
-// Hive Colony Web Framework is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Hive Colony Framework is free software: you can redistribute it and/or modify
+// it under the terms of the Apache License as published by the Apache
+// Foundation, either version 2.0 of the License, or (at your option) any
+// later version.
 //
-// Hive Colony Web Framework is distributed in the hope that it will be useful,
+// Hive Colony Framework is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// Apache License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Hive Colony Web Framework. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the Apache License along with
+// Hive Colony Framework. If not, see <http://www.apache.org/licenses/>.
 
 // __author__    = João Magalhães <joamag@hive.pt>
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision$
 // __date__      = $LastChangedDate$
 // __copyright__ = Copyright (c) 2008-2016 Hive Solutions Lda.
-// __license__   = General Public License (GPL), Version 3
+// __license__   = Apache License, Version 2.0
