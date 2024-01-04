68db6b563bead26efe0eee392909960f6bcd4dfb
João Magalhães <joamag@gmail.com>
2024-01-04 17:53:03 +0000

    chore: removed extra fields

--- mvc/src/mvc_utils/utils.py
+++ mvc/src/mvc_utils/utils.py
@@ -22,14 +22,6 @@
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
-__version__ = "1.0.0"
-""" The version of the module """
-
-__revision__ = "$LastChangedRevision$"
-""" The revision number of the module """
-
-__date__ = "$LastChangedDate$"
-""" The last change date of the module """
 
 __copyright__ = "Copyright (c) 2008-2023 Hive Solutions Lda."
 """ The copyright for the module """
