4b29ea9c6dc08cdc11682c6d69d8fe3f6c16607c
João Magalhães <joamag@gmail.com>
2025-12-28 21:10:36 +0000

    Revert "fix: standardize timestamp handling across modules"
    This reverts commit 113f05ce3022e6c71e8009f49dee846cdacba431.

--- rest/src/rest/system.py
+++ rest/src/rest/system.py
@@ -2590,7 +2590,7 @@ class RESTSession(object):
         and only available through secure connections.
         """
 
-        current_timestamp = int(time.time())
+        current_timestamp = time.time()
         expires_timestamp = current_timestamp + DEFAULT_EXPIRATION_DELTA_TIMESTAMP
         expires_date_time = datetime.datetime.utcfromtimestamp(expires_timestamp)
         expires_date_time_formatted = expires_date_time.strftime(DATE_FORMAT)
